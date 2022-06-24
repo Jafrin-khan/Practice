2780ae34147ddcb1148ff548a1375bda202fc00b
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-06-24 18:54:21 +0530

    Time: 83 ms (15.26%), Space: 90.1 MB (20.53%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -2,49 +2,32 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        int m = nums1.size();
-        int n = nums2.size();
+         vector<int> merged;
         
-        if(m > n){
-            return findMedianSortedArrays(nums2, nums1);
+        for(int i = 0 ; i < nums1.size() ; i++){
+            merged.push_back(nums1[i]);
         }
         
-        int low = 0;
-        int high = m;
-        
-        int medianPos = ((m+n)+1)/2;
-        
-        while(low <= high){
-            
-            int cut1 = (low + high)/2;
-            int cut2 = medianPos - cut1;
-            
-            int l1 = (cut1 == 0)? INT_MIN:nums1[cut1-1];
-            int r1 = (cut1 == m)? INT_MAX:nums1[cut1];
-            
-            int l2 = (cut2 == 0)?INT_MIN:nums2[cut2-1];
-            int r2 = (cut2 == n)?INT_MAX:nums2[cut2];
-            
-            if(l1 <= r2 && l2 <= r1){
-                if((m+n)%2 != 0){
-                    return max(l1,l2);
-                }
-                
-                else{
-                    return (max(l1,l2) + min(r1,r2))/2.0;
-                }
-            }
-            
-            else if(l1 > l2){
-                high = cut1-1;
-            }
-            
-            else{
-                low = cut1+1;
-            }
+        for(int i = 0 ; i < nums2.size() ; i++){
+            merged.push_back(nums2[i]);
         }
         
-        return 0.0;
+        sort(merged.begin(),merged.end());
+        
+        double ans;
+        int n =(merged.size());
+        
+        if(merged.size()%2 != 0){
+            ans = double(merged[n/2]);
+        }
+        
+        else{
+        ans =  (double(merged[n/2]) + double(merged[(n/2)-1]))/2.0;
+        
+        }
+        
+        return ans;
+        
         
     }
 };
