a9561539d6132e19ec2227148a1a3a781b6afe71
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-06-26 12:58:00 +0530

    Time: 426 ms (11.47%), Space: 74.2 MB (10.90%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -1,43 +1,69 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    
-    void store(ListNode* head , vector<int> &v){
-        
-        if(head == NULL){
-            return;
-        }
-        
-        ListNode* temp = head;
-        while(temp != NULL){
-            v.push_back(temp->val);
-            temp = temp->next;
-        }
-    }
     ListNode* sortList(ListNode* head) {
+        //If List Contain a Single or 0 Node
+        if(head == NULL || head ->next == NULL)
+            return head;
         
         
-        ListNode* temp = head;
-        vector<int> v;
+        ListNode *temp = NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
         
-        store(head , v);
-        sort(v.begin(),v.end());
-        int i = 0;
+        // 2 pointer appraoach / turtle-hare Algorithm (Finding the middle element)
+        while(fast !=  NULL && fast -> next != NULL)
+        {
+            temp = slow;
+            slow = slow->next;          //slow increment by 1
+            fast = fast ->next ->next;  //fast incremented by 2
+            
+        }   
+        temp -> next = NULL;            //end of first left half
         
-        while(temp != NULL){
-            temp->val = v[i++];
-            temp = temp->next;
+        ListNode* l1 = sortList(head);    //left half recursive call
+        ListNode* l2 = sortList(slow);    //right half recursive call
+        
+        return mergelist(l1, l2);         //mergelist Function call
+            
+    }
+    
+    //MergeSort Function O(n*logn)
+    ListNode* mergelist(ListNode *l1, ListNode *l2)
+    {
+        ListNode *ptr = new ListNode(0);
+        ListNode *curr = ptr;
+        
+        while(l1 != NULL && l2 != NULL)
+        {
+            if(l1->val <= l2->val)
+            {
+                curr -> next = l1;
+                l1 = l1 -> next;
+            }
+            else
+            {
+                curr -> next = l2;
+                l2 = l2 -> next;
+            }
+        
+        curr = curr ->next;
+        
         }
         
-        return head;
+        //for unqual length linked list
+        
+        if(l1 != NULL)
+        {
+            curr -> next = l1;
+            l1 = l1->next;
+        }
+        
+        if(l2 != NULL)
+        {
+            curr -> next = l2;
+            l2 = l2 ->next;
+        }
+        
+        return ptr->next;
     }
 };
