a6c606846bc69e0d890cfa27b5abdc020e678866
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-06-27 20:22:19 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (55.61%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,27 +1,65 @@
 class MyStack {
 public:
-    deque<int> dq;
+    
+    queue<int> q1;
+    queue<int> q2;
     
     MyStack() {
         
     }
     
     void push(int x) {
-        dq.push_back(x);
+        q1.push(x);
     }
     
     int pop() {
-        int ans = dq.back();
-        dq.pop_back();
+        if(q1.size() == 0){
+            return -1;
+        }
+        while(q1.size() != 1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        int ans = q1.front();
+        q1.pop();
+        
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
         return ans;
+        
     }
     
     int top() {
-        return dq.back();
+        if(q1.size() == 0){
+            return -1;
+        }
+        while(q1.size() != 1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        int ans = q1.front();
+        q1.pop();
+        q2.push(ans);
+        
+         while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
+        
+        return ans;
+        
     }
     
     bool empty() {
-        return dq.empty();
+        
+        return q1.empty();
+        
     }
 };
 
