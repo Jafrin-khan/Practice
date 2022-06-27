3c200d9e412f12587f9df9494b843a551f97838b
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-06-27 19:29:44 +0530

    Time: 90 ms (5.11%), Space: 8.5 MB (87.98%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -2,37 +2,21 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-        unordered_map<int,int> mp;
-        stack<int> st;
+        vector<int> ans(nums1.size() , -1);
         
-        reverse(nums2.begin(),nums2.end());
-        
-        for(int i = 0 ; i < nums2.size() ; i++){
-            
-            while(!st.empty() && st.top() < nums2[i]){
-                st.pop();
-            }
-            
-            if(st.empty()){
-                mp[nums2[i]] = -1;
-            }
-            
-            if(!st.empty() && st.top() > nums2[i]){
-                mp[nums2[i]] = st.top();
-            }
-            
-            st.push(nums2[i]);
-        }
-        
-        vector<int> ans;
+        reverse(nums2.begin() , nums2.end());
         
         for(int i = 0 ; i < nums1.size() ; i++){
             
-            if(mp.find(nums1[i]) != mp.end()){
-                ans.push_back(mp[nums1[i]]);
+            for(int j = 0 ; j < nums2.size() && nums1[i] != nums2[j] ; j++){
+                
+                if(nums2[j] > nums1[i]){
+                    ans[i] = nums2[j];
+                }
             }
         }
         
         return ans;
+        
     }
 };
