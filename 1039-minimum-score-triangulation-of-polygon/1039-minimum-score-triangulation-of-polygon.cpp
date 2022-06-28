b31166e2df7f006371cf000c3b029318e08f18c1
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-06-28 09:23:18 +0530

    Time: 8 ms (76.97%), Space: 8.5 MB (59.85%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,38 +1,12 @@
 class Solution {
 public:
     
-    int f(int i , int j , vector<int>& nums , vector<vector<int>>& dp){
-        
-        if(i == j){
-            return 0;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        int mini = 1e9;
-        int steps;
-        int k;
-        for(k = i ; k < j ; k++){
-            steps = nums[i-1]*nums[k]*nums[j] + f(i,k,nums , dp) + f(k+1,j,nums , dp);
-            mini = min(mini , steps);
-        }
-        
-        return dp[i][j] = mini;
-    }
+
     int minScoreTriangulation(vector<int>& nums) {
         
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int>(n,0));
-        
-        for(int i = 1 ; i < n ; i++){
-            dp[i][i] = 0;
-        }
-        
-       
-      
-        
+    
     for(int i = n-1 ; i >= 1 ; i--){
             
         for(int j = i+1 ; j < n ; j++){
@@ -41,7 +15,7 @@ public:
             int steps;
             int k;
             for(k = i ; k < j ; k++){
-                steps = nums[i-1]*nums[k]*nums[j] + f(i,k,nums , dp) + f(k+1,j,nums , dp);
+                steps = nums[i-1]*nums[k]*nums[j] + dp[i][k] + dp[k+1][j];
                 mini = min(mini , steps);
         }
         
