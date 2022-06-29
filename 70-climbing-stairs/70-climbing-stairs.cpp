ee1ba1c758e3bb98c618b9192b5d1c2c2f64e9e7
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-06-29 23:03:45 +0530

    Time: 2 ms (41.80%), Space: 6.2 MB (14.77%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    
-    int f(int n , vector<int>& dp){
-        
-         if(n <= 0){
-            return 0;
-        }
-        
-        if(n == 1){
-            return 1;
-        }
-        
-        if(n == 2){
-            return 2;
-        }
-        
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        
-        int x = f(n-1 , dp);
-        int y = f(n-2 , dp);
-        
-        return dp[n] = x+y;
-        
-    }
+
     int climbStairs(int n) {
         
        vector<int> dp(n+2,-1);
-       return f(n,dp);
+       
+       dp[0] = 0;
+       dp[1] = 1;
+       dp[2] = 2;
         
+       for(int i = 3 ; i <= n ; i++){
+             
+        int x = dp[i-1];
+        int y = dp[i-2];
+        
+        dp[i] = x+y;
+       }
+       
+       return dp[n] ;
     }
 };
