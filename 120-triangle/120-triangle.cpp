71ae47e963ebc488c7466805cf58bb00623e5a65
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-06-30 22:49:40 +0530

    Time: 15 ms (18.64%), Space: 9 MB (21.99%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-      int f(int i,int j,vector<vector<int>>&a,vector<vector<int>>&dp)
-    {
-        if(i==a.size()-1)
+    
+    int f(int i , int j , vector<vector<int>>& triangle , vector<vector<int>>& dp){
+        
+        if(i == triangle.size()-1){
             return 0;
-        if(dp[i][j]!=-1)
+        }
+        
+        if(dp[i][j] != -1){
             return dp[i][j];
-        int l=a[i+1][j]+f(i+1,j,a,dp);
-        int r=a[i+1][j+1]+f(i+1,j+1,a,dp);
-        return dp[i][j]=min(r,l);
+        }
+
+        int dl = triangle[i+1][j] + f(i+1 , j , triangle , dp);
+        int dr = triangle[i+1][j+1] + f(i+1 , j+1 , triangle , dp);
+        
+        return dp[i][j] = min(dl , dr);
     }
-    int minimumTotal(vector<vector<int>>& a) {
-        vector<vector<int>>dp(a.size(),vector<int>(a.size(),-1));
-     int r=f(0,0,a,dp)+a[0][0];
-        return r;
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int n = triangle.size();
+        vector<vector<int>> dp(n , vector<int>(n , -1));
+        return f(0 , 0 , triangle , dp) + triangle[0][0];
+        
     }
 };
