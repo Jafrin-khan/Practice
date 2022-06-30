0d9d4fa908c7e41a26863fdffb59411864d3dff4
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-06-30 19:33:40 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.35%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     
-    int f(int i , int j , int m , int n , vector<vector<int>>& dp){
-        
-        if(i >= m || j >= n){
-            return 0;
-        }
-        
-        if(i == m-1 && j == n-1){
-            return 1;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        int x = f(i+1 , j , m , n , dp);
-        int y = f(i , j+1 , m , n , dp);
-        
-        return dp[i][j] = x+y;
-    }
+ 
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(m , vector<int>(n , -1));
-        return f(0 , 0 , m , n , dp);
+        vector<vector<int>> dp(m , vector<int>(n, -1));
+        
+        dp[m-1][n-1] = 1;
+        
+        for(int i = m-2 ; i >= 0 ; i--){
+            
+            for(int j = n-2 ; j >= 0 ; j--){
+                
+
+                    int x = dp[i+1][j];
+                    int y = dp[i][j+1];
+
+                    dp[i][j] = x+y;
+                
+            }
+        }
+        return abs(dp[0][0]);
     }
 };
