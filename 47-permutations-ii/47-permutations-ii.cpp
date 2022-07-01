9cc5e9b82ec4622a0c0b014ae3df5c6c655bed3a
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-01 17:25:40 +0530

    Time: 450 ms (6.03%), Space: 101.4 MB (5.32%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+      
+    void f(set<vector<int>> & ans , vector<int> helper , vector<int> freq , vector<int>& nums){
+        
+        if(helper.size() == nums.size()){
+            ans.insert(helper);
+            return;
+        }
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+            
+            if(freq[i] != 1){
+                helper.push_back(nums[i]);
+                freq[i] = 1;
+                f(ans,helper,freq,nums);
+                freq[i] = 0;
+                helper.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+       set<vector<int>> ans;
+        vector<int> helper;
+        
+        vector<int> freq(nums.size(),0);
+        
+      
+        vector<vector<int>> FAns;
+       
+        f(ans,helper,freq , nums);
+        
+        for(auto x : ans){
+            FAns.push_back(x);
+        }
+        
+        return FAns;
+        
+        
+        
+    }
+};
