104b41e9391258f07198f074ad9c1b980e1bcb36
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-06 19:28:12 +0530

    Time: 11 ms (15.12%), Space: 8.4 MB (14.76%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
      vector<vector<int>> ans;
-    
-    void f(int ind , vector<int> &nums , vector<int> helper , vector<int>& freq){
+    void f(int ind , vector<int>& nums , vector<int> helper , vector<int>& marked){
         
-        if(helper.size() == nums.size()){
-            ans.push_back(helper);
-            return;
+         if(helper.size() == nums.size()){
+                ans.push_back(helper);
+                return;
         }
-       
+      
+        
         for(int i = 0 ; i < nums.size() ; i++){
             
-            if(freq[i] != -1){
-            helper.push_back(nums[i]);
-            freq[i] = -1;
-            f(i+1 , nums , helper , freq);
-            helper.pop_back();
-            freq[i] = 0;
+            if(marked[i] != -1){
+                helper.push_back(nums[i]);
+                marked[i] = -1;
+                f(ind + 1 , nums , helper , marked);
+                helper.pop_back();
+                marked[i] = 0;
             }
         }
-        
     }
     vector<vector<int>> permute(vector<int>& nums) {
         
+       
+        vector<int> marked(nums.size() , 1);
         vector<int> helper;
-        vector<int> freq(nums.size(),0);
-        f(0 , nums , helper , freq);
+        f(0 , nums , helper , marked);
         
         return ans;
-        
-        
     }
 };
