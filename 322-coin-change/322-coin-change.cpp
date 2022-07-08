21f336062ce90d9ef8632773aa07c030e8151d0d
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-08 15:45:21 +0530

    Time: 109 ms (67.47%), Space: 45.7 MB (19.07%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,60 +1,41 @@
 class Solution {
 public:
     
-    int f(int ind, vector<int>& coins , int amount , vector<vector<int>>& dp){
-      if(amount == 0){
-            return 0;
+    int f(int ind , int amt , vector<int>& coins ,  vector<vector<int>>& dp){
+        
+        if(ind == 0){
+            if(amt%coins[0] == 0){
+                return amt/coins[0];
+            }
+            
+            else{
+                return 1e9;
+            }
         }
         
-    if(dp[ind][amount] != -1){
-        return dp[ind][amount];
+        if(dp[ind][amt] != -1){
+            return dp[ind][amt];
+        }
+        
+        int notPick = 0 + f(ind-1,amt , coins , dp);
+        int pick = INT_MAX;
+        if(amt >= coins[ind]){
+            pick = 1 + f(ind , amt - coins[ind] , coins , dp);
+        }
+        
+        return dp[ind][amt] = min(pick,notPick);
     }
-      if(ind == 0){
-          
-          if(amount % coins[ind] == 0){
-              return amount/coins[ind];
-          }
-          return 1e9;
-      }   
-          int notPick = 0 + f(ind-1 , coins , amount , dp);
-          int pick = 1e9;
-          if(amount >= coins[ind]){
-          pick = 1 + f(ind , coins , amount - coins[ind] , dp);
-          }
-          return dp[ind][amount] = min(pick,notPick);
-      }
-      
-    
-    int coinChange(vector<int>& coins, int amount) {
-             
-             vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,0));
-             int n = coins.size();
-            
-             for(int i = 0 ; i <= amount ; i++){
-                  if(i % coins[0] == 0){
-                  dp[0][i] = i/coins[0];
-              }
-            else{
-                     dp[0][i] = 1e9;
-                 }
-             }
-            
-             for(int ind = 1 ; ind < n ; ind++){
-                 for(int i = 0 ; i <= amount ; i++){
-                      int notPick = 0 + dp[ind-1][i];
-                      int pick = 1e9;
-                      if(i >= coins[ind]){
-                      pick = 1 + dp[ind][i - coins[ind]];
-                      }
-                      dp[ind][i] = min(pick,notPick);
-                 }
-             }
+    int coinChange(vector<int>& coins, int amt) {
         
-             int ans= dp[coins.size()-1][amount];
-             if(ans == 1e9){
-                 return -1;
-             }
+        int n = coins.size();
+        vector<vector<int>> dp(n , vector<int>(amt+1,-1));
         
-             return ans;
+        int ans =  f(coins.size()-1 , amt , coins , dp);
+        
+        if(ans == 1e9){
+            return -1;
+        }
+        
+        return ans;
     }
 };
