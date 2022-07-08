214784fa1b6b6268883dbf7df75aa899e6598f0b
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-08 18:30:43 +0530

    Time: 153 ms (10.41%), Space: 81.6 MB (5.88%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
     
-   
+    int f(int i,int j,string s1, string s2 , vector<vector<int>>& dp){
+        
+        if(i < 0){
+            return j+1;
+        }
+        
+        if(j < 0){
+            return i+1;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s1[i] == s2[j]){
+            return dp[i][j] = 0 + f(i-1 , j-1,s1,s2 , dp);
+        }
+        
+        else{
+            
+            int replace = 1 + f(i-1 , j-1,s1,s2 , dp);
+            int del = 1 + f(i-1 , j,s1,s2 , dp);
+            int insert = 1 + f(i , j - 1,s1,s2 , dp);
+            
+            return dp[i][j] = min({replace , del , insert});
+        }
+    }
     int minDistance(string s1, string s2) {
         
-        int m = s1.size();
-        int n = s2.size();
+        int n = s1.size();
+        int m = s2.size();
         
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        vector<vector<int>> dp(n+1 , vector<int>(m+1,-1));
         
-        for(int j = 0 ; j <= n ; j++){
-            dp[0][j] = j;
-        }
-        
-        for(int i = 0 ; i <= m ; i++){
-            dp[i][0] = i;
-        }
-        
-        for(int i = 1 ; i <= m ; i++){
-            
-            for(int j = 1 ; j <= n ; j++){
-                
-                 if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-
-                else{
-
-                    int insert = 1 + dp[i][j-1];
-                    int del = 1 + dp[i-1][j];
-                    int replace = 1 + dp[i-1][j-1];
-
-                    dp[i][j] = min({insert,del,replace});
-            }}}
-        
-        return dp[m][n];
+        return f(n , m , s1 , s2 , dp);
         
     }
 };
