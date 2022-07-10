4521bcd30a8f34c135b682916293a1a2a29b3e55
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-10 13:49:29 +0530

    Time: 59 ms (5.17%), Space: 24.2 MB (59.46%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -1,36 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+public:
     
-    void inorder(TreeNode* root, int k, priority_queue<int>& pq){
+    void f(TreeNode* root , priority_queue<int>& pq , int k){
+        
         if(root == NULL){
             return;
         }
         
-        if(pq.size() < k){
-            pq.push(root->val);
-        }else{
-            if(pq.top() > root->val){
-                pq.pop();
-                pq.push(root->val);
-            }
+        pq.push(root->val);
+        if(pq.size() > k){
+            pq.pop();
         }
-        inorder(root->left,k,pq);
-        inorder(root->right,k,pq);
+        
+        f(root->left , pq , k);
+        f(root->right , pq , k);
         
     }
-    
-public:
     int kthSmallest(TreeNode* root, int k) {
         
-        //min heap priority of size k
-        priority_queue <int> pq; 
+        priority_queue<int> pq;
+        f(root , pq , k);
         
-        //recurse inorder
-        
-        inorder(root,k,pq);
-        
-        
-        
-        //return the top
         return pq.top();
     }
 };
