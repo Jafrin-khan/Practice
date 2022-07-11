c486b1ac238a18f6508c966e9c817c302834e718
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-11 17:05:58 +0530

    Time: 7 ms (83.12%), Space: 6.1 MB (47.86%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     string intToRoman(int num) {
         
-    
-        int integers[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string roman[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        string symbol[] = {"M" , "CM" , "D" , "CD" ,"C","XC", "L" , "XL" , "X" , "IX" , "V" , "IV" , "I"};
+        int value[] = {1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1};
         
+        int i = 0;   
         string ans = "";
-        
-        for(int i = 0 ; i < 13 && num > 0; i++){
+        while(num > 0){
             
-            while(num >= integers[i]){
-                
-                ans += roman[i];
-                num -= integers[i];
+            while(value[i] > num){
+                i++;
             }
+            
+            num -= value[i];
+            ans += symbol[i];
         }
         
         return ans;
