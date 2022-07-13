a12b06b42c7784f5b4dbbb398fc3991f2f5334a7
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-13 14:18:51 +0530

    Time: 39 ms (24.11%), Space: 14.5 MB (26.92%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-   
-    unordered_map <string, bool> m; // Map will work as cache or help in memoization step
+   unordered_map <string, bool> m; // Map will work as cache or help in memoization step
     
     bool wordBreak(string s, vector <string> &d) 
     {
@@ -16,11 +15,9 @@ public:
             string left = s.substr(0, i); // check for each substring of s
             if(find(d.begin(), d.end(), left) != d.end() && wordBreak(s.substr(i), d))
             { // if that left exists in d, than check for remaining part of s via recursion
-                return m[s] = true; // if both part return true , store it in map and return true
+                return m[left] = true; // if both part return true , store it in map and return true
             }
         }
         return m[s] = false; // else just store false for that s in map and return false
     }
-        
-    
 };
