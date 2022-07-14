c0b0b849351e2d21770e0f0a6e814816ea7a03b2
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-14 08:53:05 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (33.68%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(int n) {
+         //if n is even then return sum of 1st n/2 odd numbers
+      if(n%2==0)
+      {
+          n/=2;
+          return n*n;
+      }
+        //if n is odd then return sum of 1st n/2 even numbers
+     n/=2;
+        return (n+1)*n;
+    }
+};
