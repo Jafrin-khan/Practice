e3f4955fcceff5d82efe2e53d724f5d6a7f98025
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-14 16:16:34 +0530

    Time: 213 ms (29.49%), Space: 13.2 MB (90.25%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     
-    static bool cmp(string &a , string &b){
-        return a.size() < b.size();
+    static bool cmp(string& v1 , string& v2){
+        return v1.size() < v2.size();
     }
     
-    bool toCompare(string &s1 , string &s2){
+    bool compare(string& s1 , string& s2){
         
         int n = s1.size();
         int m = s2.size();
         
-        if(n != m+1){
+        if(n != m-1){
             return false;
         }
         
         int p1 = 0;
         int p2 = 0;
         
-        while(p1 < n){
+        while(p2 < m){
             
             if(s1[p1] == s2[p2]){
                 p1++;
@@ -25,7 +25,7 @@ public:
             }
             
             else{
-                p1++;
+                p2++;
             }
         }
         
@@ -35,32 +35,32 @@ public:
         
         return false;
     }
+    
     int longestStrChain(vector<string>& words) {
-      
-        int n = words.size();
-        vector<int> dp(n,1);
         
         sort(words.begin() , words.end() , cmp);
+        int n = words.size();
+        
+        vector<int> dp(n , 1);
         int maxi = 1;
         
         for(int ind = 0 ; ind < n ; ind++){
             
             for(int prev_ind = 0 ; prev_ind < ind ; prev_ind++){
                 
-                if(toCompare(words[ind] , words[prev_ind]) && (dp[ind] < dp[prev_ind]+1) ){
+                if(compare(words[prev_ind] , words[ind]) && dp[ind] < dp[prev_ind] + 1){
                     dp[ind] = dp[prev_ind] + 1;
-                  
                 }
                 
                 if(dp[ind] > maxi){
-                    maxi = dp[ind];
-                }
-                
+                maxi = dp[ind];
             }
+            }
+            
+            
         }
         
-        return maxi;   
-        
+        return maxi;
         
     }
 };
