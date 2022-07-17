761f897ceac788f3ae75420a0a8170b2e39eea95
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-17 10:14:27 +0530

    Time: 25 ms (86.96%), Space: 19.1 MB (83.28%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int start = 0;
+        int end = 0;
+        
+        while( end < nums.size()){
+            if(nums[end] != 0){
+                swap(nums[start++] , nums[end]);
+            }
+            
+            end++;
+        }
+        
+    }
+};
