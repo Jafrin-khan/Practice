c70a15f3e8d2e8cd348d6d32232dd1f463df6819
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-17 12:25:36 +0530

    Time: 76 ms (29.57%), Space: 28.3 MB (83.21%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int minMoves(vector<int>& nums) {
         
-        int mini = nums[0];
+        int smallestNum = nums[0];
+        for(int i = 0 ; i < nums.size() ; i++){
+            smallestNum = min(smallestNum , nums[i]);
+        }
         
         int cnt = 0;
-        
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(nums[i] < mini){
-                mini = nums[i];
-            }
-        }
-         
         for(auto x : nums){
-            cnt += x - mini;
+            cnt  += (x - smallestNum);
         }
         
         return cnt;
