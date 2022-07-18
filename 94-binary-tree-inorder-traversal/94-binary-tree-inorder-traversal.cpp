762b7f412f47b564ca164429103edf4342f9b4dd
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-18 10:20:32 +0530

    Time: 3 ms (59.95%), Space: 8.4 MB (74.95%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,45 +11,43 @@
  */
 class Solution {
 public:
-    
     vector<int> inorderTraversal(TreeNode* root) {
         
         if(root == NULL){
             return {};
         }
         
-        vector<int> ans;
-      
         TreeNode* curr = root;
+        vector<int> ans;
         
         while(curr != NULL){
             
-            if(curr->left == NULL){
-                ans.push_back(curr->val);
-                curr = curr->right;
-            }
+              if(curr->left == NULL){
+                  ans.push_back(curr->val);
+                  curr = curr->right;
+              }
             
-            else{
-                
-                TreeNode* prev = curr->left;
-                
-                while(prev->right && prev->right != curr){
-                    prev = prev->right;
-                }
-                
-                if(prev->right == NULL){
-                    prev->right = curr;
-                    curr = curr->left;
-                }
-                
-                else{
-                    prev->right = NULL;
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                }
-                
-            }
+              else{
+                  TreeNode* prev = curr->left;
+                  
+                  while(prev->right && prev->right != curr){
+                      prev = prev->right;
+                  }
+                  
+                  if(prev->right == NULL){
+                      prev->right = curr;
+                      curr = curr->left;
+                  }
+                  
+                  else{
+                      prev->right = NULL;
+                      ans.push_back(curr->val);
+                      curr = curr->right;
+                  }
+              }
         }
+        
         return ans;
+        
     }
 };
