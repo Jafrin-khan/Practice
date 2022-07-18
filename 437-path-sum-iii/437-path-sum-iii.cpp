38e3d1374c6925a2b6ec437ea02d75c2372d595d
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-18 09:52:35 +0530

    Time: 35 ms (56.95%), Space: 16.1 MB (62.29%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -1,28 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int count=0;
-    void recursive(TreeNode* root, long long sum, int targetSum){
-        if(root==NULL){
+    
+    int count = 0;
+    void f(TreeNode* root,long long sum , int targetSum){
+        
+        if(root == NULL){
             return;
         }
-        sum=sum+root->val;
         
-        if(sum==targetSum){
+        sum += root->val;
+        
+        if(sum == targetSum){
             count++;
         }
-        //cout<<"sum=> "<<sum<<" count=> "<<count<<endl;
-            recursive(root->left, sum,targetSum);
-            recursive(root->right, sum, targetSum);
+        
+        f(root->left , sum , targetSum);
+        f(root->right , sum , targetSum);
+        
     }
+    
     int pathSum(TreeNode* root, int targetSum) {
-        if(root==NULL){
+        
+        if(root == NULL){
             return 0;
         }
-        long long sum=0;
-        recursive(root, sum, targetSum);
-        //cout<<"cout=> "<<count<<endl;
-        pathSum(root->left,targetSum);
-        pathSum(root->right,targetSum);
+        
+        int sum = 0;
+        f(root , sum , targetSum);
+        pathSum(root->left , targetSum);
+        pathSum(root->right , targetSum);
+        
         return count;
     }
 };
