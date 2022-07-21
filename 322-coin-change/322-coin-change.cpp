a7de060dc34d36c5705732fc467f6d39356616b8
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-21 21:53:45 +0530

    Time: 134 ms (51.57%), Space: 45.7 MB (18.41%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,8 +4,8 @@ public:
     int f(int ind , int amt , vector<int>& coins , vector<vector<int>>& dp){
         
         if(ind == 0){
-             if(amt%coins[ind] == 0){
-                 return amt/coins[ind];
+             if(amt%coins[0] == 0){
+                 return amt/coins[0];
              }
             
             return 1e9;
@@ -19,18 +19,18 @@ public:
         int take = 1e9;
         
         if(coins[ind] <= amt){
-            take = 1 + f(ind , amt - coins[ind], coins , dp);
+            take = 1 + f(ind , amt - coins[ind], coins , dp); 
         }
         
         return dp[ind][amt] = min(take , notTake);
         
     }
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int amt) {
         
         int n = coins.size();
-        vector<vector<int>> dp(n , vector<int>(amount+1 , -1));
+        vector<vector<int>> dp(n , vector<int>(amt+1 , -1));
         
-        int ans =  f(n - 1 , amount , coins , dp);
+        int ans =  f(n - 1 , amt , coins , dp);
         
         if(ans == 1e9){
             return -1;
