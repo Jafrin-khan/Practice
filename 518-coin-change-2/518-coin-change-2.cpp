37e6c00255093bd923897701f2e8400909a56a72
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-21 22:06:42 +0530

    Time: 75 ms (22.41%), Space: 18.5 MB (42.13%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    
-
-    int change(int T, vector<int>& arr) {
-        int n= arr.size();
-    vector<vector<long>> dp(n,vector<long>(T+1,0));
-    //Initializing base condition
-    for(int i=0;i<=T;i++){
-        if(i%arr[0]==0)
-            dp[0][i]=1;
-        // Else condition is automatically fulfilled,
-        // as dp array is initialized to zero
-    }
-    
-    for(int ind=1; ind<n;ind++){
-        for(int target=0;target<=T;target++){
-            long notTaken = dp[ind-1][target];
-            
-            long taken = 0;
-            if(arr[ind]<=target)
-                taken = dp[ind][target-arr[ind]];
-                
-            dp[ind][target] = notTaken + taken;
+    int change(int amt, vector<int>& coins) {
+        
+         int n = coins.size();
+        vector<vector<int>> dp(n , vector<int>(amt+1 , 0));
+        
+         for(int i = 0 ; i <= amt ; i++){
+            if(i%coins[0] == 0){
+                dp[0][i] = 1;
+            }
+             
+            else{
+                dp[0][i] = 0;
+            }
         }
-    }
+        
+        for(int ind = 1 ; ind < n ; ind++){
+            for(int j = 0 ; j <= amt ; j++){
+                  
+            int notTake = dp[ind - 1][j];
+            int take = 0;
+
+            if(coins[ind] <= j){
+                take = dp[ind][j - coins[ind]]; 
+            }
+
+            dp[ind][j] = take + notTake;
+            
+            }
+        }
+        
+        int ans =  dp[n - 1][amt];
+        
     
-    return dp[n-1][T];
+        return ans;
+        
+        
     }
 };
