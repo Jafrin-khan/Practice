c19bf4413b630b43f647b06329db56d00278e407
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-21 11:59:29 +0530

    Time: 41 ms (52.62%), Space: 10.1 MB (84.61%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     string removeDuplicates(string s, int k) {
         
-        stack<pair<char,int>> st;
+        stack<pair<char , int>> st;
         
-        for(int i = 0 ; i < s.length() ; i++){
-            
-            if(st.empty()){
-                st.push({s[i],1});
-                continue;
-            }
-            
-            if(st.top().first == s[i]){
+        for(int i = 0 ; i < s.size() ; i++){
+
+            if(!st.empty() && s[i] == st.top().first){
                 
                 if(st.top().second == k-1){
                     st.pop();
@@ -23,22 +18,23 @@ public:
             }
             
             else{
-                st.push({s[i],1});
-            }
+                st.push({s[i] , 1});
+            }   
         }
         
         string ans = "";
         
         while(!st.empty()){
+            int i = st.top().second;
             
-            int i = 0;
-            while(i < st.top().second){
-            ans += st.top().first;
-            i++;
+            while(i--){
+                ans += st.top().first;
             }
+            
             st.pop();
         }
-        reverse(ans.begin(),ans.end());
+        
+        reverse(ans.begin() , ans.end());
         return ans;
         
     }
