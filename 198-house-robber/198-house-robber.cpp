8966e7f807a804f89b1308206d3372649d772a5c
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-23 19:13:50 +0530

    Time: 4 ms (35.28%), Space: 7.9 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     
     
-    int f(int ind , vector<int>& nums , vector<int>& dp){
-        
-        
-        if(ind < 0){
-            return 0;
-        }
-        if(ind == 0){
-            return nums[ind];
-        }
-        
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
-        
-        int notTaken = 0 + f(ind - 1 , nums , dp);
-        int taken = nums[ind] + f(ind - 2 , nums , dp);
-        
-        
-        return dp[ind] = max(taken , notTaken);
-        
-    }
-    
+
     int rob(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n , -1);
+        vector<int> dp(n , 0);
         
-        return f(n-1 , nums , dp);
+        if(n == 1){
+            return nums[0];
+        }
+        
+        if(n == 2){
+            return max(nums[0] , nums[1]);
+        }
+        dp[0] = nums[0];
+        dp[1] = max(nums[0] , nums[1]);
+        
+        for(int ind = 2 ; ind < n ; ind++){
+            int notTaken = 0 + dp[ind - 1];
+            int taken = INT_MIN;
+            taken = nums[ind] + dp[ind - 2];
+
+            dp[ind] = max(taken , notTaken);
+        }
+        
+        return dp[n-1];
         
     }
 };
