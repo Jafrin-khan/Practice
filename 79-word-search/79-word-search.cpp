1f19f3655f5d635ed7e99ec14e0d76b07126d756
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-24 14:33:47 +0530

    Time: 1371 ms (5.43%), Space: 8 MB (35.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     
-    bool f(int i , int j ,vector<vector<char>>& board , string word, int ind){
+    bool f(int i , int j , int ind , vector<vector<char>>& board, string word){
+        
         int n = board.size();
         int m = board[0].size();
-         if(ind == word.size()){
+        
+        if(ind >= word.size()){
             return true;
         }
         
-        if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[ind]){
+        if(i < 0 || j < 0 || i >= n || j >= m || word[ind] != board[i][j]){
             return false;
         }
         
-       
-       board[i][j] = '$';
-        bool ans =  f(i+1 , j , board , word , ind+1) || f(i , j+1 , board , word , ind+1)
-                    || f(i-1 , j , board , word , ind+1) || f(i , j-1 , board , word , ind+1);
-       board[i][j] = word[ind];
-       
-        return ans;
+        int x = board[i][j];
+        board[i][j] = '$';
         
+        int ans=  f(i+1 , j , ind + 1 , board , word) ||
+                  f(i-1 , j , ind + 1 , board , word) ||
+                  f(i , j+1 , ind + 1 , board , word) ||
+                  f(i , j-1 , ind + 1 , board , word) ;
+       board[i][j] = x;
+        
+       return ans;
     }
+    
+    
     bool exist(vector<vector<char>>& board, string word) {
         
-        int ind = 0;
+         
+        int n = board.size();
+        int m = board[0].size();
         
-        for(int i = 0 ; i < board.size() ; i++){
-            for(int j = 0 ; j < board[0].size() ; j++){
-                if(board[i][j] == word[0] && f(i , j , board , word , 0)){
-                    return true ;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(board[i][j] == word[0] && f(i , j , 0 , board , word)){
+                    return true;
                 }
-                
             }
         }
         
