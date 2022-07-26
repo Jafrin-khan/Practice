c1adfd4f424aac60f6836963124b1ebe67658ff3
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-26 14:11:38 +0530

    Time: 16 ms (62.56%), Space: 6.9 MB (85.87%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         
-        unordered_map<char , int> mp;
-        
+        int n = s.size();
         int start = 0;
         int end = 0;
-        int maxCount = 0;
-        int maxi = INT_MIN;
         
-        while(end < s.size()){
+        int maxi = INT_MIN;
+        int maxCount = 0;
+        
+        unordered_map<char , int> mp;
+        
+        while(end < n){
             
             mp[s[end]]++;
             maxCount = max(maxCount , mp[s[end]]);
             
+            //to check if we have flipped more than k characters then we decrease the window size
             if(end - start + 1 - maxCount > k){
                 mp[s[start]]--;
                 start++;
@@ -21,9 +24,9 @@ public:
             
             maxi = max(maxi , end - start + 1);
             end++;
+            
         }
         
         return maxi;
-        
     }
 };
