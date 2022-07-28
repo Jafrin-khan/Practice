d2d7610156408920497e79e9611e092424e1b31d
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-28 13:36:23 +0530

    Time: 57 ms (17.56%), Space: 13 MB (41.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,6 @@
 class Solution {
 public:
-    
-    int f(int i , int j ,string s1, string s2 ,vector<vector<int>>& dp){
-        
-        if(i == 0 || j == 0){
-            return 0;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        if(s1[i-1] == s2[j-1]){
-            return dp[i][j] = 1 + f(i-1 , j-1 , s1 ,s2 , dp);
-        }
-        
-        return dp[i][j] = max(f(i-1,j , s1 ,s2 , dp) , f(i , j-1 , s1 ,s2 , dp));
-    }
-    
+    //memozation will always give TLE int last 3 test cases hence fo for tabulation starightsway 
     int longestCommonSubsequence(string s1, string s2) {
         
         int n = s1.size();
