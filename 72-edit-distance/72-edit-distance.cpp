4e145b2e0b32bb45403f801cc7bcf05027285513
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-28 19:03:23 +0530

    Time: 783 ms (5.00%), Space: 400.3 MB (5.40%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int f(int i,int j,string s1, string s2 , vector<vector<int>>& dp){
+    int f(int i , int j , string s1 , string s2 , vector<vector<int>>& dp){
         
         if(i < 0){
             return j+1;
@@ -16,26 +16,23 @@ public:
         }
         
         if(s1[i] == s2[j]){
-            return dp[i][j] = 0 + f(i-1 , j-1,s1,s2 , dp);
+            return 0 + f(i-1 , j-1, s1 , s2 , dp);
         }
         
-        else{
-            
-            int replace = 1 + f(i-1 , j-1,s1,s2 , dp);
-            int del = 1 + f(i-1 , j,s1,s2 , dp);
-            int insert = 1 + f(i , j - 1,s1,s2 , dp);
-            
-            return dp[i][j] = min({replace , del , insert});
-        }
+        int insert = 1 + f(i , j-1, s1 , s2 , dp);
+        int del = 1 + f(i-1 , j, s1 , s2 , dp);
+        int rep = 1 + f(i-1,j-1, s1 , s2 , dp);
+        
+        return dp[i][j] = min({insert , del , rep});
     }
     int minDistance(string s1, string s2) {
         
         int n = s1.size();
         int m = s2.size();
         
-        vector<vector<int>> dp(n+1 , vector<int>(m+1,-1));
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , -1));
         
-        return f(n , m , s1 , s2 , dp);
-        
+        return f(n-1 , m-1 , s1 , s2 , dp);
+ 
     }
 };
