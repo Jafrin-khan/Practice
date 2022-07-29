cdca6f6d175bdd07a3f387a1c97fb1dab4b6d64c
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-29 15:22:17 +0530

    Time: 109 ms (16.92%), Space: 8.8 MB (76.02%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        
-        sort(nums.begin() , nums.end());
+    
+       
         int n = nums.size();
         
-        vector<int> dp(n,1) , hash(n);
-        int lastIndex = 0;
+        if(n == 0){
+            return {};
+        }
+        
+        if(n == 1){
+            return {nums[0]};
+        }
+         
+        sort(nums.begin() , nums.end());
+        vector<int> dp(n , 1);
+        vector<int> hash(n);
+        
+        int lastIndex;
         int maxi = 1;
         
-        for(int ind = 0 ; ind < nums.size() ; ind++){
-                        hash[ind] = ind;
-            for(int prev_ind = 0 ; prev_ind < ind ; prev_ind++){
+        for(int ind = 0 ; ind < n ; ind++){
+              hash[ind] = ind;
+            for(int prev = 0 ; prev < ind ; prev++){
+               
+              if(nums[ind]%nums[prev] == 0 && dp[ind] < dp[prev] + 1){
+                  dp[ind] = dp[prev] + 1;
+                  hash[ind] = prev;
+              }
                 
-                if(nums[ind]%nums[prev_ind] == 0 && dp[ind] < dp[prev_ind]+1){
-                       dp[ind] =  dp[prev_ind]+1;
-                        hash[ind] = prev_ind;
-                }
-                
-                if(dp[ind] > maxi){
-                    maxi = dp[ind];
-                    lastIndex = ind;
-                }
-                
+              if(dp[ind] > maxi){
+                  maxi = dp[ind];
+                  lastIndex = ind;
+              }
             }
         }
         
@@ -32,9 +42,12 @@ public:
         while(lastIndex != hash[lastIndex]){
             lastIndex = hash[lastIndex];
             ans.push_back(nums[lastIndex]);
+           
         }
         
-        reverse(ans.begin() , ans.end());
+        
+        
         return ans;
+        
     }
 };
