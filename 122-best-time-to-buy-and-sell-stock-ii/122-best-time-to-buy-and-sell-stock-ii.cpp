ddec25fc8cb25b4df871491a214b1f983604b592
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-29 20:39:30 +0530

    Time: 26 ms (7.23%), Space: 15.3 MB (12.00%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -34,9 +34,29 @@ public:
         if(n == 0 || n == 1){
             return 0;
         }
-        vector<vector<int>> dp(n , vector<int>(2,-1));
+        vector<vector<int>> dp(n+1 , vector<int>(3,0));
         
-        return f(0 , 1 , prices , dp);
+        for(int ind = n-1 ; ind >= 0 ; ind--){
+            
+            for(int buy = 1 ; buy >= 0 ; buy--){
+                
+                 int profit = 0;
+        
+        
+                    if(buy){
+                        profit =  max(-prices[ind] + dp[ind+1][0] , 0 + dp[ind + 1][1]);
+                    }
+
+                    else{
+                        profit = max(prices[ind] + dp[ind + 1][1] ,0 + dp[ind + 1][0]);
+                    }
+
+                    dp[ind][buy] = profit;
+                
+            }
+        }
+        
+        return dp[0][1];
         
     }
 };
