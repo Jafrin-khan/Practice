6167a50364e7fe86243569697f09a1eced1a2c2c
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-30 14:40:25 +0530

    Time: 3 ms (84.65%), Space: 7.9 MB (12.60%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     
-       int f(int i , int j ,int m,int n, vector<vector<int>>& dp , vector<vector<int>>& nums){
-           
-           if(i < 0 || j < 0 || nums[i][j] == 1){
-            return 0;
-        }
+      
+    int f(int i , int j ,  vector<vector<int>>& dp , vector<vector<int>>& grid){
         
         if(i == 0 && j == 0){
             return 1;
         }
         
-        
+        if(i < 0 || j < 0 || grid[i][j] == 1){
+            return 0;
+        }
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
-        int right = f(i-1,j,m,n , dp , nums);
-        int down = f(i,j-1,m,n , dp , nums);
+        int left = f(i , j-1 , dp , grid);
+        int up = f(i-1 , j , dp , grid);
         
-        return dp[i][j] = right + down;
+        return dp[i][j] = left + up;
     }
-   
-    int uniquePathsWithObstacles(vector<vector<int>>& nums) {
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         
+      
+         int m = grid.size();
+        int n = grid[0].size();
         
-
-        int m = nums.size();
-        int n = nums[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1 , n-1 , m , n , dp , nums);
+        if(grid[0][0] == 1){
+            return 0;
+        }
         
+         vector<vector<int>> dp(m , vector<int>(n,-1));
+        return f(m-1 , n-1 , dp , grid);
         
     }
 };
