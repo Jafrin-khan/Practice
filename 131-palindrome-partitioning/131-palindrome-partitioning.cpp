81b7722fa97942cfc705f700cf20e5190d83e86b
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-07-30 13:28:38 +0530

    Time: 396 ms (14.33%), Space: 144.9 MB (16.95%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
     
-    bool isPalindrome(int start , int end , string s){
+    bool isValid(int start , int end , string s){
         
-        while(start <= end){
+        while(start < end){
             if(s[start++] != s[end--]){
-            return false;
-        }
+                return false;
+            }
         }
         
         return true;
     }
     
-    void f(int ind , string s , vector<string> h , set<vector<string>>& st){
+    void f(int ind , vector<vector<string>>& ans , vector<string> helper , string s){
         
-        if(ind >= s.size()){
-            st.insert(h);
+        if(ind == s.size()){
+            if(helper.size() > 0){
+            ans.push_back(helper);
+            }
             return;
         }
         
         for(int i = ind ; i < s.size() ; i++){
             
-            if(isPalindrome(ind , i , s)){
-                h.push_back(s.substr(ind , i - ind + 1));
-                 f(i+1 , s , h , st);
-                h.pop_back();
+            if(isValid(ind , i , s)){
+                helper.push_back(s.substr(ind , i - ind + 1));
+                f(i+1 , ans , helper , s);
+                helper.pop_back();
             }
         }
+        
     }
+    
     vector<vector<string>> partition(string s) {
+        
         vector<vector<string>> ans;
-        vector<string> h ;
-        set<vector<string>> st;
-        
-        f(0 , s , h , st);
-        
-        for(auto x : st){
-            ans.push_back(x);
-        }
+        vector<string> helper;
+        f(0 , ans , helper , s);
         
         return ans;
+        
     }
 };
