b63c4939da0be433882e3c58f23b10910f13f603
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-02 19:23:07 +0530

    Time: 3 ms (82.01%), Space: 6.9 MB (99.59%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.cpp
+++ 980-unique-paths-iii/980-unique-paths-iii.cpp
@@ -1,57 +1,51 @@
 class Solution {
 public:
     
-    int f(int i , int j ,vector<vector<int>> grid){
+    int f(int i , int j , vector<vector<int>>& grid){
         
-        int n = grid.size();
+         int n = grid.size();
         int m = grid[0].size();
         
-        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == -1 || grid[i][j] == 1){
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == -1){
             return 0;
         }
         
         if(grid[i][j] == 2){
             
-            int cnt = 0;
-            for(int p = 0 ; p < n ; p++){
-                for(int q = 0 ; q < m ; q++){
-                    if(grid[p][q] == 0){
-                        cnt++;
+            for(int i = 0 ; i < n ; i++){
+                for(int j = 0 ; j < m ; j++){
+                    if(grid[i][j] == 0){
+                        return 0;
                     }
                 }
-            }
-            
-            if(cnt != 0){
-                return 0;
             }
             
             return 1;
         }
         
-        grid[i][j] = 1;
+        grid[i][j] = -1;
+        int ans = f(i+1 , j , grid) + f(i , j+1 , grid) + f(i-1 , j , grid) + f(i , j-1 , grid);
+        grid[i][j] = 0;
         
-        int u = f(i - 1 , j , grid);
-        int d = f(i + 1 , j , grid);
-        int l = f(i , j - 1 , grid);
-        int r = f(i , j + 1 , grid);
-        
-        return u + d + l + r;
+        return ans;
+    
     }
     int uniquePathsIII(vector<vector<int>>& grid) {
         
         int n = grid.size();
         int m = grid[0].size();
-        int ans;
         
-         for(int i = 0 ; i < n ; i++){
-                for(int j = 0 ; j < m ; j++){
-                    if(grid[i][j] == 1){
-                        ans = f(i+1 , j , grid) + f(i-1 , j , grid) + f(i , j-1 , grid) + f(i , j+1 , grid);
-                        break;
-                    }
+        int cnt;
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 1){
+                    cnt = f(i , j , grid);
                 }
             }
+        }
         
-        return ans;
+        return cnt;
+        
     }
 };
