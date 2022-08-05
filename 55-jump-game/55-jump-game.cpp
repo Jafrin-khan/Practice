c4ffe5bae392e385bf726add902362f0b00f8f95
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-05 10:26:33 +0530

    Time: 120 ms (44.59%), Space: 48.4 MB (78.26%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,13 +3,16 @@ public:
     bool canJump(vector<int>& nums) {
         
         int farthest = 0;
+        
         for(int i = 0 ; i < nums.size()-1 ; i++){
-            farthest = max(farthest , i + nums[i]);
+            
+            farthest = max(farthest , nums[i]+i);
             if(i == farthest && nums[i] == 0){
                 return false;
             }
         }
         
         return true;
+        
     }
 };
