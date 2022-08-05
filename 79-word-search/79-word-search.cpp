df1528c8706196a31a6066f3bb4d014fecc8db7b
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-05 10:10:06 +0530

    Time: 910 ms (28.40%), Space: 8 MB (37.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -3,38 +3,40 @@ public:
     
     bool f(int i , int j , int ind , vector<vector<char>>& board, string word){
         
-        int n = board.size();
+         int n = board.size();
         int m = board[0].size();
         
         if(ind >= word.size()){
             return true;
         }
         
-        if(i < 0 || j < 0 || i >= n || j >= m || word[ind] != board[i][j]){
+        if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[ind]){
             return false;
         }
         
-        int x = board[i][j];
+        char ch = board[i][j];
         board[i][j] = '$';
         
-        int ans=  f(i+1 , j , ind + 1 , board , word) ||
-                  f(i-1 , j , ind + 1 , board , word) ||
-                  f(i , j+1 , ind + 1 , board , word) ||
-                  f(i , j-1 , ind + 1 , board , word) ;
-       board[i][j] = x;
+        bool ans =  f(i+1 , j , ind+1 , board , word) ||
+                f(i , j+1 , ind+1 , board , word) ||
+                f(i-1 , j , ind+1 , board , word) ||
+                f(i , j-1 , ind+1 , board , word);
         
-       return ans;
+        board[i][j] = ch;
+        
+        return ans;
+     
     }
-    
     
     bool exist(vector<vector<char>>& board, string word) {
         
-         
         int n = board.size();
         int m = board[0].size();
         
         for(int i = 0 ; i < n ; i++){
+            
             for(int j = 0 ; j < m ; j++){
+                
                 if(board[i][j] == word[0] && f(i , j , 0 , board , word)){
                     return true;
                 }
@@ -42,5 +44,6 @@ public:
         }
         
         return false;
+        
     }
 };
