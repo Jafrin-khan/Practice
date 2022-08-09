ede5982b78bbe5a947c119844f0514e014e28f38
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-09 13:29:03 +0530

    Time: 27 ms (39.09%), Space: 13 MB (70.13%) - LeetHub

--- 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.cpp
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.cpp
@@ -23,30 +23,35 @@ public:
     int size = 0;
     
     void helper(vector<NestedInteger> &nestedList){
-        for(int i = 0 ; i < nestedList.size() ; i++){
+        
+         for(int i = 0 ; i < nestedList.size() ; i++){
             if(nestedList[i].isInteger()){
                 output.push_back(nestedList[i].getInteger());
+        
             }
             
             else{
-                helper(nestedList[i].getList());
+                 helper(nestedList[i].getList());
             }
         }
         
     }
     NestedIterator(vector<NestedInteger> &nestedList) {
-        helper(nestedList);    
+         helper(nestedList);
     }
     
     int next() {
+        
         int ans = output.at(size);
         size++;
-        return ans;
         
+        return ans;
+       
     }
     
     bool hasNext() {
-        return size != output.size();
+        
+        return output.size() != size;
     }
 };
 
