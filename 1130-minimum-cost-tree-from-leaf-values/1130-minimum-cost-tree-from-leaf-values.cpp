b7d7542c98039bc047daac37acea1e57ef46858c
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-09 11:01:22 +0530

    Time: 84 ms (9.43%), Space: 10.2 MB (12.10%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     
-    // for explanation refer this if confused
-    // https://www.youtube.com/watch?v=pYs3qj42h3c
-    
-    map<pair<int,int> , pair<int,int>>dp;
-    
-    pair<int,int> f(vector<int>& arr , int s , int e){
+     map<pair<int,int> , pair<int,int>>dp;
+    pair<int,int> f(int s , int e , vector<int>& arr){
         
         if(s == e){
-            return {arr[s] ,0};
+            return {arr[s],0};
+            //zero bcoz it doesnt have left or right leaf hence their product will be zero eventually as they dont exist
         }
         
         int leafMax = INT_MIN;
         int minSum = INT_MAX;
         
+         
        if(dp.count({s,e})){
            return dp[{s,e}];
        }
         
+        
         for(int i = s ; i < e ; i++){
             
-            auto left = f(arr,s,i);
-            auto right = f(arr,i+1,e);
+            auto left = f(s , i , arr);
+            auto right = f(i+1 , e , arr);
             
-            leafMax = max(left.first,right.first);
+            
+            
+            leafMax =  max(left.first,right.first);
+            
             minSum = min(minSum , left.second + right.second + (left.first*right.first));
         }
         
-      
-        return dp[{s,e}] = {leafMax,minSum};
+        return dp[{s,e}] ={leafMax,minSum};
         
     }
     int mctFromLeafValues(vector<int>& arr) {
-       
-        int n = arr.size();
-     
-       return f(arr,0,arr.size()-1).second;
+        
+        return f(0 , arr.size()-1 ,arr).second;
+        
     }
 };
