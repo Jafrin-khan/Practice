1a5828d2851a24e035cf50540126aded4ee3a0bd
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-09 13:58:23 +0530

    Time: 115 ms (66.73%), Space: 69.3 MB (89.02%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -2,28 +2,34 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         
+        int totCost = 0;
+        int totPet = 0;
+        
         int n = gas.size();
         
-        int g = 0;
-        int c = 0;
-        
         for(int i = 0 ; i < n ; i++){
-            g += gas[i];
-            c += cost[i];
+            totCost += cost[i];
+            totPet += gas[i];
         }
         
-        if(c > g){
+        if(totCost > totPet){
             return -1;
         }
         
         int start = 0;
         int tank = 0;
+        
+  
         for(int i = 0 ; i < n ; i++){
+            
+            
             tank += gas[i] - cost[i];
+            
             if(tank < 0){
                 start = i+1;
                 tank = 0;
             }
+        
         }
         
         return start;
