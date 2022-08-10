a84dc4ab5c755c4ec8a33fd30d6b54e628b182f8
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-10 22:13:10 +0530

    Time: 280 ms (29.63%), Space: 171.4 MB (8.33%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     
+    vector<vector<string>> ans;
+    
     bool isValid(int start , int end , string s){
         
-        while(start < end){
+        while(start <= end){
             if(s[start++] != s[end--]){
                 return false;
             }
@@ -12,12 +14,10 @@ public:
         return true;
     }
     
-    void f(int ind , vector<vector<string>>& ans , vector<string> helper , string s){
+    void f(int ind , vector<string> helper , string s){
         
         if(ind == s.size()){
-            if(helper.size() > 0){
             ans.push_back(helper);
-            }
             return;
         }
         
@@ -25,18 +25,17 @@ public:
             
             if(isValid(ind , i , s)){
                 helper.push_back(s.substr(ind , i - ind + 1));
-                f(i+1 , ans , helper , s);
+                f(i+1 , helper , s);
                 helper.pop_back();
             }
+            
         }
-        
     }
-    
     vector<vector<string>> partition(string s) {
         
-        vector<vector<string>> ans;
         vector<string> helper;
-        f(0 , ans , helper , s);
+        
+        f(0 , helper , s);
         
         return ans;
         
