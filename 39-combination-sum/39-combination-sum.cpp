bc737bc01f2272209b372c424b5aedee7dc75c65
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-10 20:15:07 +0530

    Time: 258 ms (11.70%), Space: 71.4 MB (13.00%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
     
-      void f(int ind , vector<int>& nums , set<vector<int>> &ans , vector<int>helper , int target){
-           if(target == 0){
-            sort(helper.begin(),helper.end());
-            ans.insert(helper);
-            }
+    
+    void f(int ind , int target , vector<int>& nums, vector<int> helper , set<vector<int>>& st){
+        
+        if(target == 0){
+            sort(helper.begin() , helper.end());
+            st.insert(helper);
+            
+ 
+        }
         
         if(ind == nums.size()){
             return;
         }
         
-        //notpick
-        f(ind+1 , nums , ans , helper , target);
-        //pick
+        //notTaken
+        f(ind+1 , target , nums , helper , st);
+        //taken
         helper.push_back(nums[ind]);
         if(target >= nums[ind]){
-        f(ind, nums , ans , helper , target - nums[ind]);
+        f(ind , target - nums[ind] , nums , helper , st);
         }
         helper.pop_back();
+        
     }
-    
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         
+        vector<int> helper;
         vector<vector<int>> ans;
         set<vector<int>> st;
-        vector<int> helper;
         
-        f(0 , nums , st , helper , target);
+        f(0, target , nums , helper , st);
         
-        for(auto x : st){
-            ans.push_back(x);
+        for(auto it : st){
+            ans.push_back(it);
         }
         
         return ans;
+        
+        
+        
         
     }
 };
