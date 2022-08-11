c61ad5a1c3d5e84948757e955ff20f04a44a4a05
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-11 10:09:29 +0530

    Time: 32 ms (80.95%), Space: 6.4 MB (49.84%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -3,25 +3,21 @@ public:
     
     bool isPossible(int row , int col , char ch , vector<vector<char>>& board){
         
-        int n = board.size();
-        // int m = board[0].size();
+                int n = board.size();
         
         for(int i = 0 ; i < n ; i++){
             
-            //checking row
-            if(board[i][col] == ch){
-                return false;
-            }
+          if(board[row][i] == ch){
+              return false;
+          }
             
-            //checking col
-            if(board[row][i] == ch){
-                return false;
-            }
+          if(board[i][col] == ch){
+              return false;
+          }
             
-            //checking boxes
-            if(board[3*(row/3) + i/3][3*(col/3) + i%3] == ch){
-                return false;
-            }
+          if(board[3*(row/3) + i/3][3*(col/3) + i%3] == ch){
+              return false;
+          }
         }
         
         return true;
@@ -29,41 +25,38 @@ public:
     }
     
     bool solve(vector<vector<char>>& board){
-         int n = board.size();
-         int m = board[0].size();
         
-         for(int i = 0 ; i < n ; i++){
-             for(int j = 0 ; j < m ; j++){
-                 
-                  if(board[i][j] == '.'){
-                      for(char ch = '1' ; ch <= '9' ; ch++){
+        int n = board.size();
+        int m = board[0].size();
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            for(int j = 0 ; j < m ; j++){
+                
+                
+                 if(board[i][j] == '.'){        
+                for(char ch = '1' ; ch <= '9' ; ch++){
                     
-                         if(isPossible(i , j , ch , board)){
-                             board[i][j] = ch;
+                   
+                        if(isPossible(i , j , ch , board)){
+                            board[i][j] = ch;
+                            if(solve(board)) return true;
+                            board[i][j] = '.';
+                            
+                        }
+                    }
                      
-                         if(solve(board) == true){
-                             return true;
-                     }
-                     
-                     else{
-                         board[i][j] = '.';
-                        
-                     }
-                 }
-             }
-                      
-            return false;
-                          }
-                     }
-                     
-         }
+                return false;
+                }
+            }
+        }
         
         return true;
-        
     }
     void solveSudoku(vector<vector<char>>& board) {
         
         solve(board);
+        
         
     }
 };
