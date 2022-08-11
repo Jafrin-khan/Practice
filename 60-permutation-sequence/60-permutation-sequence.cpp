d0c03c08c58fe2ad159c574f2865ba84d3439e13
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-11 07:56:36 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (51.15%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -4,19 +4,21 @@ public:
         
         int fact = 1;
         vector<int> numbers;
+        
         for(int i = 1 ; i < n ; i++){
-            fact = fact*i;
             numbers.push_back(i);
+            fact = fact*i;
         }
         
         numbers.push_back(n);
         
         string ans = "";
-        k = k -1;
-        
+        k = k-1 ;// as zero based indexing
+            
         while(true){
+            
             ans = ans + to_string(numbers[k/fact]);
-            numbers.erase(numbers.begin()+k/fact);
+            numbers.erase(numbers.begin() + k/fact);
             
             if(numbers.size() == 0){
                 break;
@@ -24,6 +26,7 @@ public:
             
             k = k%fact;
             fact = fact/numbers.size();
+            
         }
         
         return ans;
