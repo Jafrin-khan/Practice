0bd67c309fadaadda42c3c6d0ddb83acacd010cb
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-12 15:53:16 +0530

    Time: 18 ms (22.67%), Space: 26.8 MB (42.08%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-      int dis2(vector<int>& p1, vector<int>& p2) {
-        return (p1[0] - p2[0])* (p1[0] - p2[0]) + (p1[1] - p2[1])* (p1[1] - p2[1]);
+    
+    int cal(vector<int>& p1, vector<int>& p2){
+        
+        int x = p2[0] - p1[0];
+        int y = p2[1] - p1[1];
+        
+        return x*x + y*y;
     }
-public:
-
+    
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        map<int, int> ss;
-
-        ss[dis2(p1, p2)]++;
-        ss[dis2(p1, p3)]++;
-        ss[dis2(p1, p4)]++;
-        ss[dis2(p3, p2)]++;
-        ss[dis2(p4, p2)]++;
-        ss[dis2(p3, p4)]++;
-
-        return ss.size() == 2 && (ss.begin()-> second == 4);
+        
+        map<int,int> mp;
+        
+        mp[cal(p1 , p2)]++;
+        mp[cal(p2 , p3)]++;
+        mp[cal(p3 , p4)]++;
+        mp[cal(p4 , p1)]++;
+        mp[cal(p1 , p3)]++;
+        mp[cal(p2 , p4)]++;
+        
+        if(mp.size() != 2){
+            return false;
+        }
+        
+        for(auto x : mp){
+            return  x.second == 4;
+        }
+        
+        return false;
+        
     }
 };
