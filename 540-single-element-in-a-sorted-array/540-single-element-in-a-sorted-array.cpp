c5f0e231d5f7b908dfa888e8fffd12ae1a9c17e0
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-13 11:10:55 +0530

    Time: 44 ms (46.69%), Space: 22.3 MB (91.64%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -6,6 +6,7 @@ public:
             return nums[0];
         }
         
+        
         int low = 0;
         int high = nums.size()-1;
         
@@ -13,18 +14,30 @@ public:
             
             int mid = (low + high)/2;
             
-            if(nums[mid] == nums[mid^1]){
-                low = mid+1;
+            if(mid%2 == 0){
+                if(nums[mid+1] == nums[mid]){
+                    low = mid+1;
+                }
+                
+                else{
+                    high = mid-1;
+                }
             }
             
             else{
-                high = mid-1;
+                 if(nums[mid-1] == nums[mid]){
+                    low = mid+1;
+                }
+                
+                else{
+                    high = mid-1;
+                }
             }
-            
             
         }
         
         return nums[low];
+        
         
     }
 };
