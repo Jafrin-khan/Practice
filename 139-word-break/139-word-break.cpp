49937b2ea8284e09304cc344ddf6e1d53e647020
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-14 09:09:02 +0530

    Time: 43 ms (21.47%), Space: 14.7 MB (20.76%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    bool f(string s, vector<string>& d , unordered_map<string,bool>& dp){
-            
-        if(find(d.begin() , d.end() , s) != d.end()){
+    bool f(string s, vector<string>& wordDict ,  unordered_map<string , bool>& dp){
+        
+        if(find(wordDict.begin() , wordDict.end() , s) != wordDict.end()){
             return true;
         }
         
@@ -11,23 +11,24 @@ public:
             return dp[s];
         }
         
+        if(s.size() == 0){
+            return false;
+        }
         for(int i = 1 ; i < s.size() ; i++){
+            string temp = s.substr(0 , i);
             
-             string temp = s.substr(0,i);
-             if(find(d.begin() , d.end(),temp) != d.end() && f(s.substr(i) , d , dp)){
-                 return dp[s] =  true;
-             }
-            
-            
+            if(find(wordDict.begin() , wordDict.end() , temp) != wordDict.end() && f(s.substr(i) , wordDict , dp)){
+                return dp[s] = true;
+            }
         }
         
-         return dp[s] = false;
+        return dp[s] = false;
     }
-    bool wordBreak(string s, vector<string>& d) {
+    bool wordBreak(string s, vector<string>& wordDict) {
         
-        //here we have done memoization using map
-        unordered_map<string,bool> dp;
-        return f(s , d , dp);
+        unordered_map<string , bool> dp;
+        return f(s , wordDict , dp);
+        
         
     }
 };
