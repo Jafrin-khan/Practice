a0d6ec996a53c2c79dab47790cbc43b9ee170101
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-15 14:42:17 +0530

    Time: 7 ms (86.26%), Space: 13 MB (78.80%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -8,54 +8,60 @@ public:
         
         int n = grid.size();
         int m = grid[0].size();
-        
         int tot = 0;
-        queue<pair<int,int>> rotten;
-        int cnt = 0;
+        
+        queue<pair<int,int>> q;
         
         for(int i = 0 ; i < n ; i++){
+            
             for(int j = 0 ; j < m ; j++){
                 if(grid[i][j] == 2){
-                    rotten.push({i,j});
+                    q.push({i , j});
                 }
                 
                 if(grid[i][j] != 0){
                     tot++;
                 }
+                
             }
         }
+        
         int mins = 0;
         
-        int dx[4] = {0 , 0 , 1 ,-1};
-        int dy[4] = {1, -1 , 0 , 0};
-        
-        while(!rotten.empty()){
+        int dx[4] = {0 , 0 , 1 , -1};
+        int dy[4] = {1 , -1 , 0 , 0};
+        int cnt = 0;
+        while(!q.empty()){
             
-            int k = rotten.size();
+            int k = q.size();
             cnt += k;
             
             while(k--){
-            int x = rotten.front().first;
-            int y = rotten.front().second;
-             rotten.pop();
-            for(int i = 0 ; i < 4 ; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
                 
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m || grid[nx][ny] != 1){
-                    continue;
+                int x = q.front().first;
+                int y = q.front().second;
+                
+                q.pop();
+                
+                for(int i = 0 ; i < 4 ; i++){
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    
+                    if(nx < 0 || ny < 0 || nx >= n || ny >= m || grid[nx][ny] != 1){
+                        continue;
+                    }
+                    
+                    grid[nx][ny] = 2;
+                    q.push({nx , ny});
                 }
-                
-                grid[nx][ny] = 2;
-                rotten.push({nx,ny});
             }
-           
-        }
             
-            if(!rotten.empty()){
+            if(!q.empty()){
                 mins++;
             }
-        } 
-                return tot == cnt ? mins : -1;
+        }
+        
+        return cnt == tot ? mins : -1;
+        
     }
 };
