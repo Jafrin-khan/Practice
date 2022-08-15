63a04712d2562606be7b273a48efe2bae9a3fd9f
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-15 13:36:18 +0530

    Time: 304 ms (95.10%), Space: 134.8 MB (42.01%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -7,22 +7,21 @@ public:
         
         for(int i = 0 ; i < nums.size() ; i++){
             
-            //sliding the window
             if(!dq.empty() && dq.front() == i-k){
                 dq.pop_front();
             }
             
-            while(!dq.empty() && nums[i] >= nums[dq.back()]){
+            while(!dq.empty() && nums[i] > nums[dq.back()]){
                 dq.pop_back();
             }
             
             dq.push_back(i);
-            
-            if(i>=k-1){
+                
+            if(i >= k-1){
                 ans.push_back(nums[dq.front()]);
             }
         }
-        
+                              
         return ans;
         
     }
