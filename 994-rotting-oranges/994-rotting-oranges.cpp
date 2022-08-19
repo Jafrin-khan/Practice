3a0c0df98a87d41307a1c61004927b4929896da9
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-19 17:13:48 +0530

    Time: 18 ms (23.19%), Space: 13 MB (79.16%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
+    
+    int dx[4] = {0 , 0 , -1 , 1};
+    int dy[4] = {-1 , 1 , 0 , 0};
+    
     int orangesRotting(vector<vector<int>>& grid) {
         
+          
          if(grid.size() == 0){
             return 0;
         }
         
+        
         int n = grid.size();
         int m = grid[0].size();
-        int tot = 0;
         
         queue<pair<int,int>> q;
+        int tot = 0;
         
         for(int i = 0 ; i < n ; i++){
-            
             for(int j = 0 ; j < m ; j++){
+                
                 if(grid[i][j] == 2){
                     q.push({i , j});
                 }
@@ -22,46 +28,41 @@ public:
                 if(grid[i][j] != 0){
                     tot++;
                 }
-                
             }
         }
         
-        int mins = 0;
+        int k = 0;
+        int mins = -1;
         
-        int dx[4] = {0 , 0 , 1 , -1};
-        int dy[4] = {1 , -1 , 0 , 0};
-        int cnt = 0;
         while(!q.empty()){
             
-            int k = q.size();
-            cnt += k;
+            int currSize = q.size();
+            k+=currSize;
+            mins++;
             
-            while(k--){
-                
+            while(currSize--){
                 int x = q.front().first;
                 int y = q.front().second;
-                
                 q.pop();
-                
+
                 for(int i = 0 ; i < 4 ; i++){
                     int nx = x + dx[i];
                     int ny = y + dy[i];
-                    
-                    if(nx < 0 || ny < 0 || nx >= n || ny >= m || grid[nx][ny] != 1){
-                        continue;
+
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 1){
+                        grid[nx][ny] = 2;
+                        q.push({nx , ny});
                     }
-                    
-                    grid[nx][ny] = 2;
-                    q.push({nx , ny});
-                }
+            }
             }
             
-            if(!q.empty()){
-                mins++;
-            }
         }
         
-        return cnt == tot ? mins : -1;
+        if(mins == -1){
+            mins = 0;
+        }
+        
+        return   k == tot?mins : -1;
         
     }
 };
