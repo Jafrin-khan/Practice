c949c274827d32b8a420ebae31e965e300e030a2
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-08-24 13:52:01 +0530

    Time: 13 ms (34.98%), Space: 5.9 MB (78.79%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int fib(int n) {
     
-        if(n <= 0){
-            return 0;
-        }
+    int f(int n){
         
-        if(n == 1 || n == 2){
+        if(n == 1){
             return 1;
         }
         
+        if(n == 0){
+            return 0;
+        }
         
-       return fib(n-1) + fib(n-2);
+        return f(n-1) + f(n-2);
+    }
+    int fib(int n) {
+        
+        return f(n);    
     }
 };
