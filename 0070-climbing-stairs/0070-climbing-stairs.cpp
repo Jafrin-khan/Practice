55ec93c421d90f233b7a548c219192c6eff6d10a
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-11-10 18:41:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (30.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,11 +7,11 @@ public:
     
     Time Complexity: O(N)
 
-Reason: The overlapping subproblems will return the answer in constant time O(1). Therefore the total number of new subproblems we solve is ‘n’. Hence total time complexity is O(N).
+    Reason: The overlapping subproblems will return the answer in constant time O(1). Therefore the total number of new subproblems we solve    is ‘n’. Hence total time complexity is O(N).
 
-Space Complexity: O(N)
+    Space Complexity: O(N)
 
-Reason: We are using a recursion stack space(O(N)) and an array (again O(N)). Therefore total space complexity will be O(N) + O(N) ≈ O(N)
+    Reason: We are using a recursion stack space(O(N)) and an array (again O(N)). Therefore total space complexity will be O(N) + O(N) ≈ O(N)
     */
 
     int f(int n , vector<int>& dp){
