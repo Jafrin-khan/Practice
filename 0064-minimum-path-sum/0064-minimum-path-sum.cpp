3f36ab9ae16171542938a8042adacb35dd3eb8d7
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-11-11 09:38:44 +0530

    Time: 16 ms (70.57%), Space: 10.2 MB (31.46%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,39 +2,40 @@ class Solution {
 public:
     
     /*TC : O(m*n)
-    SC :O((M-1)+(N-1)) + O(N*M)
-
-Reason: We are using a recursion stack space:O((M-1)+(N-1)), here (M-1)+(N-1) is the path length and an external DP Array of size ‘N*M’.
+    SC : O(N*M)
     */
-    
-    int f(int i , int j , vector<vector<int>>& grid , vector<vector<int>>& dp){
-        
-        if(i == 0 && j == 0){
-            return grid[0][0];
-        }
-        
-        if(i < 0 || j < 0){
-            return 1e9;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-       
-        int left = grid[i][j] + f(i , j-1 , grid , dp);
-        int up = grid[i][j] + f(i-1 , j , grid , dp);
-        
-        return dp[i][j] = min(left , up);
-    }
-    
+  
     int minPathSum(vector<vector<int>>& grid) {
         
         int m = grid.size();
         int n = grid[0].size();
         
-        vector<vector<int>> dp(m , vector<int>(n , -1));
+        vector<vector<int>> dp(m , vector<int>(n , 0));
         
-        return f(m-1 , n-1 , grid , dp);
+        dp[0][0] = grid[0][0];
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                
+                if(i == 0 && j == 0){
+                    continue;
+                }
+                int left = 1e9;
+                int up = 1e9;
+                
+                if(j >= 1){
+                left = grid[i][j] + dp[i][j-1];
+                }
+                
+                if(i >= 1){
+                up = grid[i][j] + dp[i-1][j];
+                }
+                dp[i][j] = min(left , up);
+            }
+        }
+
+        
+        return dp[m-1][n-1];
         
     }
 };
