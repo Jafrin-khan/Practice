77d1f4db064746de43d696fbf2a809d8567a7b78
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-11-11 08:36:31 +0530

    Time: 4 ms (30.14%), Space: 6.6 MB (29.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     
-    int f(int i , int j , int m , int n , vector<vector<int>>& dp){
-        
-        if(i < 0 || j < 0 || i >= m || j >= n){
-            return 0;
-        }
-        
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        int left = f(i , j-1 , m , n , dp);
-        int up = f(i-1 , j , m , n , dp);
-        
-        return dp[i][j] = left + up;
-    }
+    /*
+    TC : O(m*n)
+    SC : O(m*n)     ..........Reason: We are using an external array of size ‘M*N’’.
     
+    
+    */
+
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(m+1 , vector<int>(n+1 , -1));
-        return f(m-1 , n-1 , m , n , dp);
+        vector<vector<int>> dp(m , vector<int>(n , 0));
+        
+        dp[0][0] = 1;
+       
+        for(int i = 0 ; i < m ; i++){
+            
+            for(int j = 0 ; j < n ; j++){
+                
+                if(i == 0 && j == 0){
+                    continue;
+                }
+                
+                 int left = 0;
+                 int right = 0;
+            
+                
+                if(j >= 1){
+                left = dp[i][j-1];
+                }
+                
+                if(i >= 1){
+                right = dp[i-1][j];
+                }
+
+                dp[i][j] = left + right;
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
