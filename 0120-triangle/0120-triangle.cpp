9c12be39667d19e4ea52df905ea7f6a68a923b93
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-11-13 00:21:04 +0530

    Time: 17 ms (27.82%), Space: 9.1 MB (32.03%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     
-   int minimumTotal(vector<vector<int>>& triangle) {
+    /*Time Complexity: O(N*N)
+
+Reason: At max, there will be (half of, due to triangle) N*N calls of recursion.
+
+Space Complexity: O(N) + O(N*N)
+
+Reason: We are using a recursion stack space: O((N), where N is the path length and an external DP Array of size ‘N*N’.*/
+    
+    int solve(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp) {
+		if(i == n - 1)
+			return triangle[i][j];
+		if(dp[i][j] != -1)
+			return dp[i][j];
+		int bottom = triangle[i][j] + solve(i+1, j, n, triangle, dp);
+		int  bottom_right = triangle[i][j] + solve(i+1, j+1, n, triangle, dp);
+		return dp[i][j] = min(bottom, bottom_right);
+	}
+	int minimumTotal(vector<vector<int>>& triangle) {
 		int n = triangle.size();
 		vector<vector<int>>dp(n, vector<int>(n, -1));
-		for(int i = 0; i < n; i++) 
-			dp[n-1][i] = triangle[n-1][i];
-       
-       
-		for(int i = n-2; i >= 0; i--) {
-			for(int j = i; j >= 0; j--) {
-				int up = triangle[i][j] + dp[i+1][j];
-				int up_left = triangle[i][j] + dp[i+1][j+1];
-				dp[i][j] = min(up, up_left);
-			}
-		}
-		return dp[0][0];
+		return solve(0, 0, n, triangle, dp);
 	}
    
    
