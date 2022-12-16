cefb4e990679d321345fdc88bea13bcc8d25476e
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-12-16 15:44:50 +0530

    Time: 8 ms (80.28%), Space: 10.5 MB (65.68%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    /*
+    TC = O(N)
+    SC = O(1)
+    */
+    int minOperations(vector<string>& logs) {
+        
+        int cnt = 0;
+        
+        for(int i = 0 ; i < logs.size() ; i++){
+            
+            if(logs[i] == "./"){
+                continue;
+            }
+            
+            else if(logs[i] == "../"){
+                if(cnt > 0){
+                    cnt--;
+                }
+            }
+            
+            else{
+                cnt++;
+            }
+        }
+        
+        return cnt;
+        
+    }
+};
