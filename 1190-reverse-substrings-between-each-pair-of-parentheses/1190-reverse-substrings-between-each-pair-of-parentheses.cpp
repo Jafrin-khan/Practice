84cf2be54751107eb23a722c33ea61a0694a1f0e
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-12-17 14:49:16 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (53.31%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    
+    /*
+    TC = O(N)...check from friends once though 90% sure its correct
+    SC = O(N) + O(N) ~ O(N) 
+    */
+    string reverseParentheses(string s) {
+        
+        stack<char> st;
+        queue<char> q;
+        
+        string ans = "";
+        
+        for(int i = 0 ; i < s.size() ; i++){
+            
+            if(s[i] == '('){
+                st.push(s[i]);
+            }
+            
+            else if(s[i] != ')' && !st.empty()){
+                st.push(s[i]);
+            }
+            
+            else if(s[i] != ')' && st.empty()){
+                ans += s[i];
+            }
+         
+            else{
+                
+                while(st.top() != '('){
+                    q.push(st.top());
+                    st.pop();
+                }
+        
+                st.pop();
+                
+                if(st.empty()){
+                    
+                    while(!q.empty()){
+                    ans += q.front();
+                    q.pop();
+                    }
+                }
+               
+                else{
+                    while(!q.empty()){
+                    st.push(q.front());
+                    q.pop();
+                    }
+                }
+                
+                
+            }
+        }
+        
+        while(!st.empty()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+        
+        return ans;
+        
+    }
+};
