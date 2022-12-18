342a96e079462a4f19bc5ace0d56cb3f80ba58a0
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2022-12-18 17:02:15 +0530

    Time: 26 ms (90.09%), Space: 13.6 MB (59.20%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,58 +1,54 @@
-class Solution {
+class Solution{
 public:
     
     /*
-    TC = O(V + 2E)..... Where V = Vertices, 2E is for total degrees as we traverse all adjacent nodes.
-    SC = O(3VO) ~ (V)....., Space for queue data structure, colour array and an adjacency list.
+    TC = O(V+2E)
+    SC = O(3V) ~ O(V)
     */
     
     /*
-    BFS
+    This is DFS
+    BFS wala bhi h iska prev accepted ans 
     */
     
-    bool check(int start , vector<int>& color , vector<vector<int>>& adj){
-        queue<int> q;
-        
-        q.push(start);
-        color[start] = 0;
-        
-        while(!q.empty()){
+    bool dfs(int node , vector<vector<int>>& graph , vector<int>& color , int col){
             
-            int node = q.front();
-            q.pop();
+        for(auto it : graph[node]){
             
-            for(auto it : adj[node]){
-                
-                if(color[it] == -1){
-                    color[it] = !color[node];
-                    q.push(it);
-                }
-                
-                else{
-                    if(color[it] == color[node]){
-                        return false;
-                    }
+            if(color[it] == -1){
+                color[it] = !col;
+                if(!dfs(it , graph , color , color[it])){
+                    return false;
                 }
             }
-        }
+            
+            else{
+                if(color[it] == col){
+                    return false;
+                }
+            }
+             
+        }   
         
         return true;
         
     }
-    bool isBipartite(vector<vector<int>>& adj) {
+    
+    bool isBipartite(vector<vector<int>>& graph) {
         
-        vector<int> color(adj.size() , -1);
-        //for loop for considering non connected components ,i.e, separate graphs as well
-        for(int i = 0 ; i < adj.size() ; i++){
-            
+        int n = graph.size();
+        int m = graph[0].size();
+        
+        vector<int> color(n , -1);
+        
+        for(int i = 0 ; i < n ; i++){
             if(color[i] == -1){
-                if(!check(i , color , adj)){
+                if(!dfs(i , graph , color , color[i])){
                     return false;
                 }
             }
         }
         
         return true;
-       
     }
 };
