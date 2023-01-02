2e8907c96a147ebb7760a3389cb594045edca7f9
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-02 18:47:12 +0530

    Time: 7 ms (98.78%), Space: 15.9 MB (83.35%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    
+    //Moore's algo
+    
+    /*
+    TC = O(N)
+    */
+    
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        int ele1 = -1e9;
+        int ele2 = -1e9;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        
+        for(auto x : nums){
+            
+            if(ele1 == x){
+                cnt1++;
+            }
+            
+            else if(ele2 == x){
+                cnt2++;
+            }
+            
+            else if(cnt1 == 0){
+                ele1 = x;
+                cnt1 = 1;
+            }
+            
+            else if(cnt2 == 0){
+                ele2 = x;
+                cnt2 = 1;
+            }
+            
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        
+        cnt1 = 0;
+        cnt2 = 0;
+        
+         for(auto x : nums){
+            if(x == ele1){
+                cnt1++;
+            }
+            
+            if(x == ele2){
+                cnt2++;
+            }
+        }
+        
+        vector<int> ans;
+        int n = nums.size()/3;
+        
+        if(cnt1 > n){
+            ans.push_back(ele1);
+        }
+        
+        if(cnt2 > n){
+            ans.push_back(ele2);
+        }
+        
+        return ans;
+        
+    }
+};
