d2ffcf1af0b51c1007829af91519daac139ed86c
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-08 12:29:32 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (30.22%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -3,6 +3,9 @@ public:
     
     /*
     Just an algorithm that you should know...remember we can use this algo as it is written " permutation of the integers in the range [0, n - 1]"
+    
+    see this problem for real algo :  Max Chunks To Make Sorted II
+    
     TC = O(N)
     SC = O(1)
     */
