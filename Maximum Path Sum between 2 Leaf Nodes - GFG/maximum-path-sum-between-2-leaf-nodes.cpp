55bdf8a2d9ca1e07b015b99013278166a70fcedd
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-12 19:38:20 +0530

    Added solution - LeetHub

---
+++ Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.cpp
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    /*
+    TC = O(n)
+    SC = O(n)
+    */
+    int f(Node* root , int& ans){
+        
+        if(root == NULL){
+            return -1e9;
+        }
+        
+        if(root->left == NULL && root->right == NULL){
+            return root->data;
+        }
+        
+        int sL = f(root->left , ans);
+        int sR = f(root->right , ans);
+        
+        if(root->left && root->right){
+            ans = max(ans , sL+sR+root->data);
+        }
+        
+        return max(sL , sR) + root->data;
+        
+    }
+    
+    int maxPathSum(Node* root)
+    {
+        // code here
+        
+        if(root == NULL){
+            return 0;
+        }
+        
+        int maxSum = -1e9;
+        int toCompare = f(root , maxSum);
+        
+        //why this if statement ncluded find out
+        if(root->left == NULL || root->right == NULL){
+            return max(toCompare , maxSum);
+        }
+        
+        return maxSum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution ob;
+        cout << ob.maxPathSum(root) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
