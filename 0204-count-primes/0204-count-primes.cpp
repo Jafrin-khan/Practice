12d7ba24dfc4e379f3236d9f735f6ac47ed6ed1f
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-19 13:55:25 +0530

    Time: 183 ms (91.40%), Space: 10.2 MB (94.11%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    //https://www.youtube.com/watch?v=5LMkddl2NCk
+    
+    
+    int countPrimes(int n) {
+        
+        if(n <= 2){
+            return 0;
+        }
+        
+        vector<bool> composites(n , false);
+        int limit = int(sqrt(n));
+        int cnt = 0;
+        
+        for(int i = 2 ; i <= limit ; i++){
+            if(composites[i] == false){
+            for(int j = i*i ; j < n ; j+=i){
+                composites[j] = true;
+            }
+        }
+        }
+        for(int i = 2 ; i < n ; i++){
+            if(composites[i] == false){
+                cnt++;
+            }
+        }
+        
+        return cnt;
+        
+    }
+};
