0578c1dc9efbf5fa7d389423ceeda24a4c73aa4f
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-20 22:26:11 +0530

    Time: 3 ms (36.38%), Space: 6.2 MB (30.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,9 +1,48 @@
 class Solution {
 public:
+    /*
+     //Recursive
+    int f(int n){ 
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return 1;
+        }
+        int one = f(n-1);
+        int two = f(n-2);
+        return one + two;
+    }
+    
+    int climbStairs(int n) {
+        return f(n);
+    }*/
+    
+    //Memoised
+    int f(int n , vector<int>& dp){
+        
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int one = f(n-1 , dp);
+        int two = f(n-2 , dp);
+        return dp[n] = one + two;
+    }
+    
+    int climbStairs(int n) {
+        vector<int> dp(n+1 , -1);
+        return f(n , dp);
+    }
     
     //Tabulated
    
-    int climbStairs(int n) {
+   /* int climbStairs(int n) {
         vector<int> dp(n+1 , 0);
         dp[0] = 1;
         dp[1] = 1;
@@ -16,5 +55,5 @@ public:
         }
         
         return dp[n];
-    }
+    }*/
 };
