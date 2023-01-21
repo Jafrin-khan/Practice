1fb659a151bf4a5db00570e18c3707c0b4aae129
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-21 13:25:38 +0530

    Time: 921 ms (34.79%), Space: 26.6 MB (10.37%) - LeetHub

---
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    //https://www.youtube.com/watch?v=jZcAldZP1ag
+    
+    int threeSumMulti(vector<int>& arr, int target) {
+        
+        int n = arr.size();
+        int ans= 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            vector<int> cnt(101 , 0);
+            for(int j = i+1 ; j < n ; j++){
+                int k = target - arr[j] - arr[i];
+                if(k>=0 && k <= 100 && cnt[k]>0){
+                    ans += cnt[k];
+                    ans = ans%1000000007;
+                }
+                cnt[arr[j]]++;
+            }
+        }
+        
+        return ans;
+    }
+};
