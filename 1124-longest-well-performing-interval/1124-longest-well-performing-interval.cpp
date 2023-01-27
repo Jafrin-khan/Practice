0ba724f3eaa3b01901b1c3fd4200ed4a62dcb8aa
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-27 16:46:29 +0530

    Time: 43 ms (54.64%), Space: 23.1 MB (62.86%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.cpp
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     
-    //simple sliding window
+    /*
+    to find any consecutive sum or consecution 1s we use map in this fashion.....v.imp trick
+    TC = O(N)
+    
+    */
     int longestWPI(vector<int>& hours) {
         
-        int ans = 0;
-        int cnt = 0;
-        int n = hours.size();
-        
-        for(int i = 0 ; i < n ; i++){
-            if(hours[i] > 8)cnt++;
-            if(cnt*2 > i+1)ans = i+1;
-            int currCnt = cnt;
-            int left = 0;
-            while(left < i && 2*currCnt <= i - left + 1){
-                if(hours[left] > 8) currCnt--;
-                 left++;
-                if(2*currCnt > i - left + 1) ans = max(ans , i-left+1);
+        int n=hours.size();
+        unordered_map<int,int>m;
+        int res=0,sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(hours[i] > 8) sum++;
+            else sum--;
+            if(sum>0)
+            {
+                res=i+1;
+            }
+            if(sum<=0)
+            {
+                if(m.find(sum-1)!=m.end())
+                {
+                     int l=i-m[sum-1];//interval
+                     res=max(res,l);
+                }
+                if(m.find(sum)==m.end())
+                    m[sum]=i;
             }
         }
-        
-        return ans;
+        return res;
        
     }
 };
