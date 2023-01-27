d8185013406481b41be31e9ccda74846d6678f19
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-27 21:12:10 +0530

    Time: 2 ms (92.83%), Space: 8.2 MB (96.84%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        
+        int n = answers.size();
+        sort(answers.begin() , answers.end());
+        int i = 0;
+        int cnt = 0;
+        
+        while(i < n){
+            int curr = answers[i];
+            int j = i+1;
+            while(j < n && curr &&  answers[i] == answers[j]){
+                j++;
+                curr--;
+            }
+            cnt += curr;
+            i = j;
+        }
+        cnt+=answers.size();
+        return cnt;
+    }
+};
