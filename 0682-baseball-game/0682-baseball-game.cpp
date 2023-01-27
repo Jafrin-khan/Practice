5cdcd89a5643355280fb9fe275f7902db6f017fd
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-27 17:14:36 +0530

    Time: 4 ms (81.04%), Space: 8.5 MB (31.86%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int calPoints(vector<string>& s) {
+        
+        int ans = 0;
+        int n = s.size();
+        stack<int> st;
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            if(s[i] == "+"){
+                int top1 = st.top();st.pop();
+                int top2 = st.top();st.push(top1);
+                st.push(top1+top2);
+            }
+            else if(s[i] == "D"){
+                st.push(st.top()*2);
+            }
+            else if(s[i] == "C"){
+                int top = st.top();
+                st.pop();
+            }
+            else{
+                int num = std:: stoi(s[i]);
+                st.push(num);
+            }
+           
+        }
+        
+        while(!st.empty()){
+            ans+=st.top();st.pop();
+        } 
+        
+        return ans;
+        
+    }
+};
