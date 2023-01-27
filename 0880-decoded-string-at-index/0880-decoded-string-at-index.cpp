db1018ce50c836d93fb2af6a53d26a1e3dba0ee3
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-27 15:30:14 +0530

    Time: 6 ms (7.58%), Space: 6.1 MB (31.82%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    /*
+    https://www.youtube.com/watch?v=6iE7QJRMXzs
+    */
+    
+    
+    string decodeAtIndex(string s, int k) {
+        
+        int n = s.size();
+        long size = 0;
+        
+        //calculating the size;
+        for(int i = 0 ; i < n ; i++){
+            if(isdigit(s[i])) size = size*(s[i]-'0');
+            else size++;
+        }
+        
+        for(int i = n-1 ; i >= 0 ; i--){
+            //in case when k goes out of bound
+            k %= size;
+            if((k == 0 || k == size) && !isdigit(s[i])){
+                string ans = "";
+                ans+=s[i];
+                return ans;
+            }
+            
+            else if(isdigit(s[i])) size/=(s[i]-'0');
+            else size--;
+        }
+        
+        return "";
+    }
+    
+};
