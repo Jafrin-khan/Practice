b30f9642b1fbfc87fd50972d46311833f1624839
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-29 14:51:58 +0530

    Added solution - LeetHub

---
+++ Check if array contains contiguous integers with duplicates allowed - GFG/check-if-array-contains-contiguous-integers-with-duplicates-allowed.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// C++ implementation to check whether the array
+// contains a set of contiguous integers
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    // Function to check whether the array contains
+    // a set of contiguous integers
+    bool areElementsContiguous(int arr[], int n)
+    {
+	   sort(arr , arr + n);
+	   
+	   for(int i = 0 ; i < n-1 ; i++){
+	       if(arr[i+1] - arr[i] > 1){
+	           return false;
+	       }
+	   }
+	   
+	   return true;
+	   
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n + 1];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        if (ob.areElementsContiguous(arr, n))
+            cout << "Yes" <<endl;
+        else
+            cout << "No" <<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
