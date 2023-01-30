c41f7d502df19ea830f0f55d744b003bec6b0605
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-01-30 20:33:29 +0530

    Added solution - LeetHub

---
+++ Find distinct elements - GFG/find-distinct-elements.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int distinct(vector<vector<int>> matrix, int n)
+    {
+        unordered_map<int,int> mp;
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(mp[matrix[i][j]] == i){
+                 mp[matrix[i][j]]++;
+                }
+        }
+        }
+        
+        int ans = 0;
+        for(auto it : mp){
+            if(it.second == n)ans++;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<vector<int>> M(N, vector<int>(N, 0));
+        for(int i = 0;i < N*N; i++)
+            cin>>M[i/N][i%N];
+        
+        Solution ob;
+        cout<<ob.distinct(M, N)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
