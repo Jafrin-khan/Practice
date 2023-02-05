7c6c6bc2bbc82d68d8fc8a40cc6ce16de1ee47ef
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-02-05 22:52:24 +0530

    Time: 7 ms (99.97%), Space: 20.4 MB (30.44%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -12,19 +12,20 @@
 class Solution {
 public:
     /*
-    TC = O(NlogN)
+    
+    TC = O(N^2)
     SC = O(N)
-    */
+    
     void f(TreeNode* root , long long k , int& cnt){
         
         if(root == NULL) return;
         if(root->val == k){
             cnt++;
-            //dont return here consider this eg test case 
-           /* [1,-2,-3,1,3,-2,null,-1]
-              -1
-              here the ans should be 4 but it gives 3 hence we dont need to return from here
-            */
+           //dont return here consider this eg test case 
+           //[1,-2,-3,1,3,-2,null,-1]
+           //   -1
+           //   here the ans should be 4 but it gives 3 hence we dont need to return from here
+           // 
         } 
         
         f(root->left , k - root->val , cnt);
@@ -47,5 +48,43 @@ public:
         
        return cnt;
         
+    }*/
+    
+    /*
+    similar to find no. of subarray with sum equal to k
+    TC = O(N)
+    SC = O(1)
+    
+     //https://www.youtube.com/watch?v=yyZA4v0x16w
+    //similar to subarray with sum k,TC-O(N),SC-O(N)
+    */
+    
+    int count = 0;
+     void f(TreeNode* root, int targetSum,long sum,unordered_map<long,int> &mp){
+        
+        if(root==NULL)return;
+        
+        //operation on root node
+        sum+=root->val;
+        if(sum==targetSum)count++;
+        if(mp.find(sum-targetSum)!=mp.end())count+=mp[sum-targetSum];
+        mp[sum]++;
+        
+        //left and right node
+        f(root->left,targetSum,sum,mp);
+        f(root->right,targetSum,sum,mp);
+        
+        //backtrack
+        if(mp[sum]>1)mp[sum]--;
+        else mp.erase(sum);
+    }
+     int pathSum(TreeNode* root, int targetSum) {
+      
+        if(root==NULL)return 0;
+        
+        unordered_map<long,int> mp;
+        long sum=0;
+        f(root,targetSum,sum,mp);
+        return count;
     }
 };
