7dbc5c9d4b5a4e25d1b068af23b413c5d991cc6c
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-02-07 21:02:28 +0530

    Time: 25 ms (97.08%), Space: 26 MB (33.99%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     /*
-    TC = O(N) + O(NlogN) ~ O(NlogN)
+    TC = O(N)
     SC = O(1)
     */
     vector<int> sortedSquares(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> ans;
+        vector<int> ans(n);
         
         int i = 0;
         int j = n-1;
-        
+        int ind = n-1;
         while(i <= j){
-            if(pow(nums[i],2) >= pow(nums[j],2)) ans.push_back(pow(nums[i++],2));
-            else ans.push_back(pow(nums[j--],2));
+            if(pow(nums[i],2) >= pow(nums[j],2)) ans[ind--] = (pow(nums[i++],2));
+            else ans[ind--] = (pow(nums[j--],2));
         }
-        
-        reverse(ans.begin(),ans.end());
+       
         return ans;
     }
 };
