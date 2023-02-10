7adfba91d5229a8120f00da2d665b1d4a0df3f49
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-02-10 17:56:02 +0530

    Time: 50 ms (41.91%), Space: 17.6 MB (96.05%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,56 +10,68 @@
  */
 class Solution {
 public:
-
     /*
-    TC = O(n/2) + O(n/2) + O(n/2)
+    TC = O(N/2) + O(N/2) + O(N) ~ O(N)
     SC = O(1)
     */
-    ListNode* middle(ListNode* head){
+    
+    ListNode* reverse(ListNode* head){
+        
+        if(head == NULL) return NULL;
+        
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        
+        while(curr != NULL){
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        
+        return prev;
+    }
+    
+    ListNode* findMid(ListNode* head){
+        
+        if(head == NULL) return NULL;
         ListNode* slow = head;
         ListNode* fast = head;
+        
         while(fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
-        return slow;
+       return slow;
     }
-    ListNode* reverse(ListNode* head){
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-
-        while(curr != NULL){
-            ListNode* nextPtr = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextPtr;
-        }
-        return prev;
-    }
+    
     void reorderList(ListNode* head) {
-
-          ListNode* m = middle(head);
-          ListNode* t2 = m->next;
-          m->next = NULL;
-
-          ListNode* t1 = head;
-          t2 = reverse(t2);
-
-          ListNode* n1;
-          ListNode* n2;
-
-          while(t1 != NULL && t2 != NULL){
-
-              n1 = t1->next;
-              t1->next = t2;
-
-              n2 = t2->next;
-              t2->next = n1;
-
-              t1 = n1;
-              t2 = n2;
-
-          }
         
+        if(head == NULL) return;
+        ListNode* mid =findMid(head);
+        ListNode* t2 = mid->next;
+        mid->next = NULL;
+        
+        ListNode* t1 = head;
+        t2 = reverse(t2);
+        
+        ListNode* ans = new ListNode(0);
+        ListNode* tail = ans;
+        
+        while(t1 || t2){
+            if(t1){
+            ans->next = t1;
+            ans = ans->next;
+            t1 = t1->next;
+            }
+            
+            if(t2){
+            ans->next = t2;
+            ans = ans->next;
+            t2 = t2->next;
+            }
+        }
+        
+        head =  ans->next;
     }
 };
