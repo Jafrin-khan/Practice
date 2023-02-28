b2d48be7d6c4c5dc3efa79db15e716ff4db9538e
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-02-28 10:33:53 +0530

    Time: 34 ms (27.99%), Space: 12.2 MB (56.89%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -12,12 +12,11 @@ public:
         if(i < 0 || j < 0) return 0;
         
         if(s1[i] == s2[j]) return 1 + lcs(i-1 , j-1 , s1 , s2);
-        
-        int a = lcs(i-1,j-1,s1,s2);
+   
         int b = lcs(i-1,j,s1,s2);
         int c = lcs(i,j-1,s1,s2);
         
-        return max({a,b,c});
+        return max({b,c});
         
     }
     int minDistance(string word1, string word2) {
@@ -36,6 +35,7 @@ public:
     TC = O(N*M)
     SC = O(N*M) + O(N*M)
     */
+    /*
     int lcs(int i , int j , string s1 , string s2,vector<vector<int>>& dp){
         
         if(i < 0 || j < 0) return 0;
@@ -44,7 +44,6 @@ public:
         
         if(s1[i] == s2[j]) return dp[i][j] = 1 + lcs(i-1 , j-1 , s1 , s2,dp);
         
-      //  int a = lcs(i-1,j-1,s1,s2,dp);
         int b = lcs(i-1,j,s1,s2,dp);
         int c = lcs(i,j-1,s1,s2,dp);
         
@@ -61,5 +60,35 @@ public:
         vector<vector<int>> dp(n+1 , vector<int>(m+1,-1));
         
         return (n+m) - 2*lcs(n-1,m-1,word1,word2,dp);
+    }
+    */
+    
+    /*
+    Tabulation
+    TC = O(N*M)
+    SC = O(N*M)
+    */
+     int minDistance(string s1, string s2) {
+        
+        int n = s1.size();
+        int m = s2.size();
+        
+        vector<vector<int>> dp(n+1 , vector<int>(m+1,0));
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                
+                else{
+                int b = dp[i-1][j];
+                int c = dp[i][j-1];
+
+                dp[i][j] = max({b,c});
+                }
+            }
+        }
+        
+        
+        return (n+m) - 2*dp[n][m];
     }
 };
