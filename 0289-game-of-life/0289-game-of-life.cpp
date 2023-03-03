7079153b097ee849361dd7fa94878fe070516933
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-03 09:41:18 +0530

    Time: 0 ms (100.00%), Space: 7 MB (23.69%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,50 +1,32 @@
 class Solution {
 public:
-    bool isValid(int x,int y,int n,int m){
-        if(x<0 || x>=n || y<0 || y>=m)return 0;
-        return 1;
-    }
     void gameOfLife(vector<vector<int>>& board) {
-        int n=board.size();
-        int m=board[0].size();  
-        // create a ans vector to store next state
         
-        vector<vector<int>>ans(n,vector<int>(m,0));
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>> dup = board;
         
-        for(int row=0;row<n;row++){
-            for(int col=0;col<m;col++){
-                int live=0;
-                
-                // move in ll eight direction and count the no. of live cells
+        for(int row = 0 ; row < n ; row++){
+            for(int col = 0 ; col < m ; col++){
+                int cell = dup[row][col];
+                int live = 0;
                 for(int i = -1 ; i <= 1 ; i++){
                     for(int j = -1 ; j <= 1 ; j++){
-                    int cx=row+i;
-                    int cy=col+j;
-                    if(row == cx && col == cy) continue; // inp condition baar baar bhulne ki aadat
-                    if(isValid(cx,cy,n,m) && board[cx][cy]==1){
-                        live++;
-            
+                        int nx = row + i;
+                        int ny = col + j;
+                        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                        if(nx == row && ny == col) continue;// V.imp condition baar baar bhul jaate ho
+                        else{
+                            if(dup[nx][ny] == 1)live++;
+                        }
                     }
                 }
-                }
-                // Check if current cell is live or dead and apply rules accordingly and update ans vector
-                if(board[row][col]==1){
-                    if(live<2 || live >3){
-                        ans[row][col]=0;
-                    }else{
-                        ans[row][col]=1;
-                    }
-                }
-                else{
-                    if(live==3){
-                        ans[row][col]=1;
-                    }else{
-                        ans[row][col]=0;
-                    }
-                }
+              
+                if(live < 2 && cell == 1) board[row][col] = 0;
+                else if((live == 2 || live == 3) && cell == 1) board[row][col] = 1;
+                else if(live > 3 && cell == 1) board[row][col] = 0;
+                else if(cell == 0 && live == 3) board[row][col] = 1;  
             }
         }
-        // update board to next state
-        board=ans;
     }
 };
