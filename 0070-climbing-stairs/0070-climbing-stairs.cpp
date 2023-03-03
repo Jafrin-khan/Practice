5dee6142b2664aa43e6c1dfc2a469f1a16e137fa
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-03 15:17:24 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (33.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,59 +1,45 @@
 class Solution {
 public:
+    //recursive
     /*
-     //Recursive
-    int f(int n){ 
-        if(n == 0){
-            return 1;
-        }
-        if(n == 1){
-            return 1;
-        }
+    TC = Exponential,,,,,mp --> O(2^N) {check from friend}
+    SC = O(N)
+    
+    int f(int n){
+        if(n == 1) return 1;
+        if(n == 0) return 1;
+        
         int one = f(n-1);
         int two = f(n-2);
-        return one + two;
+        return one+two;
     }
     
     int climbStairs(int n) {
         return f(n);
-    }*/
+    }
+    */
     
-    //Memoised
+    //memoised
+    /*
+    dono check karo TC and SC
+    TC = O(N)
+    SC = O(N)
+    */
+    
     int f(int n , vector<int>& dp){
+        if(n == 1) return 1;
+        if(n == 0) return 1;
         
-        if(n == 0){
-            return 1;
-        }
-        if(n == 1){
-            return 1;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
+        if(dp[n] != -1) return dp[n];
+        
         int one = f(n-1 , dp);
         int two = f(n-2 , dp);
-        return dp[n] = one + two;
+        return dp[n] = one+two;
     }
     
     int climbStairs(int n) {
         vector<int> dp(n+1 , -1);
-        return f(n , dp);
+        return f(n,dp);
     }
     
-    //Tabulated
-   
-   /* int climbStairs(int n) {
-        vector<int> dp(n+1 , 0);
-        dp[0] = 1;
-        dp[1] = 1;
-      
-        for(int i = 2 ; i <= n ; i++){
-            int one = dp[i-1];
-            int two = dp[i-2];
-        
-            dp[i] = one + two;
-        }
-        
-        return dp[n];
-    }*/
 };
