1a43f781329b0bcf34117768b067125e4d8a1831
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-05 21:49:01 +0530

    Time: 14 ms (69.70%), Space: 13.8 MB (46.42%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    int findMinDifference(vector<string>& s) {
-       vector<int> v;
+    /*
+    TC = O(N)
+    SC = O(1)
+    */
+    
+    // https://www.youtube.com/watch?v=-QAC8MgJEAI
+    
+    int findMinDifference(vector<string>& s){
+        vector<int> v;
         
+        //converting and storing mins
         for(int i = 0 ; i < s.size() ; i++){
             string hrs = s[i].substr(0,2);
             string mins = s[i].substr(3);
             
             int h = stoi(hrs);
-            int m = stoi(mins);
+            int m = stoi(mins);  
             
             v.push_back(h*60 + m);
         }
         
         sort(v.begin() , v.end());
+        
+        //diff nikaalre h starting from ind 1
         int ans =  1e9;
         for(int i = 1 ; i < v.size() ; i++){
             ans = min(ans , v[i]-v[i-1]);
         }
         
+        //edge case k lie
         ans = min(ans , 24*60-v[v.size()-1] + v[0]);
         
         return ans;
