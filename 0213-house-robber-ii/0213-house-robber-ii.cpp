84f0be5f1253ab90f6c2d6167ab6e1ffc0393da1
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-06 15:54:11 +0530

    Time: 0 ms (100.00%), Space: 8 MB (21.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,53 +1,45 @@
 class Solution {
 public:
     
-    //diff from house robberI as here we are given circular array
+    int f(int ind , vector<int> &nums , vector<int>& dp){
     
-    int f(int ind , vector<int>& nums , vector<int>& dp){
-         
-        if(ind < 0){
-            return 0;
-        }
-        
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
-        
-        int notTaken = 0 + f(ind - 1 , nums , dp);
-        int taken = nums[ind] + f(ind - 2 , nums , dp);
-        
-        return dp[ind] = max(taken,notTaken);
-        
+    if(ind == 0){
+        return nums[ind];
     }
+    if(ind < 0){
+        return 0;
+    }
+    
+    if(dp[ind] != -1){
+        return dp[ind];
+    }
+    int notTake = 0 + f(ind-1 , nums , dp);
+    int take = nums[ind] + f(ind-2 , nums , dp);
+    
+    return dp[ind] = max(take , notTake);
+}
+    
     int rob(vector<int>& nums) {
         
         int n = nums.size();
-        
-       
-        if(n == 1){
-            return nums[0];
-        }
-        vector<int> arr1;
-        vector<int> arr2;
-        
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0] , nums[1]);
+    
+        vector<int> v1;
+        vector<int> v2;
         
         for(int i = 0 ; i < n ; i++){
-            
-            if(i != 0){
-                arr1.push_back(nums[i]);
-            }
-            
-            if(i != n-1){
-                arr2.push_back(nums[i]);
-            }
+            if(i != 0) v1.push_back(nums[i]);
+            if(i != n-1) v2.push_back(nums[i]);
         }
-        vector<int> dp1(arr1.size()+1 , -1);
         
-        int ans1 = f(arr1.size()-1 , arr1 , dp1);
-         vector<int> dp2(arr2.size()+1 , -1);
-        int ans2 = f(arr2.size()-1 , arr2 , dp2);
+        vector<int> dp1(v1.size() , -1);
+        int ans1 = f(v1.size()-1 , v1 , dp1);
+        
+        vector<int> dp2(v2.size() , -1);
+        int ans2 = f(v2.size()-1 , v2 , dp2);
         
         return max(ans1 , ans2);
-         
+  
     }
 };
