657968e29aadf5f9e990240823bb7b79a16d3fdd
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-07 15:14:49 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (50.73%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,7 +4,7 @@ public:
     //memoization
     TC = O(M*N)
     SC = O(M*N)
-    */
+    
      int f(int i , int j , vector<vector<int>>& dp , vector<vector<int>>& grid){
         if(i < 0 || j < 0 || grid[i][j] == 1) return 0;
         if(i == 0 && j == 0) return 1;
@@ -24,6 +24,41 @@ public:
         vector<vector<int>> dp(m , vector<int>(n,-1));
         
         return f(m-1,n-1,dp,grid);
+        
+    }
+    */
+    
+    /*
+    //Tabulation
+    
+    
+    */
+     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+         
+        if(grid[0][0] == 1) return 0;
+        
+        vector<vector<int>> dp(m , vector<int>(n,0));
+        dp[0][0] = 1;
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+               
+                if(grid[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                 if(i == 0 && j == 0) continue;
+                 int left = 0, up = 0;
+                
+                 if(i > 0) left = dp[i-1][j];
+                 if(j > 0) up = dp[i][j-1];
+
+                dp[i][j] = left + up;
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
