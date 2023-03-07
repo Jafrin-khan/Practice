214674ed398c0a04371d69fc12929c457cd7a22e
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-07 14:43:44 +0530

    Time: 5 ms (13.61%), Space: 6.6 MB (32.11%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
   /*
-Memoization
-Time Complexity: O(M*N)
+   // Memoization
+    Time Complexity: O(M*N)
 
-Reason: At max, there will be M*N calls of recursion.
+    Reason: At max, there will be M*N calls of recursion.
 
-Space Complexity: O((N-1)+(M-1)) + O(M*N)
+    Space Complexity: O((N-1)+(M-1)) + O(M*N)
 
-Reason: We are using a recursion stack space:O((N-1)+(M-1)), here (N-1)+(M-1) is the path length and an external DP Array of size ‘M*N’.
-    */
+    Reason: We are using a recursion stack space:O((N-1)+(M-1)), here (N-1)+(M-1) is the path length and an external DP Array       of size ‘M*N’.
+
 
     int f(int i , int j , vector<vector<int>>& dp){
         if(i < 0 || j < 0) return 0;
@@ -22,10 +22,30 @@ Reason: We are using a recursion stack space:O((N-1)+(M-1)), here (N-1)+(M-1) is
         
         return dp[i][j] = left + up;
     }
+ */
+ 
+    /*
+    //tabulation
+    TC = 
+    SC = 
+    */
     
     int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m , vector<int>(n,0));
+        dp[0][0] = 1;
         
-        vector<vector<int>> dp(m , vector<int>(n,-1));
-        return f(m-1 , n-1,dp);
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 && j == 0) continue;
+                
+                 int left = 0, up = 0;
+                
+                 if(i > 0) left = dp[i-1][j];
+                 if(j > 0) up = dp[i][j-1];
+
+                dp[i][j] = left + up;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
