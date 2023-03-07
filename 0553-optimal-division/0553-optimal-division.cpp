193e0fb46a48ae35d05c37dd0d8ce0f8fbd09940
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-07 08:43:58 +0530

    Time: 6 ms (15.47%), Space: 8 MB (60.22%) - LeetHub

--- 0553-optimal-division/0553-optimal-division.cpp
+++ 0553-optimal-division/0553-optimal-division.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    
+    /*
+ 
+Initialize a string str
+If the size of the given num is equal to 1 then add the only element if num to the string ("num[0]")
+If the size of the given num is equal to 2 then no brackets required hence add 1st element ,'/' and 2nd element to the string
+("num[0]/num[1]")
+Else make it in the form "num[0]/(num[1]/......)"
+Return the string
+Complexity
+Time complexity:
+O(n)
+    */
     string optimalDivision(vector<int>& nums) {
         string str="";
         if(nums.size()==1) return to_string(nums[0]);
