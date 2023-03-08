3a153016771a4e0b045012ee65bff13e50be12d5
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-08 15:53:41 +0530

    Time: 12 ms (15.83%), Space: 9 MB (35.47%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,35 +1,60 @@
 class Solution {
 public:
+    /* See why this approach not working
+    int f(int i , int j , vector<vector<int>>& triangle){
+        if(j < 0 || i < 0) return 1e9;
+        if(i == 0) return triangle[0][0];
+        int l = f( i-1 , j , triangle);
+        int r = f( i-1 , j-1 , triangle);
+        
+        return triangle[i][j] + min(l,r);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int n = triangle.size();
+        int ans = 1e9;
+        for(int j = 0 ; j < triangle[n-1].size() ; j++){
+            ans = min(ans , f(n-1 , j ,triangle));
+        }
+        
+        return ans;
+    }
+    */
     
     /*
-    Time Complexity: O(N*N)
-
-Reason: There are two nested loops
-
-Space Complexity: O(N*N)
-
-Reason: We are using an external array of size ‘N*N’. The stack space will be eliminated.
-*/
+    //recursion
+    //TC = 
+    //SC = 
+    int f(int i , int j , int n , vector<vector<int>>& triangle){
+        if(i == n-1) return triangle[i][j];
+        int l = f(i+1,j,n,triangle);
+        int r = f(i+1,j+1,n,triangle);
+        
+        return triangle[i][j] + min(l,r);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int n = triangle.size();
+        return f(0 , 0 , n , triangle);
+    }
+    */
     
-   int minimumTotal(vector<vector<int>>& triangle) {
-		int n = triangle.size();
-		vector<vector<int>>dp(n, vector<int>(n, -1));
-		for(int i = 0; i < n; i++) 
-			dp[n-1][i] = triangle[n-1][i];
-       
-       
-		for(int i = n-2; i >= 0; i--) {
-			for(int j = i; j >= 0; j--) {
-				int up = triangle[i][j] + dp[i+1][j];
-				int up_left = triangle[i][j] + dp[i+1][j+1];
-				dp[i][j] = min(up, up_left);
-			}
-		}
-		return dp[0][0];
-	}
-   
-   
+    //memoization
+    //TC = 
+    //SC = 
+    int f(int i , int j , int n , vector<vector<int>>& triangle , vector<vector<int>>& dp){
+        if(i == n-1) return triangle[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int l = f(i+1,j,n,triangle,dp);
+        int r = f(i+1,j+1,n,triangle,dp);
+        
+        return dp[i][j] = triangle[i][j] + min(l,r);
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int n = triangle.size();
+        vector<vector<int>> dp(n , vector<int>(n,-1));
+        return f(0 , 0 , n , triangle , dp);
+    }
 };
-
-
-
