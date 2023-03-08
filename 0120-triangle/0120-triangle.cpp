976ab145f5db1111e4cf2f73e33156fed4ca8a25
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-08 16:15:50 +0530

    Time: 8 ms (48.81%), Space: 9.1 MB (35.47%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -39,9 +39,15 @@ public:
     }
     */
     
+    /*
     //memoization
-    //TC = 
-    //SC = 
+// Time Complexity: O(N*N)
+
+// Reason: At max, there will be (half of, due to triangle) N*N calls of recursion.
+
+// Space Complexity: O(N) + O(N*N)
+
+// Reason: We are using a recursion stack space: O((N), where N is the path length and an external DP Array of size ‘N*N’.
     int f(int i , int j , int n , vector<vector<int>>& triangle , vector<vector<int>>& dp){
         if(i == n-1) return triangle[i][j];
         if(dp[i][j] != -1) return dp[i][j];
@@ -56,5 +62,35 @@ public:
         int n = triangle.size();
         vector<vector<int>> dp(n , vector<int>(n,-1));
         return f(0 , 0 , n , triangle , dp);
+    }
+    */
+    
+    //Tabulation
+// Time Complexity: O(N*N)
+
+// Reason: There are two nested loops
+
+// Space Complexity: O(N)
+
+// Reason: We are using an external array of size ‘N’ to store only one row.
+    
+      int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n , vector<int>(n,0));
+          
+        for(int j = 0 ; j < triangle[n-1].size() ; j++) dp[n-1][j] = triangle[n-1][j];
+         
+        for(int i = n-2 ; i >= 0 ; i--){
+            for(int j = i ; j >= 0 ; j--){
+                int l = dp[i+1][j];
+                int r = dp[i+1][j+1];
+               
+        
+                dp[i][j] = triangle[i][j] + min(l,r);
+            }
+        }
+          
+        return dp[0][0];
+       
     }
 };
