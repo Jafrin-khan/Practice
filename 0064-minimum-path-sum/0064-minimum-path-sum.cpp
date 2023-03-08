94314ea564eecd3b938d862f817187a170e734b2
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-08 15:12:48 +0530

    Time: 8 ms (81.00%), Space: 10.1 MB (60.57%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -23,10 +23,16 @@ public:
     }
     */
     
+ /*    
     //Memoization
     
-    //TC = 
-    //SC = 
+//    Time Complexity: O(N*M)
+
+// Reason: At max, there will be N*M calls of recursion.
+
+// Space Complexity: O((M-1)+(N-1)) + O(N*M)
+
+// Reason: We are using a recursion stack space:O((M-1)+(N-1)), here (M-1)+(N-1) is the path length and an external DP Array of size ‘N*M’.
     int f(int i , int j , int n , int m , vector<vector<int>>& grid , vector<vector<int>>& dp){
         if(i >= n || j >= m || i < 0 || j < 0) return 1e9;
         
@@ -48,5 +54,36 @@ public:
         vector<vector<int>> dp(n , vector<int>(m , -1));
         
         return f(n-1 , m-1 , n , m , grid , dp);
+    }
+    */
+    
+    
+    //tabulation
+    
+    //TC = O(N*M)
+    //SC = O(N*M)
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> dp(n , vector<int>(m , 0));
+        
+        dp[0][0] = grid[0][0];
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+               if(i == 0 && j == 0) continue;
+                
+               int left = 1e9 , up = 1e9;
+                  
+               if(j > 0) left = dp[i][j-1];
+               if(i > 0)  up = dp[i-1][j];
+
+               dp[i][j] = grid[i][j] + min({left  , up});
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
