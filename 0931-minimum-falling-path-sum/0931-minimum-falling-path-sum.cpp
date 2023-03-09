04cfde849607d68d0a156c3dda47fd3b7b7398cf
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-09 13:34:20 +0530

    Time: 286 ms (5.02%), Space: 10.4 MB (32.27%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -24,7 +24,8 @@ public:
         return sum;
     }
 */
-    
+
+/*
 //Memoization
 // Time Complexity: O(N*N)
 
@@ -56,4 +57,47 @@ public:
         }
         return sum;
     }
+*/
+    
+//Tabulation
+/*
+Time Complexity: O(N*M)
+
+Reason: There are two nested loops
+
+Space Complexity: O(N*M)
+
+Reason: We are using an external array of size ‘N*M’. The stack space will be eliminated.
+*/
+    
+    int f(int i , int j,  int n , int m ,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        for(int j = 0 ; j < m ; j++) dp[n-1][j] = grid[n-1][j];
+        
+        for(int i = n-2 ; i >= 0 ; i--){
+            for(int j = m-1 ; j >= 0 ; j--){
+                
+                int l = 1e9,d = 1e9,r = 1e9;
+                if(j > 0) l = dp[i+1][j-1];
+                d = dp[i+1][j];
+                if(j < m-1) r = dp[i+1][j+1];
+                dp[i][j] = grid[i][j] + min({l,r,d});
+            }
+        }
+        
+        return dp[i][j];
+            
+    }
+    
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n , vector<int>(m,0));
+        int sum = 1e9;
+        for(int i = 0 ; i < m ; i++){
+            sum = min(sum , f(0,i,n,m,grid,dp));
+        }
+        return sum;
+    } 
+    
+    
 };
