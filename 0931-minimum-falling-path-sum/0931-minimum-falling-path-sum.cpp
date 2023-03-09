b94b89c989817a8544438017ebca38a7eee08c4a
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-09 13:09:24 +0530

    Time: 15 ms (71.94%), Space: 10.4 MB (8.01%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,55 +1,59 @@
 class Solution {
 public:
+/*
+ //Recursion
     
-    /*
-    Time Complexity: O(N*N)
-
-Reason: At max, there will be M*N calls of recursion to solve a new problem,
-
-Space Complexity: O(N) + O(N*M)
-
-Reason: We are using a recursion stack space: O(N), where N is the path length and an external DP Array of size ‘N*M’.
+ //TC : 
+ //SC : 
     
-    
-    */
-    
-    int f(int i , int j , vector<vector<int>>& grid , vector<vector<int>>& dp){
-        
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        if(i >= m || j < 0 || j >= n){
-            return 1e9;
+    int f(int i , int j,  int n , int m ,vector<vector<int>>& grid){
+        if(i < 0 || j < 0 || i >= n || j >= m) return 1e9;
+        if(i == n-1) return grid[i][j];
+        int l = f(i+1,j-1,n,m,grid);
+        int d = f(i+1,j,n,m,grid);
+        int r = f(i+1,j+1,n,m,grid);
+        return grid[i][j] + min({l,d,r});
+    }
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int sum = 1e9;
+        for(int i = 0 ; i < m ; i++){
+            sum = min(sum , f(0,i,n,m,grid));
         }
+        return sum;
+    }
+*/
+    
+//Memoization
+// Time Complexity: O(N*N)
+
+// Reason: At max, there will be M*N calls of recursion to solve a new problem,
+
+// Space Complexity: O(N) + O(N*M)
+
+// Reason: We are using a recursion stack space: O(N), where N is the path length and an external DP Array of size ‘N*M’.
+    
+    int f(int i , int j,  int n , int m ,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        if(i < 0 || j < 0 || i >= n || j >= m) return 1e9;
+        if(dp[i][j] != -1) return dp[i][j];
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        if(i == m-1){
-            return grid[i][j];
-        }
-        
-        int left = f(i+1 , j-1 , grid , dp) + grid[i][j];
-        int down = f(i+1 , j , grid , dp) + grid[i][j];
-        int right = f(i+1 , j+1 , grid , dp) + grid[i][j];
-        
-        return dp[i][j] = min({left , down , right});
+        if(i == n-1) return dp[i][j] = grid[i][j];
+       
+        int l = f(i+1,j-1,n,m,grid,dp);
+        int d = f(i+1,j,n,m,grid,dp);
+        int r = f(i+1,j+1,n,m,grid,dp);
+        return dp[i][j] = grid[i][j] + min({l,d,r});
     }
     
     int minFallingPathSum(vector<vector<int>>& grid) {
-        
-       int n = grid.size();
+        int n = grid.size();
         int m = grid[0].size();
-        int mini = 1e9;
-        
-        vector<vector<int>> dp(n , vector<int>(m , -1));
-    
+        vector<vector<int>> dp(n , vector<int>(m,-1));
+        int sum = 1e9;
         for(int i = 0 ; i < m ; i++){
-            mini = min(mini , f(0 , i , grid , dp));
+            sum = min(sum , f(0,i,n,m,grid,dp));
         }
-        
-        return mini;
-        
+        return sum;
     }
 };
