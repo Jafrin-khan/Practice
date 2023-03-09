0294ffd83cbfcfe02f2b972198379f1c8e1f34a4
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-09 14:39:16 +0530

    Time: 256 ms (18.64%), Space: 16.9 MB (59.64%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -59,7 +59,7 @@ Reason: We are using a recursion stack space: O(N), where N is the path length a
         }
         
         for(int i = n-2 ; i >= 0 ; i--){
-            for(int j1 = 0 ; j1 < m ; j1++){ //for(int j1 = m-1 ; j1 >= 0 ; j1--){ ye kyu ni h?
+            for(int j1 = m-1 ; j1 >= 0 ; j1--){
                 for(int j2 = 0 ; j2 < m ; j2++){
                     int maxi = -1e9;
                     for(int dj1 = -1 ; dj1 <= 1 ; dj1++){
