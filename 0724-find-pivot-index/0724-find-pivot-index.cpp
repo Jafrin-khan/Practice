2d8280d06a85f34f644f60bd572d93212365127a
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-11 09:26:07 +0530

    Time: 33 ms (33.65%), Space: 31.2 MB (22.60%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    
+/* 2nd approch 
+    if all elements are non-negative, we can use only one pass.
+But for this problem, we have to go 2 passes to get sum first.
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int i = 0, j = nums.size() - 1, left_sum = 0, right_sum = 0;
+        while (i <= j) {
+            if (i == j) {
+                if (left_sum == right_sum) {
+                    return i;
+                }
+                return -1;
+            }
+            
+            if (left_sum <= right_sum) {
+                left_sum += nums[i];
+                ++i;
+            } else {
+                right_sum += nums[j];
+                --j;
+            }
+        }
+        return -1;
+    }
+};
+    */
     int pivotIndex(vector<int>& nums) {
      int sum1=0,sum2=0;
         for(int i=0;i<nums.size();i++){
