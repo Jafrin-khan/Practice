4ef7eb9a7510a2bc4c4b887dd6b2b3e8530ecded
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-12 14:44:07 +0530

    Time: 32 ms (27.43%), Space: 13.5 MB (87.04%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,50 +1,32 @@
 class Solution {
 public:
-    
-     /*
-    TC = O(V+2E)
-    SC = O(3V) ~ O(V)
-    */
-    
-    /*
-    This is DFS
-    BFS wala bhi h iska prev accepted ans 
-    */
-    
-    bool dfs(int node , int col , vector<vector<int>> adj , vector<int>& color){
+    //TC aur explanation pichle submission mai
+    bool dfs(int node , int color , vector<vector<int>>& adj , vector<int>& col){
+        col[node] = color;
         
         for(auto it : adj[node]){
-            if(color[it] == -1){
-                color[it] = 1-col;
-                if(!dfs(it , color[it],adj,color)){
-                    return false;
-                }
+             if(col[it] == -1){
+                if(!dfs(it , 1- color, adj , col)) return false;
             }
-            
-            else if(color[it] == col){
-                return false;
+            else{
+                if(color == col[it]) return false;
             }
         }
-        
         return true;
     }
-    
     bool isBipartite(vector<vector<int>>& adj) {
         
         int n = adj.size();
-        int m = adj[0].size();
+       
+        vector<int> col(n,-1);
         
-        vector<int> color(n , -1);
-      
         for(int i = 0 ; i < n ; i++){
-            if(color[i] == -1){
-                color[i] = 0;
-                if(!dfs(i , 0 , adj , color)){
-                    return false;
-                }
+            if(col[i] == -1){
+                if(!dfs(i , 0 , adj , col)) return false;
             }
         }
         
         return true;
+        
     }
 };
