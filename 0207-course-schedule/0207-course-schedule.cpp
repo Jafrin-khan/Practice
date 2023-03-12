aa580cd7065e6b37453960e234628a0beec17727
Jafrin <82137973+Jafrin-khan@users.noreply.github.com>
2023-03-12 15:44:18 +0530

    Time: 25 ms (54.27%), Space: 14 MB (51.78%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,62 +1,33 @@
 class Solution {
 public:
-    
-    /*
-    we can use DFS but here i have used kahn's algo for cycle ddetection
-    TC = O(V+E)
-    SC = O(2N)
-    */
-     bool isCyclic(int v, vector<int> adj[]) {
-        
-        vector<int> indegree(v , 0);
-        
-        for(int i = 0 ; i < v ; i++){
-            
-            for(auto it : adj[i]){
-                indegree[it]++;
+     bool isCycle(int node , vector<vector<int>>& adj , vector<int>& vis , vector<int>& dfsVis){
+        vis[node] = 1;
+        dfsVis[node] = 1;
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if(isCycle(it , adj, vis , dfsVis)) return true;
             }
+            else if(dfsVis[it] == 1) return true;
         }
         
-        queue<int> q;
-        
-        for(int i = 0 ; i < v ; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
-        }
-        
-        int cnt = 0;
-        while(!q.empty()){
-            
-            int node = q.front();
-            q.pop();
-            cnt++;
-            
-            for(auto it : adj[node]){
-                indegree[it]--;
-                
-                if(indegree[it] == 0){
-                    q.push(it);
-                }
-            }
-            
-        }
-        
-        return cnt != v;
+        dfsVis[node] = 0;
+        return false;
     }
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+  
+    bool canFinish(int n, vector<vector<int>>& nums) {
+      vector<vector<int>> adj(n);
+       
+       for(int i = 0 ; i < nums.size() ; i++){
+           adj[nums[i][0]].push_back(nums[i][1]);// as directed graph
+       }
         
-        vector<int> adj[n];
+       vector<int> vis(n,0);
+       vector<int> dfsVis(n,0);
         
-        for(int i = 0 ; i < prerequisites.size() ; i++){
-                int u = prerequisites[i][0];
-                int v = prerequisites[i][1];
-                
-                adj[u].push_back(v);
-                
-            
-        }
-        return !(isCyclic(n , adj));
+       for(int i = 0 ; i < n ; i++){
+           if(!vis[i] && isCycle(i , adj , vis,dfsVis)) return false;
+       }
         
+        return true;
     }
 };
