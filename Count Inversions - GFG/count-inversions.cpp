4b0a784a64807a387848596f5852a3350935a5c6
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-15 17:48:53 +0530

    Added solution - LeetHub

---
+++ Count Inversions - GFG/count-inversions.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
+    
+ long long merge(long long *arr , long long start , long long mid , long long end){
+    
+    long long i = start;
+    long long j = mid+1;
+    vector<long long> temp(end-start+1);
+    
+    long long cnt = 0;
+    long long k = 0;
+    while(i <= mid && j <= end){
+        if(arr[i] <= arr[j]){
+            temp[k++] = arr[i++];
+        }
+        
+        else{
+            temp[k++] = arr[j++];
+            cnt += (mid+1 - i);
+        }
+    }
+    
+    while(i <= mid){
+        temp[k++] = arr[i++];
+    }
+    
+    while(j <= end){
+        temp[k++] = arr[j++];
+    }
+    
+    k = 0;
+    for(int i = start ; i <= end; i++){
+        arr[i] = temp[k++];
+    }
+    
+    return cnt;
+}
+
+long long mergeSort(long long *arr , long long start , long long end){
+    
+    long long cnt = 0;
+    if(start < end){
+        long long mid = (start + end)/2;
+        cnt += mergeSort(arr , start , mid);
+        cnt += mergeSort(arr , mid+1 , end);
+        cnt += merge(arr , start , mid , end);
+    }
+    
+    return cnt;
+}
+    long long int inversionCount(long long arr[], long long n)
+    {
+        return mergeSort(arr , 0 , n-1);
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    
+    long long T;
+    cin >> T;
+    
+    while(T--){
+        long long N;
+        cin >> N;
+        
+        long long A[N];
+        for(long long i = 0;i<N;i++){
+            cin >> A[i];
+        }
+        Solution obj;
+        cout << obj.inversionCount(A,N) << endl;
+    }
+    
+    return 0;
+}
+
+// } Driver Code Ends
