57c4f0ae7f5761afa3a6ac744bdfdfa0b5e9d155
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-17 00:18:39 +0530

    Time: 131 ms (37.88%), Space: 45.2 MB (29.00%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,15 +3,16 @@ public:
 /*Recursive...TC =     SC = 
 
     
- /*Memoised
-Time Complexity: O(N*T)
+ /*
+ //Memoised
+//Time Complexity: O(N*T)
 
-Reason: There are N*T states therefore at max ‘N*T’ new problems will be solved.
+//Reason: There are N*T states therefore at max ‘N*T’ new problems will be solved.
 
-Space Complexity: O(N*T) + O(N)
+//Space Complexity: O(N*T) + O(N)
 
-Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*T)).
-    */
+//Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*T)).
+    
     
     int f(int ind , int amt , vector<int>& coins , vector<vector<int>>& dp){
         
@@ -41,5 +42,42 @@ Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*T)).
         
         return ans;
         
+    }*/
+    
+    //Tabulation
+//Time Complexity: O(N*T)
+
+//Reason: There are two nested loops
+
+//Space Complexity: O(N*T)
+
+//Reason: We are using an external array of size ‘N*T’. Stack Space is eliminated.
+      int coinChange(vector<int>& coins, int amount) {
+        
+        int n = coins.size();
+        vector<vector<int>> dp(n , vector<int>(amount+1 , 0));//amlount+1
+        
+        for(int amt = 0 ; amt <= amount ; amt++){
+             if(amt%coins[0] == 0) dp[0][amt] = amt/coins[0];
+             else dp[0][amt] = 1e9;
+        }
+        
+        for(int ind = 1 ; ind < n ; ind++){
+            for(int amt = 0 ; amt <= amount ; amt++){
+                int take = 1e9;
+                if(coins[ind] <= amt) take = 1 + dp[ind][amt - coins[ind]];
+                int notTake = 0 + dp[ind-1][amt];
+
+                dp[ind][amt] = min(take,notTake);
+            }
+        }
+        
+        
+        int ans = dp[n-1][amount];
+        if(ans == 1e9) return -1;
+        
+        return ans;
+        
     }
+    
 };
