7a8b373f3bfd5f89988d7dce051ffd6588ceffdc
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-16 23:07:29 +0530

    Time: 20 ms (84.36%), Space: 13.2 MB (93.21%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,73 +1,46 @@
 class Solution {
 public:
     
-      
-    /*
-    we can use DFS but here i have used kahn's algo for cycle ddetection
-    TC = O(V+E)
-    SC = O(2N)
-    */
-    
-      vector<int> isCyclic(int v, vector<int> adj[]) {
+    //solving by using toposort...KAHN'S ALGO
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<int> adj[n];
         
-        vector<int> indegree(v , 0);
-        
-        for(int i = 0 ; i < v ; i++){
-            
-            for(auto it : adj[i]){
-                indegree[it]++;
-            }
+        for(int i = 0 ; i < prerequisites.size() ; i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u);
         }
         
-        queue<int> q;
-        vector<int> topoSort;
-        for(int i = 0 ; i < v ; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            }
+        vector<int> indegree(n,0);
+        
+        for(int i = 0 ; i < n ; i++){
+            for(auto it : adj[i]) indegree[it]++;
         }
         
         int cnt = 0;
+        queue<int> q;
+        vector<int> topo;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            } 
+        }
+        
         while(!q.empty()){
-            
-            int node = q.front();
-            q.pop();
-            topoSort.push_back(node);
+            int node = q.front();q.pop();
+            topo.push_back(node);
+            cnt++;
             
             for(auto it : adj[node]){
                 indegree[it]--;
-                
-                if(indegree[it] == 0){
+                if(indegree[it] == 0) {
                     q.push(it);
                 }
             }
-            
         }
         
-        return topoSort;
-    }
-  
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        
-         vector<int> adj[n];
-        
-        for(int i = 0 ; i < prerequisites.size() ; i++){
-                int u = prerequisites[i][0];
-                int v = prerequisites[i][1];
-                
-                //bs yhan change h baaki same hi hai Course Schedule I ke
-                adj[v].push_back(u);
-                
-            
-        }
-        
-        vector<int> ans = isCyclic(n, adj); 
-        
-        if(ans.size() != n){
-            return {};
-        }
-        
-        return ans;
-        
+        if(cnt == n) return topo;
+        return {};
     }
 };
