84e3da1e31179797fc91671244a1677cecd313b2
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-16 20:12:33 +0530

    Time: 7 ms (36.67%), Space: 11 MB (98.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    /*
+    TC = O(logN)
+    SC = O(1)
+    */
+    
+    /*
+        /*
+    
+     nums = [4,5,6,7,0,1,2], target = 0
+            0  1 2 3 4 5 6
+            
+      
+     l = 0;
+     h = 6;
+     m = 3;
+     
+     4 <= 7 yes , 4 <= 0 no , hence l = 4
+     
+     l = 4;
+     h = 6;
+     m = 5;
+     
+     
+     0 <= 1 no , 1 <= 0 no , hence h =4
+     
+     l = 4;
+     h = 4;
+     m = 4;
+     
+     0 == 0 return 4;
+     
+     
+    */
     int search(vector<int>& nums, int target) {
         
         int n = nums.size();
