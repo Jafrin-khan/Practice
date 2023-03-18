ab5a3221ab20ae800fb9df23e156a97bed0c5757
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-18 14:53:05 +0530

    Time: 76 ms (59.10%), Space: 21.8 MB (39.60%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        if(n == 1){
+            return grid[0][0] == 0;
+        } 
+       
+        vector<vector<int>> distTo(n , vector<int>(n,1e9));
+        queue<pair<int,pair<int,int>>> q;
+        
+        q.push({1,{n-1,n-1}});
+        distTo[n-1][n-1] = 1;
+        int ans = 1e9;
+        
+        while(!q.empty()){
+            int dis = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;q.pop();
+            
+             for(int i = -1 ; i <= 1 ; i++){
+                 for(int j = -1 ; j <= 1 ; j++){
+                     int nx = x + i;
+                     int ny = y + j;
+                     
+                     if(nx < 0 || ny < 0 || nx >= n || ny >= n || (nx == x && ny == y) || grid[nx][ny] == 1) continue;
+                     if(nx == 0 && ny == 0 && grid[nx][ny] == 0) ans = min(ans , dis+1);
+                     if(distTo[nx][ny] > dis + 1){
+                         distTo[nx][ny] = dis+1;
+                         q.push({dis+1,{nx,ny}});
+                     }
+                     
+                 }
+             }
+        }
+        if(ans == 1e9) return -1;
+        
+        return ans;
+        
+    }
+};
