c5f13616dd38ffd2e72158e55e52112a6376c3ee
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-19 16:18:04 +0530

    Time: 28 ms (69.44%), Space: 13.6 MB (57.20%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
-     /*
-  TC is  Elog(V) but no priority queue here hence remove logV
-  TC = E = flight.size();*/
-  
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-           // Code here
+    //TC and SC pichle submission m
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        //{node , cost}
         vector<pair<int,int>> adj[n];
+        
         for(auto it : flights){
-            adj[it[0]].push_back({it[1] , it[2]});
+            adj[it[0]].push_back({it[1],it[2]});
         }
-       
-       //{stops,{node , cost}}
-       queue<pair<int , pair<int,int>>> q;
-       q.push({0 , {src , 0}});
-       vector<int> dist(n , 1e9);
-       dist[src] = 0;
-       
-       while(!q.empty()){
-           
-           auto it = q.front();
-           q.pop();
-           
-           int stops = it.first;
-           int node = it.second.first;
-           int cost = it.second.second;
-           
-           if(stops > K){
-               continue;
-           }
-           
-           for(auto iter : adj[node]){
-               int adjNode = iter.first;
-               int edW = iter.second;
-               
-               if(cost + edW < dist[adjNode] && stops <= K){
-                   dist[adjNode] = cost + edW;
-                   q.push({stops+1 , {adjNode , cost+edW}});
-               }
-           }
-       }
-       
-       if(dist[dst] == 1e9){
-           return -1;
-       }
-       
-       return dist[dst];
+        
+        vector<int> disArr(n,1e9);
+        disArr[src] = 0;
+        
+        queue<pair<int,pair<int,int>>> q;// <stops , node , dist>
+        q.push({0 ,{ src , 0}});
+        
+        while(!q.empty()){
+            auto front = q.front();q.pop();
+            int stops = front.first;
+            int node = front.second.first;
+            int cost = front.second.second;
+            
+            if(stops > k) continue;
+            
+            for(auto it : adj[node]){
+                int adjNode = it.first;
+                int edW = it.second;
+                
+                if(stops <= k && cost + edW < disArr[adjNode]){
+                    disArr[adjNode] = cost + edW;
+                    q.push({stops+1,{adjNode,cost+edW}});
+                }
+                
+            }
+            
+        }
+        
+        if(disArr[dst] == 1e9) return -1;
+        
+        return disArr[dst];
+        
     }
 };
