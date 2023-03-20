10ba9e12b1f4adfa0be795097fcbfb33f6d14c98
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-20 20:44:10 +0530

    Time: 28 ms (69.38%), Space: 13.8 MB (42.60%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    //TC and SC pichle submission m
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
         vector<pair<int,int>> adj[n];
@@ -8,36 +7,33 @@ public:
         for(auto it : flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
+        queue<pair<int,pair<int,int>>> q;
+        q.push({src , {0 , 0}}); // <node,steps,cost>
         
-        vector<int> disArr(n,1e9);
+        vector<int> disArr(n , 1e9);
         disArr[src] = 0;
-        
-        queue<pair<int,pair<int,int>>> q;// <stops , node , dist>
-        q.push({0 ,{ src , 0}});
         
         while(!q.empty()){
             auto front = q.front();q.pop();
-            int stops = front.first;
-            int node = front.second.first;
-            int cost = front.second.second;
             
-            if(stops > k) continue;
+            int node = front.first;
+            int steps = front.second.first;
+            int costTillNode = front.second.second;
+            
+            if(steps > k) continue;
             
             for(auto it : adj[node]){
                 int adjNode = it.first;
-                int edW = it.second;
+                int adjW = it.second;
                 
-                if(stops <= k && cost + edW < disArr[adjNode]){
-                    disArr[adjNode] = cost + edW;
-                    q.push({stops+1,{adjNode,cost+edW}});
+                if(steps <= k && adjW + costTillNode < disArr[adjNode]){
+                    disArr[adjNode] = adjW + costTillNode;
+                    q.push({adjNode,{steps+1,adjW + costTillNode}});
                 }
                 
             }
-            
         }
-        
-        if(disArr[dst] == 1e9) return -1;
-        
+         if(disArr[dst] == 1e9) return -1;
         return disArr[dst];
         
     }
