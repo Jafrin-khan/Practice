8868c2e5114041594eafbe3bf6c9d0b483c50f02
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-22 14:37:38 +0530

    Time: 22 ms (30.55%), Space: 9 MB (39.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,6 +5,7 @@ public:
     //TC = O(3^N * 3^M)
     //SC = O(N+M)
     
+    /*
     //Memoised
     //TC = O(N*M)
     //SC = O(N*M) + O(N+M)
@@ -35,5 +36,36 @@ public:
         vector<vector<int>> dp(n+1 , vector<int>(m+1 , -1));
         
         return f(n-1 , m-1 , s1 , s2 , dp);
+    }
+    */
+    
+    //Tabulation
+    //TC = O(N*M)
+    //SC = O(N*M) 
+   
+    int minDistance(string s1, string s2) {
+        
+        int n = s1.size();
+        int m = s2.size();
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , 0));
+        
+        for(int j = 0 ; j <= m ; j++) dp[0][j] = j;
+        for(int i = 0 ; i <= n ; i++) dp[i][0] = i;
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j =1 ; j <= m ; j++){
+                if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
+        
+                else{
+                    int insert = 1 + dp[i][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+
+                    dp[i][j] = min({insert,del,replace});
+                } 
+            }
+        }
+        
+        return dp[n][m];
     }
 };
