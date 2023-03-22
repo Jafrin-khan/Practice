c9165a45b97d9ccd9274f98c755a9a90f25cd0b4
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-22 14:03:15 +0530

    Time: 74 ms (12.91%), Space: 80.8 MB (8.97%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    //Recursion
+    //TC = O(3^N * 3^M)
+    //SC = O(N+M)
+    
+    int f(int i , int j , string s1, string s2 , vector<vector<int>>& dp){
+        
+        if(i < 0) return j+1;
+        if(j < 0) return i+1;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s1[i] == s2[j]) return dp[i][j] = f(i-1 , j-1 , s1 , s2 , dp);
+        
+        else{
+            int insert = 1 + f(i , j-1 , s1 , s2 , dp);
+            int del = 1 + f(i-1 , j , s1 , s2 , dp);
+            int replace = 1 + f(i-1 , j-1 , s1 , s2 , dp);
+            
+            return dp[i][j] = min({insert,del,replace});
+        }
+    }
+    
+    int minDistance(string s1, string s2) {
+        
+        int n = s1.size();
+        int m = s2.size();
+        
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , -1));
+        
+        return f(n-1 , m-1 , s1 , s2 , dp);
+    }
+};
