937ea821878facd0a9e239e34fe91942dbd5019a
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-24 06:24:11 +0530

    Time: 24 ms (36.66%), Space: 18.6 MB (55.69%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    /*
+    TC = O(N*logN)
+    SC = O(1)
+    */
+    bool isH_Index(int h , vector<int>& citations){
+        
+        int cnt = citations.size();
+        for(auto it : citations){
+            if(it < h) cnt--;
+            if(cnt < h) return false;
+        }
+        return true;
+    }
+    
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        if(n == 1) return min(citations[0] , 1);
+        
+        int low = 0 , high = n;
+        int ans;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(isH_Index(mid , citations)){
+                ans = mid;
+                low = mid+1;
+            }
+            else high = mid-1;
+        }
+        
+        return ans;
+    }
+};
