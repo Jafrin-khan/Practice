c4874a7014e04599fe3da88cd067092a4421ab6c
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-24 15:59:09 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -9,42 +9,38 @@ using namespace std;
 
 class Solution {
   public:
-  
   /*
-  TC = O(N^3)
+  TC = O(V^3)
+  SC = O(1)
   */
 	void shortest_distance(vector<vector<int>>&matrix){
-	    // Code here
-	    
-	    int n = matrix.size();
-	    
-	    for(int i = 0 ; i < n ; i++){
-	        for(int j = 0 ; j < n ; j++){
-	            if(matrix[i][j] == -1){
-	                matrix[i][j] = 1e9;
-	            }
-	            
-	            if(i == j){
-	                matrix[i][j] = 0;
-	            }
-	        }
-	    }
-	    
-	    for(int via = 0 ; via < n ; via++){
-	        for(int i = 0 ; i < n ; i++){
-	            for(int j = 0 ; j < n ; j++){
-	                matrix[i][j] = min(matrix[i][j] , matrix[i][via] + matrix[via][j]);
-	            }
-	        }
-	    }
-	    
-	    for(int i = 0 ; i < n ; i++){
-	        for(int j = 0 ; j < n ; j++){
-	            if(matrix[i][j] == 1e9){
-	                matrix[i][j] = -1;
-	            }
-	        }
-	    }
+	     
+	     int n = matrix.size();
+	     
+	      for(int i = 0 ; i < n ; i++){
+	             for(int j = 0 ; j < n ; j++){
+	                 if(matrix[i][j] == -1) matrix[i][j] = 1e9;
+	                 if (i == j) matrix[i][j] = 0; //ye waali condition mt bhulna
+	             }
+	             
+	         }
+	     
+	     
+	     
+	     for(int k = 0 ; k < n ; k++){
+	         for(int i = 0 ; i < n ; i++){
+	             for(int j = 0 ; j < n ; j++){
+	                 matrix[i][j] = min(matrix[i][j] , matrix[i][k] + matrix[k][j]);
+	             }
+	         }
+	     }
+	     
+	     for(int i = 0 ; i < n ; i++){
+	             for(int j = 0 ; j < n ; j++){
+	                 if(matrix[i][j] == 1e9) matrix[i][j] = -1;
+	             }
+	         }
+	     
 	}
 };
 
