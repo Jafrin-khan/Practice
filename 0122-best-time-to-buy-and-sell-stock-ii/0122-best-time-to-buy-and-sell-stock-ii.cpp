e8b4cd98a814c46c537610d18659ee311fff00b0
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-24 12:16:46 +0530

    Time: 19 ms (9.11%), Space: 15.3 MB (21.84%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -60,23 +60,7 @@ public:
      //Tabulated
     //TC = O(N*2)-->O(N)
     //SC = O(N*2) ~ O(N) + O(N) --> O(N)
-    
-    int f(int ind , int buy , vector<int>& prices , int n , vector<vector<int>>& dp){
-        
-        if(ind == n) return 0;
-        
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-        
-        int profit = 0;
-        
-        if(buy){       
-            profit = max(-prices[ind] + f(ind+1 , 0 , prices , n , dp) , 0 + f(ind+1 , 1 , prices , n , dp));//max(buy,notBuy);
-        }
-        else profit = max(prices[ind] + f(ind+1 , 1 , prices , n , dp) , 0 + f(ind+1 , 0 , prices , n , dp));//max(sell,notSell)
-        
-        return dp[ind][buy] = profit;
-    }
-    
+ 
     int maxProfit(vector<int>& prices) {
        
         int n = prices.size();
