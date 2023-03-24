7d9fbe44d2ad33777a47093d7690da8c423bb17a
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-24 17:07:17 +0530

    Time: 92 ms (72.84%), Space: 12.6 MB (51.13%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -4,6 +4,7 @@ public:
         
         vector<vector<int>> disMatrix(n , vector<int>(n , 1e9));
         
+        //yhn adjacency matrix bnaare lekin usse disMatrix naam dere kyunki min dis nikaalna h to samajhne m easy
         for(auto it : edges){
             int u = it[0];
             int v = it[1];
@@ -13,8 +14,10 @@ public:
             disMatrix[v][u] = wt;
         }
         
+        //diagonal elements zero hi hoteyy hai
         for(int i = 0 ; i < n ; i++) disMatrix[i][i] = 0;
         
+        //simple Floyd wrshall algo to fill disMatrix with shortest distances
         for(int k = 0 ; k < n ; k++){
             for(int i = 0 ; i < n ; i++){
                 for(int j = 0 ; j < n ; j++){
@@ -24,9 +27,11 @@ public:
             }
         
         
+        //cntCity is max possible initially while cityNum is not known to us hence -1
         int cntCity = n;
         int cityNum = -1;
         
+       // again iterating in disMatrix aur hum wo city dhundre jiske neighbours ka distance distanceThreshold se kam hai....fir iske cnt ko maxCnt yaani cntCity se compare krke hum min cnt maintain krre hai aur corresponding city ki value store krte jaare h
         for(int i = 0 ; i < n ; i++){
             int cnt = 0;
             for(int j = 0 ; j < n ; j++){
