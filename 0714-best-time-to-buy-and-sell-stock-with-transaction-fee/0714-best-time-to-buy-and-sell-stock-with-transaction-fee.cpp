b005dbb1a785c7a1017b7c8b531c337c3e526aac
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-25 12:28:26 +0530

    Time: 216 ms (57.11%), Space: 91 MB (43.88%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -23,6 +23,8 @@ public:
         
     }*/
     
+    /*
+    
      //Memoised
     //TC = O(N*2)
     //SC = O(N*2) + O(N+2)
@@ -46,6 +48,32 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n , vector<int>(2,-1));
         return f(0 , 1 , prices , fee , n , dp);
+        
+    }
+    */
+    
+     //Tabulated
+    //TC = O(N*2)
+    //SC = O(N*2)
+    
+
+    int maxProfit(vector<int>& prices, int fee) {
+        
+        int n = prices.size();
+        vector<vector<int>> dp(n+1 , vector<int>(2,0));
+        
+        for(int ind = n-1 ; ind >= 0 ; ind--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+                 int profit = 0;
+        
+                if(buy)  profit = max(-prices[ind] + dp[ind+1][0] , 0 + dp[ind+1][1]);
+                else profit = max(prices[ind] + dp[ind+1][1]-fee, 0 + dp[ind+1][0]); //jab sell krenge tab transaction complete hogi
+
+                dp[ind][buy] = profit;
+            }
+        }
+        
+        return dp[0][1];
         
     }
 };
