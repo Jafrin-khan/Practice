5aa3d7dcc820a0e5b4413cae6f9ed75f272ca410
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-25 10:04:06 +0530

    Time: 36 ms (92.59%), Space: 17.7 MB (59.83%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.cpp
@@ -5,17 +5,16 @@ public:
         vector<vector<int>> ans;
         vector<vector<int>> vis(n , vector<int>(m,0));
         
-        queue<pair<int,pair<int,int>>> q;//<steps , <i,j>>
-        q.push({0 , {sr , sc}});
+        queue<pair<int,int>> q;//<i,j>
+        q.push({sr , sc});
         vis[sr][sc] = 1;
         
         int dx[4] = {-1 , 1 , 0 , 0};
         int dy[4] = {0 , 0 , -1 , 1};
         
         while(!q.empty()){
-            int steps = q.front().first;
-            int x = q.front().second.first;
-            int y = q.front().second.second;q.pop();
+            int x = q.front().first;
+            int y = q.front().second;q.pop();
             
             ans.push_back({x,y});
             
@@ -24,7 +23,7 @@ public:
                 int ny = y + dy[i];
                 if(nx >= 0 && ny >= 0 && nx < n && ny < m && !vis[nx][ny]){
                     vis[nx][ny] = 1;
-                    q.push({steps+1 , {nx,ny}});
+                    q.push({nx,ny});
                 }
             }
             
