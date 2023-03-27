f93e22c65e527578b0e6de79a27f31a08853cec1
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-27 15:54:11 +0530

    Time: 341 ms (52.53%), Space: 10.5 MB (58.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-    /*
-     //Recursion
-    //TC = O(2^N)
-    //SC = O(N)
-    int f(int ind , int prev_ind , int n , vector<int>& nums){
-        if(ind == n) return 0;
-        
-        int take = 0;
-        int notTake = 0;
-        if(prev_ind == -1 || nums[ind] > nums[prev_ind]) take = 1 + f(ind+1 , ind , n , nums);
-        notTake = 0 + f(ind+1 , prev_ind , n , nums); // ye statement else mai ni daalni...V.IMP to remember
-        
-        return max(take , notTake);
-    }
+    
+    //recursing , memo , tabulated pichle submission m hai explanation k saath
+    
+    //EASY TRICK FOR LIS WITH O(N) SC
     
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        vector<int> dp(n , 1);
         
-        return f(0 , -1 , n , nums);
-    }*/
-    
-    
-    //Memoised
-    //TC = O(N*N)
-    //SC = O(N/N) + O(N)
-    int f(int ind , int prev_ind , int n , vector<int>& nums , vector<vector<int>>& dp){
-        if(ind == n) return 0;
+        int ans = 0;
         
-        if(dp[ind][prev_ind+1] != -1) return dp[ind][prev_ind+1];
+        for(int i = 0 ; i < n ; i++){
+            for(int prev = 0 ; prev <= i-1 ; prev++){
+                if(nums[prev] < nums[i]) dp[i] = max(dp[i] , 1 + dp[prev]);
+            }
+        }
         
-        int take = 0;
-        int notTake = 0;
-        if(prev_ind == -1 || nums[ind] > nums[prev_ind]) take = 1 + f(ind+1 , ind , n , nums , dp);
-        notTake = 0 + f(ind+1 , prev_ind , n , nums , dp); // ye statement else mai ni daalni...V.IMP to remember
+        for(int i = 0 ; i < n ; i++) ans = max(ans , dp[i]);
         
-        return dp[ind][prev_ind+1] = max(take , notTake);
-    }
-    
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        
-        vector<vector<int>> dp(n , vector<int>(n+1 , -1));
-        return f(0 , -1 , n , nums , dp);
+        return ans;
     }
 };
