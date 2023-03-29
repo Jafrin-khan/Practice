8b46c1bb3579f35827486cae4bcbda9793e529b9
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-29 16:31:20 +0530

    Time: 50 ms (79.29%), Space: 9 MB (20.91%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    
+    /*
+Time Complexity: O(N*N)
+
+Reason: There are two nested loops.
+
+Space Complexity: O(N)
+
+Reason: We are only using two rows of size n.
+    */
+    
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        
+        sort(nums.begin() , nums.end());
+        int n = nums.size();
+        
+        vector<int> dp(n , 1);
+        vector<int> hash(n);
+        
+       
+        for(int ind = 0 ; ind < n ; ind++){
+            hash[ind] = ind;
+            for(int prev = 0 ; prev <= ind-1 ; prev++){
+                if((nums[ind]%nums[prev] == 0) && dp[ind] < dp[prev]+1){
+                    dp[ind] = dp[prev]+1;
+                    hash[ind] = prev;
+                }
+            }
+            
+        }
+        int lastIndex = -1;
+        int cmp = -1;
+        
+        for(int ind = 0 ; ind < n ; ind++){
+            if(dp[ind] > cmp){
+                cmp = dp[ind];
+                lastIndex = ind;
+            }
+        }
+        
+        
+        vector<int> ans;
+        ans.push_back(nums[lastIndex]);
+        while(lastIndex != hash[lastIndex]){
+            lastIndex = hash[lastIndex]; 
+            ans.push_back(nums[lastIndex]);
+        }
+        
+        reverse(ans.begin() , ans.end());
+        return ans;
+    }
+};
