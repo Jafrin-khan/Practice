eb9e2e03aaca8bcfb705b8c6b4fb0ddcd4fac523
Jafrin <khan.jafrin.9346@gmail.com>
2023-03-31 12:19:06 +0530

    Time: 45 ms (76.70%), Space: 13.2 MB (82.22%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
-    
-    int largestRectangleArea(vector<int>& heights) {
+    /*
+    TC = O(N*(M+N))
+    SC = O(N)
+    */
+    int largestRectangleHisto(vector<int>& histo) {
         
+        int n = histo.size();
         stack<int> st;
-        int n = heights.size();
+        
         int ans = 0;
+        
         for(int i = 0 ; i <= n ; i++){
-            while(!st.empty() && (i == n || heights[st.top()] > heights[i])){
-                int h = heights[st.top()];
+            while( !st.empty() && (i == n || histo[st.top()] >= histo[i])){
+                
+                int height = histo[st.top()];
                 st.pop();
+                
                 int width;
-                if(st.empty()){
-                    width = i;
-                }
-                else{
-                    width = i - st.top() - 1;
-                }
-                ans = max(ans , h*width);     
+                if(st.empty()) width = i;
+                else width = i - st.top()-1;
+                
+                ans = max(ans , height*width);
             }
             st.push(i);
         }
+        
         return ans;
     }
     
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix[0].size();
-          vector<int> heights(n,0);
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<int> histo(m , 0);
         int ans = 0;
-        for(int i = 0 ; i < matrix.size() ; i++){
-            for(int j = 0 ; j < matrix[0].size() ; j++){
-                if(matrix[i][j] == '1') heights[j]++;
-                else heights[j] = 0; //rememebr this else statement...very imp
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(matrix[i][j] == '1') histo[j]++;
+                else histo[j] = 0;
             }
-             ans = max(ans , largestRectangleArea(heights));
+            
+            ans = max(ans , largestRectangleHisto(histo));
         }
         
         return ans;
