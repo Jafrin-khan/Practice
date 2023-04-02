d1258bb17d979c886229517204e063c7d0088dd4
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-02 12:15:51 +0530

    Added solution - LeetHub

--- Tywin's War Strategy - GFG/tywins-war-strategy.cpp
+++ Tywin's War Strategy - GFG/tywins-war-strategy.cpp
@@ -43,6 +43,14 @@ int min_soldiers(int arr[], int n, int k){
     sort(arr , arr + n);//pehle se sort krne ka faayda ni is particular position pe sort krna hoga coz hmein resultant mod vals ko sort krna hai
     
     int i = n-1;
+    //loop usli direction m islie chlaare bcoz of the case :
+/*
+    2 17
+    9 11
+    
+Your Code's output is: 8 //agr hum i = 0 se iterate kreyy
+It's Correct output is: 6
+*/
     while(i >= 0){
            if(arr[i] != 0) {//mod ki wajah se divisible waale sb zero ho gye aur waise bhi wo pehle hi cnt m consider kr lie theyy humne
                ans += (k-arr[i]);
