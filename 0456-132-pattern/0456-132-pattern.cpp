928c68b4943c81bbbdb59d5c810ca672bb325f54
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-05 17:42:20 +0530

    Time: 76 ms (68.44%), Space: 37 MB (95.49%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -7,17 +7,17 @@ public:
     bool find132pattern(vector<int>& nums) {
         
         int n = nums.size();
-        stack<pair<int , int>> st; //<num , minLeft>
+        stack<pair<int , int>> st; //<num , minLeft>.....<3,1>
         
         int currMin = nums[0];
         
-        for(int i = 1 ; i < n ; i++){
+        for(int i = 1 ; i < n ; i++){//hum 2nd no of dhundre hai looping krke
             
-            while(!st.empty() && st.top().first <= nums[i]){
+            while(!st.empty() && st.top().first <= nums[i]){//yahan pattern ke 3 se compare krre
                 st.pop();
             }
             
-            if(!st.empty() && st.top().second < nums[i]){
+            if(!st.empty() && st.top().second < nums[i]){//yahan pattern ke 1 se compare krre
                 return true;
             }
             
