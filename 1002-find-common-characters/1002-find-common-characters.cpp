45547e323b5a6ef7097547de96328707900b609d
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-05 15:53:24 +0530

    Time: 7 ms (84.10%), Space: 9.7 MB (54.81%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -1,58 +1,29 @@
 class Solution {
 public:
-    
-    /*remember imp. trick in these type of questions...ek iske simailar common int in matrix krke ek ques h wo bhi bhaut imp h...in dono k logic*/
-    
     vector<string> commonChars(vector<string>& words) {
         
-       /*
-       YE METHOD GFG K LIYE OPTIMUM H YHA NI ..KYU YE DRY RUN KRKE DEKHO
-       unordered_map<char,int> mp;
-        
-        for(int i = 0 ; i < words.size() ; i++){
-            for(int j = 0 ; j < words[i].size() ; j++){
-                if(mp[words[i][j]] == i){
-                    mp[words[i][j]]++;
-                }
-            }
-        }
-        
         vector<string> ans;
-        for(auto it : mp){
-            if(it.second == words.size()){
-                string s = "";
-                s += it.first;
-                ans.push_back(s);
-            }
-        }
-        return ans;
-        */
         
-        vector<int> cnt(26,0);
-        for(auto c : words[0]){
-            cnt[c-'a']++;
-        }
+        vector<int> original(26,0);
         
-        for(int i = 1 ; i < words.size() ; i++){
-            vector<int> v(26,0);
+        for(auto it : words[0]) original[it-'a']++;
+        
+        for(auto word : words){
+            vector<int> copy(26,0);
+            for(auto it : word) copy[it - 'a']++;
             
-            for(auto c : words[i]){
-                v[c-'a']++;
-            }
-            
-            for(int j = 0 ; j < 26 ; j++){
-                cnt[j] = min(cnt[j],v[j]);
+            for(int i = 0 ; i < 26 ; i++){
+            original[i] = min(original[i] , copy[i]);
             }
         }
-        
-        vector<string> ans;
         
         for(int i = 0 ; i < 26 ; i++){
-            while(cnt[i]--){
+            int cnt = original[i];
+            while(cnt--){
                 string s = "";
-                s+=(i+'a');
+                s += (i+'a');
                 ans.push_back(s);
-            }
+            }    
         }
         
         return ans;
