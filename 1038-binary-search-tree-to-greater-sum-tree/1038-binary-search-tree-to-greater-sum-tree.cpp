da7c74eb267243e253dcaf2438fc4829e1bfc0aa
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-07 16:45:16 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (70.63%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,45 +11,18 @@
  */
 class Solution {
 public:
-    /*
-    TC = O(N)
-    SC = O(H) ...... If we don’t consider the size of the stack for function calls then O(1) otherwise O(h) where h is the height of the tree.   
-    */
     
-    void sumOfAllNodes(TreeNode* root , int& sum){
-        
-        if(root == NULL){
-            return;
-        }
-        
+    void f(TreeNode* root , int& sum){
+        if(root == NULL) return;
+       
+        f(root->right , sum);
         sum += root->val;
-        
-        sumOfAllNodes(root->left , sum);
-        sumOfAllNodes(root->right , sum);
-        
-    }
-    
-    void calculate(TreeNode* root , int& sum){
-        
-        if(root == NULL){
-            return;
-        }
-        //simple inorder
-        calculate(root->left , sum);
-        
-        int x = root->val;
         root->val = sum;
-        sum -= x;
-        
-        calculate(root->right , sum);
+        f(root->left , sum);
     }
-    
     TreeNode* bstToGst(TreeNode* root) {
-        
         int sum = 0;
-        sumOfAllNodes(root , sum);
-        calculate(root , sum);
-        
+        f(root , sum);
         return root;
     }
 };
