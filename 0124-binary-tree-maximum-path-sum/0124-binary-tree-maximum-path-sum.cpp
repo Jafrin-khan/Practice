f3bdbe637415032837779f1c66325628b229eff0
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-10 22:22:42 +0530

    Time: 26 ms (67.17%), Space: 28.6 MB (7.23%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,57 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     
-/*
-
-                 -10   m=42 , s = 25
-                /    \ 
-             9        20   m=42 , s=35                     15,20,7 = 42
-           s=9       /  \
-           m=9     15    7
-                   m=15    m=7
-                   s=15     s=7
-
-*/
-    
-    /*
-Time Complexity: O(N).
-
-Reason: We are doing a tree traversal.
-
-Space Complexity: O(N)
-
-Reason: Space is needed for the recursion stack. In the worst case (skewed tree), space complexity can be O(N).
-    */
     int f(TreeNode* root , int& maxSum){
+        if(root == NULL) return 0;
         
-        if(root == NULL){
-            return 0;
-        }
+        int sumL = max({0 , f(root->left , maxSum)});
+        int sumR = max({0 , f(root->right , maxSum)});
         
-        int sumLeft = max(0 , f(root->left , maxSum));
-        int sumRight = max(0 , f(root->right , maxSum));
+        maxSum = max(maxSum , sumL + sumR + root->val);
         
-        maxSum = max(maxSum , sumLeft + sumRight + root->val);
-        
-        return max(sumLeft , sumRight) + root->val;
-        
+        return max(sumL , sumR) + root->val;
     }
     int maxPathSum(TreeNode* root) {
-        
-        if(root == NULL){
-            return 0;
-        }
         
         int maxSum = -1e9;
         f(root , maxSum);
