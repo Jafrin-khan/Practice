4c45314fd5a8041c9ac3bb5f670e13bd4f958164
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-12 22:54:46 +0530

    Time: 7 ms (65.24%), Space: 12.4 MB (94.94%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,15 +1,6 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-       vector<int>v;
+    vector<int>v;
     //CASE 1 ---> When we are checking in subtree of target
     void checkSubTree(TreeNode* root,TreeNode* target,int k){
         if(!root || k<0)return ;
@@ -21,11 +12,12 @@ public:
     //CASE 2 ---> When we are checking in parent/ancestor subtree of target
     int checkParent(TreeNode* root,TreeNode* target,int k){
         if(!root)return -1;
+        //simple subtrees pe search maarre hai yhan
         if(root==target){
             checkSubTree(root,target,k);
             return 0;
         }
-        int left_distance=checkParent(root->left,target,k); //equation 1
+        int left_distance=checkParent(root->left,target,k); ////i.e we found our target in left subtree
         if(left_distance!=-1){//i.e we found our target in left subtree
             if(left_distance+1==k)v.push_back(root->val);
             else checkSubTree(root->right,target,k-left_distance-2); //-2 because we ignore the distance between root to root->right here which is one unit and also ignore one unit diatsnce from root to root->left in ewuation 1
