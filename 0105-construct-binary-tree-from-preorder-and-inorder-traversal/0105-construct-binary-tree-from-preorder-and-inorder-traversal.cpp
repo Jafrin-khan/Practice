40cc7f434d87af8d4139dbf47311250091dff515
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-14 22:47:55 +0530

    Time: 28 ms (48.03%), Space: 26 MB (83.78%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,48 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     
-    /*
-    TC = 
-    SC = 
-    */
-    
     TreeNode* f(int& ind , int si , int ei , vector<int>& preorder, vector<int>& inorder){
         
-        if(si > ei){
-            return NULL;
-        }
+        if(si > ei) return NULL;
+        
+        int value = preorder[ind];
+        TreeNode* root = new TreeNode(value);
+        ind++;
         
         int idx;
         for(int i = si ; i <= ei ; i++){
-            if(inorder[i] == preorder[ind]){
+            if(inorder[i] == value){
                 idx = i;
                 break;
             }
         }
         
-        TreeNode* node = new TreeNode(preorder[ind]);
-        ind++;
+        root->left = f(ind , si , idx-1 , preorder , inorder);
+        root->right = f(ind , idx+1 , ei , preorder , inorder);
         
-       
-        node->left = f(ind , si , idx-1 , preorder , inorder);
-        node->right = f(ind , idx+1 , ei , preorder , inorder);
-        
-        return node;
+        return root;
         
     }
+    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        
         int ind = 0;
         return f(ind , 0 , inorder.size()-1 , preorder , inorder);
     }
