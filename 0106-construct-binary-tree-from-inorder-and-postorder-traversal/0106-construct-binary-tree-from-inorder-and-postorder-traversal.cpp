a232ef18cba1f9375a2ec8b9d4ae5eb02d978d48
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-14 23:09:13 +0530

    Time: 34 ms (26.10%), Space: 26 MB (73.53%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,49 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-     /*
-    TC = 
-    SC = 
-    */
-    
-    TreeNode* f(int& ind , int si , int ei , vector<int>& postorder, vector<int>& inorder){
-        
-        if(si > ei){
-            return NULL;
-        }
+    TreeNode* f(int& ind , int si , int ei , vector<int>& inorder, vector<int>& postorder){
+        if(si > ei) return NULL;
+        int value = postorder[ind];
+        ind--;
+        TreeNode* root = new TreeNode(value);
         
         int idx;
         for(int i = si ; i <= ei ; i++){
-            if(inorder[i] == postorder[ind]){
+            if(inorder[i] == value){
                 idx = i;
                 break;
             }
         }
         
-        TreeNode* node = new TreeNode(postorder[ind]);
-        ind--;
+        root->right = f(ind , idx+1 , ei , inorder , postorder);
+        root->left = f(ind , si , idx-1 , inorder , postorder);
         
-       //remeber to call node->right first in postorder.......vimp
-        node->right = f(ind , idx+1 , ei , postorder , inorder);
-        node->left = f(ind , si , idx-1 , postorder , inorder);
-        
-        
-        return node;
-        
+        return root;  
     }
-   
+    
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int ind = postorder.size()-1;
-        return f(ind , 0 , inorder.size()-1 , postorder , inorder);
+        return f(ind , 0 , inorder.size()-1 , inorder , postorder);
     }
 };
