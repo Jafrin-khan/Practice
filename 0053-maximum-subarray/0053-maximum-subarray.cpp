0397f3eb719497909f9c5c25d5d8570fef94d4ea
Jafrin <khan.jafrin.9346@gmail.com>
2023-04-21 09:30:46 +0530

    Time: 111 ms (78.01%), Space: 67.8 MB (62.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
     
-    /********************Kadan's Algo********************/
+    //see submission of 01/09/2023
     
-    /*
-    TC = O(N)
-    SC = O(1)
-    */    
-    //remeber difference b/w subarray , subset etc
     int maxSubArray(vector<int>& nums) {
         
-        int n = nums.size();
-        
-        if(n == 1){
-            return nums[0];
-        }
-        
-        //remember to initialise vars with nums[0] otherwise the algorithm wont work
         int currSum = nums[0];
         int maxSum = nums[0];
         
         for(int i = 1 ; i < nums.size() ; i++){
             
-            if(currSum < 0){
-                currSum = 0;
-            }
-            
+            if(currSum < 0) currSum = 0;
             currSum += nums[i];
             maxSum = max(maxSum , currSum);
         }
         
         return maxSum;
-        
     }
 };
