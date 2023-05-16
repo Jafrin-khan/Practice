2ed03ff72b025fcce45b37f54199acce16a11900
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-16 11:04:34 +0530

    Time: 5 ms (77.55%), Space: 13.7 MB (82.94%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    
-    /*
-    TC = O(N)
-    SC = O(1)
-    */
     int maxProduct(vector<int>& nums) {
         
-        int maxProduct = 1;
-        int minProduct = 1;
-        int ans = -1e9;
+        int n = nums.size();
         
-        for(int i = 0 ; i < nums.size() ; i++){
-            //for considering -ve*-ve as well
-            int temp = maxProduct;
-            maxProduct = max({temp*nums[i] ,minProduct*nums[i] , nums[i]});
-            minProduct = min({temp*nums[i] , minProduct*nums[i], nums[i]});
-            ans = max(ans , maxProduct);
+        int prod = 1;
+        int maxLeft = -1e9;
+        
+        bool isZero = false;
+        
+        for(int i = 0 ; i < n ; i++){
+            prod*=nums[i];
+            if(nums[i] == 0){
+                isZero = true;
+                prod = 1;
+                continue;
+            }
+            
+            maxLeft = max(maxLeft ,prod);
         }
         
-        return ans;
+        prod = 1;
+        int maxRight = -1e9;
+        for(int i = n-1 ; i >= 0 ; i--){
+            prod*=nums[i];
+            if(nums[i] == 0){
+                isZero = true;
+                prod = 1;
+                continue;
+            }
+            
+            maxRight = max(maxRight , prod);
+        }
+        
+        if(isZero) return max({maxLeft , maxRight , 0});
+        
+        return max(maxLeft , maxRight);
         
     }
 };
