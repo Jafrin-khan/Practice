8dca7eb91fb8165694d7befbe234b2c1ae804279
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-17 10:39:46 +0530

    Time: 345 ms (5.02%), Space: 400.2 MB (5.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     
-    //Recursion
-    //TC = O(3^N * 3^M)
-    //SC = O(N+M)
-    
-    /*
-    //Memoised
-    //TC = O(N*M)
-    //SC = O(N*M) + O(N+M)
+    //see 03/22/2023 submission
     
     int f(int i , int j , string s1, string s2 , vector<vector<int>>& dp){
         
@@ -17,14 +10,13 @@ public:
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(s1[i] == s2[j]) return dp[i][j] = f(i-1 , j-1 , s1 , s2 , dp);
-        
+        if(s1[i] == s2[j]) return 0 + f(i-1 , j-1 , s1 , s2 , dp);
         else{
             int insert = 1 + f(i , j-1 , s1 , s2 , dp);
             int del = 1 + f(i-1 , j , s1 , s2 , dp);
-            int replace = 1 + f(i-1 , j-1 , s1 , s2 , dp);
+            int rep = 1 + f(i-1 , j-1 , s1 , s2 , dp);
             
-            return dp[i][j] = min({insert,del,replace});
+            return dp[i][j] = min({insert , del , rep});
         }
     }
     
@@ -33,39 +25,9 @@ public:
         int n = s1.size();
         int m = s2.size();
         
-        vector<vector<int>> dp(n+1 , vector<int>(m+1 , -1));
+        vector<vector<int>> dp(n , vector<int>(m,-1));
         
         return f(n-1 , m-1 , s1 , s2 , dp);
-    }
-    */
-    
-    //Tabulation
-    //TC = O(N*M)
-    //SC = O(N*M) 
-   
-    int minDistance(string s1, string s2) {
         
-        int n = s1.size();
-        int m = s2.size();
-        vector<vector<int>> dp(n+1 , vector<int>(m+1 , 0));
-        
-        for(int j = 0 ; j <= m ; j++) dp[0][j] = j;
-        for(int i = 0 ; i <= n ; i++) dp[i][0] = i;
-        
-        for(int i = 1 ; i <= n ; i++){
-            for(int j =1 ; j <= m ; j++){
-                if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1];
-        
-                else{
-                    int insert = 1 + dp[i][j-1];
-                    int del = 1 + dp[i-1][j];
-                    int replace = 1 + dp[i-1][j-1];
-
-                    dp[i][j] = min({insert,del,replace});
-                } 
-            }
-        }
-        
-        return dp[n][m];
     }
 };
