fe07c16aa89f0f58e2fb45c0b0d6630dd4baf2d8
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-19 22:39:06 +0530

    Time: 25 ms (49.76%), Space: 14.6 MB (25.89%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,51 +1,43 @@
 class Solution {
-public:
- /*
-Time Complexity: O(V+E), where V = no. of nodes and E = no. of edges. This is a simple BFS algorithm.
-
-Space Complexity: O(N) + O(N) ~ O(2N), O(N) for the indegree array, and O(N) for the queue data structure used in BFS(where N = no.of nodes). Extra O(N) for storing the topological sorting. Total ~ O(3N).
+public: 
     
-*/
-    //By toposort we detect whether cycle is present or not...it can also be implemented via dfs
+    bool isCycle(int v , vector<int>& vis , vector<vector<int>>& adj  , vector<int>& dfsVis){
+            vis[v] = 1;
+            dfsVis[v] = 1;
+            for(auto it : adj[v]){
+                if(!vis[it]){
+                    if(isCycle(it , vis , adj ,  dfsVis)) return true;
+                }
+                
+                else{
+                    if(dfsVis[it] == 1) return true;
+                }
+            }
+        
+        dfsVis[v] = 0;
+        
+        return false;
+    }
     
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         
-         vector<int> adj[n];
+        vector<vector<int>> adj(n);
         
-        for(int i = 0 ; i < prerequisites.size() ; i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj[v].push_back(u);
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            
+            adj[v].push_back(u);//pehle v fulfilled hona chahie tb jaake u course kr skte
         }
         
-        vector<int> indegree(n,0);
+        vector<int> vis(n,0);
+        vector<int> dfsVis(n,0);
         
         for(int i = 0 ; i < n ; i++){
-            for(auto it : adj[i]) indegree[it]++;
-        }
-        
-        int cnt = 0;
-        queue<int> q;
-        
-        for(int i = 0 ; i < n ; i++){
-            if(indegree[i] == 0){
-                q.push(i);
+            if(!vis[i]){
+                if(isCycle(i,  vis , adj , dfsVis)) return false;
             } 
         }
-        
-        while(!q.empty()){
-            int node = q.front();q.pop();
-            cnt++;
-            
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0) {
-                    q.push(it);
-                }
-            }
-        }
-        
-        return cnt == n;
-        
+        return true;
     }
 };
