565c26519bb0ce8e856b67dae78ce54e2d7d15f1
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-23 21:17:22 +0530

    Time: 49 ms (18.22%), Space: 71.4 MB (43.49%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,92 +10,27 @@
  */
 class Solution {
 public:
-    /*
-    Time Complexity: O(max(m,n)). Assume that m and n represent the length of l1 and l2 respectively, the algorithm above iterates at most max(m,n) times.
-
-Space Complexity: O(max(m,n)). The length of the new list is at most max(m,n)+1.
-    */
-  
+   
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        if(l1 == NULL){
-            return l2;
-        }
-        
-        if(l2 == NULL){
-            return l1;
-        }
-        
+        ListNode* ans = new ListNode(0);
+        ListNode* newHead = ans;
         int carry = 0;
-        ListNode* nH = new ListNode(0);
-        ListNode* nT = nH;
         
-        while(l1 != NULL && l2 != NULL){
-            int sum = l1->val + l2->val + carry;
+        while(l1 || l2 || carry){
+            int sum = 0;
+            if(l1) {sum += l1->val;l1 = l1->next;}
+            if(l2) {sum += l2->val; l2 = l2->next;}
+            if(carry) sum += carry;
+            
             carry = sum/10;
-            sum = sum%10;
+            sum%=10;
             
-            nT->next = new ListNode(sum);
-            nT = nT->next;
-            
-            l1 = l1->next;
-            l2 = l2->next;
+            ans->next = new ListNode(sum);
+            ans = ans->next;
+        
         }
         
-        while(l1 != NULL){
-            int sum = l1->val + carry;
-            carry = sum/10;
-            sum = sum%10;
-            
-            nT->next = new ListNode(sum);
-            nT = nT->next;
-            
-            l1 = l1->next;
-        }
-        
-         while(l2 != NULL){
-            int sum =  l2->val + carry;
-            carry = sum/10;
-            sum = sum%10;
-            
-            nT->next = new ListNode(sum);
-            nT = nT->next;
-            
-            l2 = l2->next;
-        }
-        
-        if(carry){
-            nT->next = new ListNode(carry);
-            nT = nT->next;
-        }
-        
-        return nH->next;
-        
-        /*ye short code bhi dekho accha hai...logic same h bs OR condition daalke better bna diya hai
-        
-        ListNode *dummy = new ListNode(); 
-        ListNode *temp = dummy; 
-        int carry = 0;
-        while( (l1 != NULL || l2 != NULL) || carry) {
-            int sum = 0; 
-            if(l1 != NULL) {
-                sum += l1->val; 
-                l1 = l1 -> next; 
-            }
-            
-            if(l2 != NULL) {
-                sum += l2 -> val; 
-                l2 = l2 -> next; 
-            }
-            
-            sum += carry; 
-            carry = sum / 10; 
-            ListNode *node = new ListNode(sum % 10); 
-            temp -> next = node; 
-            temp = temp -> next; 
-        }
-        return dummy -> next; 
-        */
-        
+        return newHead->next;
     }
 };
