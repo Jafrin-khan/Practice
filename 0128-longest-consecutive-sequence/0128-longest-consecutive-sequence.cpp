63353395baf5c39acafa1a273ec5724921da040d
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-26 20:25:27 +0530

    Time: 845 ms (11.69%), Space: 67 MB (11.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    /*
+    TC = O(N)
+    SC = O(N)
+    */
     int longestConsecutive(vector<int>& nums) {
         
          if(nums.size() == 0){
@@ -16,15 +20,15 @@ public:
         int curr;
         int currLen;
         
-        for(int i = 0 ; i < nums.size() ; i++){
+        for(int i = 0 ; i < nums.size() ; i++){//humne alag se har ele jo set mai hai usme traverse ni kiya kyunki TC badh jaati aur wo TLE dera tha pichle submission mai
             
-            if(st.count(nums[i]-1) == 0){
+            if(st.count(nums[i]-1) == 0){ // mtlb ye first ele hai seq ka
                 curr = nums[i];
                 currLen = 1;
             }
             
            
-                while(st.count(curr + 1) != 0){
+                while(st.count(curr + 1) != 0){ // mtlb subsequent ele dhundre hum...aage waale
                     curr+=1;
                     currLen++;
                 }
