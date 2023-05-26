edb8defecea5d6632e42111506d41dd839a9cdd8
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-26 17:25:02 +0530

    Time: 19 ms (75.19%), Space: 19.7 MB (25.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    
-    //Moore's voting algorithm
-    
-    /*
-    TC = O(N)
-    SC = O(1)
-    */
-    
     int majorityElement(vector<int>& nums) {
-        
+       
         int cnt = 0;
-        int currMajority = 0;
-        
+        int ele = nums[0];
         for(int i = 0 ; i < nums.size() ; i++){
-            
-            if(cnt){
-                cnt += (nums[i] == currMajority ? 1 : -1);
-            }
-            
-            else{
-                currMajority = nums[i];
-                cnt = 1;
-            }
+            if(cnt == 0) {ele = nums[i]; cnt = 1;}
+            else if(ele == nums[i]) cnt++;
+            else cnt--;
         }
         
-        return currMajority;
+        cnt = 0;
+        for(auto it : nums){
+            if(it == ele) cnt++;
+        }
+        
+        if(cnt >= (floor)(nums.size()/2)) return ele;
+        return -1;
     }
 };
