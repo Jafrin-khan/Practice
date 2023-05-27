e6ff69ac7b9fa5e46f301a9c5b7e81af99e035b8
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-27 15:10:50 +0530

    Time: 7 ms (67.51%), Space: 10.6 MB (89.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     
-    void f(int ind , int k , vector<int>& helper , set<vector<int>>& st , vector<int>& nums){
+    void f(int ind , int k , vector<int>& helper , vector<vector<int>>& ans , vector<int>& nums){
         
         if(ind < 0){
             if(k == 0) {
-            st.insert(helper);
+            ans.push_back(helper);
         }
             return;
         }
         
-        
-        
         //take
         if(k >= nums[ind]){
         helper.push_back(nums[ind]);
-        f(ind , k - nums[ind] , helper , st , nums);
+        f(ind , k - nums[ind] , helper , ans , nums);
         helper.pop_back();
         }
         
         //notTake
-        f(ind-1 , k , helper , st , nums);
+        f(ind-1 , k , helper , ans , nums);
     }
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         
         int n = nums.size();
         
-        set<vector<int>> st;
+         vector<vector<int>> ans;
         vector<int> helper;
-        f(n-1 , target ,helper, st , nums);
+        f(n-1 , target ,helper, ans , nums);
         
-        vector<vector<int>> ans;
-        for(auto it : st){
-            ans.push_back(it);
-        }
+       
         
         return ans;
     }
