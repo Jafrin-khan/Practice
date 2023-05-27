5922cf1ac9bb2a92fa2c9b46cfe1bfc965d9e185
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-27 12:47:39 +0530

    Time: 4 ms (55.81%), Space: 7.9 MB (47.10%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    /*
-Time Complexity: O( 2^n *(k log (x) )). 2^n  for generating every subset and k* log( x)  to insert every combination of average length k in a set of size x. 
+/* ///BRUTE
+    
+// Time Complexity: O( 2^n *(k log (x) )). 2^n  for generating every subset and k* log( x)  to insert every combination of average length k in a set of size x. 
 
-Space Complexity:  O(2^n * k) to store every subset of average length k. Since we are initially using a set to store the answer another O(2^n *k) is also used.
-    */
+// Space Complexity:  O(2^n * k) to store every subset of average length k. Since we are initially using a set to store the answer another O(2^n *k) is also used.
+    
     void f(int ind , vector<int>& nums , vector<int> helper , set<vector<int>>& st){
         if(ind < 0){
             sort(helper.begin() , helper.end());
@@ -37,4 +38,42 @@ Space Complexity:  O(2^n * k) to store every subset of average length k. Since w
         
         return ans;
     }
+     
+*/
+    
+    //OPTIMISED
+    /*
+Time Complexity: O(2^n) for generating every subset and O(k)  to insert every subset in another data structure if the average length of every subset is k. Overall O(k * 2^n).
+
+Space Complexity: O(2^n * k) to store every subset of average length k. Auxiliary space is O(n)  if n is the depth of the recursion tree.
+    */
+    
+     vector<vector<int>> ans;
+    
+    void f(int ind , vector<int> helper , vector<int>& nums){
+        
+       
+        ans.push_back(helper);
+           
+        for(int i = ind ; i < nums.size() ; i++){
+            
+            if(i != ind && nums[i] == nums[i-1]){
+                continue;
+            }
+            
+            helper.push_back(nums[i]);
+            f(i+1 , helper , nums);
+            helper.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        sort(nums.begin() , nums.end());
+        vector<int> helper;
+        f(0 , helper , nums);
+        
+        return ans;
+        
+    }
+
 };
