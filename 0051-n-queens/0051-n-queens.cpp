9d26066f026225769ff50b95ea2b2d54fb269c1d
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-28 09:22:43 +0530

    Time: 63 ms (15.96%), Space: 45.6 MB (17.27%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    
+    bool isValid(vector<string> chess , int i , int j){
+        
+        for(int row = i-1 ; row >= 0 ; row--){
+            if(chess[row][j] == 'Q') return false; 
+        }
+        
+        for(int row = i - 1 , col = j - 1 ; row >= 0 && col >= 0 ; row-- , col--){
+            if(chess[row][col] == 'Q') return false;
+        }
+        
+        for(int row = i - 1 , col = j + 1 ; row >= 0 && col < chess[0].size() ; row-- , col++){
+            if(chess[row][col] == 'Q') return false;
+        }
+        
+        return true;
+    }
+    
+    void play(int row , vector<string>& chess , vector<vector<string>>& ans){
+        if(row == chess.size()){
+            ans.push_back(chess);
+            return;
+        }
+        
+        for(int col = 0 ; col < chess[0].size() ; col++){
+            if(isValid(chess , row , col)){
+                chess[row][col] = 'Q';
+                play(row+1 , chess , ans);
+                chess[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        
+        vector<vector<string>> ans;
+        
+        string s = "";
+        for(int i = 0 ; i < n ; i++){
+            s += ".";
+        }
+        
+        vector<string> chess(n , s);
+        play(0 , chess , ans);
+        
+        return ans;
+    }
+};
