506ccf34e485ec34ecf4495d84268301599123ab
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-02 20:28:37 +0530

    Time: 24 ms (21.21%), Space: 8 MB (44.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int romanToInt(string s) {
         
-//         char symbol[7] = {'I' , 'V' , 'X' , 'L' , 'C' , 'D' , 'M'};
-//         int value[7] = {1 , 5 , 10 , 50 , 100 , 500 , 1000};
-        
         unordered_map<char,int> mp;
         mp['I'] = 1;
         mp['V'] = 5;
@@ -14,17 +11,14 @@ public:
         mp['D'] = 500;
         mp['M'] = 1000;
         
+        int ans = 0;
         int n = s.size();
-        int ans = mp[s[n-1]];
+        ans += mp[s[n-1]];
         
-        int i = n-2;
-        while(i >= 0){
+        for(int i = n-2 ; i >= 0 ; i--){
             if(mp[s[i]] < mp[s[i+1]]) ans -= mp[s[i]];
             else ans += mp[s[i]];
-            i--;
         }
-        
         return ans;
-        
     }
 };
