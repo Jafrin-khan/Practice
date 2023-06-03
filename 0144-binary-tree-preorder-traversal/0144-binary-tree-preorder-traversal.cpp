d6a071e4523ae26c4b515faa563934a56e505511
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-03 14:58:14 +0530

    Time: 6 ms (22.11%), Space: 8.2 MB (95.48%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,6 +11,31 @@
  */
 class Solution {
 public:
+    /*
+    //Recursion
+    //TC = O(N)
+    //SC = O(N)
+    
+     void preOrder(TreeNode* root , vector<int>& ans){
+        if(root == NULL) return;
+        
+        ans.push_back(root->val);
+        preOrder(root->left , ans);
+        preOrder(root->right , ans);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        preOrder(root , ans);
+        
+        return ans;
+    }
+    */
+    
+    /*
+    //Iterative
+    //TC = O(N)
+    //SC = O(N)
     vector<int> preorderTraversal(TreeNode* curr) {
           vector < int > preOrder;
           if (curr == NULL)
@@ -30,4 +55,44 @@ public:
           }
           return preOrder;
             }
+    */
+    
+
+    //Morris
+    //TC = O(N)
+    //SC = O(1)
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        vector<int> ans;
+        TreeNode* curr = root;
+        
+        while(curr != NULL){
+            
+            if(curr->left == NULL){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            
+            else{
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr){
+                    prev = prev->right;
+                }
+                
+                if(prev->right == curr){
+                    prev->right = NULL;
+                    curr = curr->right;
+                }
+                
+                else{
+                    prev->right = curr;
+                    ans.push_back(curr->val);
+                    curr = curr->left;
+                }
+            }
+        }
+        
+        return ans;
+    }
+
 };
