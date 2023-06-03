09d65b05307251978e50b5a8a5d33649b8389818
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-03 08:34:26 +0530

    Time: 52 ms (14.63%), Space: 121.1 MB (18.05%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    /*
-    TC = O(N*M)
-    SC = O(1)
-    */
     string countAndSay(int n) {
         
         if(n == 1) return "1";
         if(n == 2) return "11";
         
-        string ans = "11";
-        
-        for(int num = 3 ; num <= n ; num++){
+        string s = "11";
+        n -= 2;
+         
+        while(n--){
+            
+            string str = "";
             int i = 0;
-            string toReplace = "";
-            while(i < ans.size()){
-                int cnt = 0;
-                char ch = ans[i];
-                while(i < ans.size() && ans[i] == ch){
-                    cnt++;i++;
+            while(i < s.size()){
+                int cnt = 1 ; char ch = s[i];bool flag = 0;
+                while(i < s.size()-1 && s[i] == s[i+1]){
+                    flag = 1;
+                    cnt++ ; i++;
                 }
-              
-                toReplace += to_string(cnt);
-                toReplace += ch;
+                str = str + to_string(cnt) + s[i];
+                i++;
             }
-            ans = toReplace;
+            s = str;
+            cout<<s<<endl;
         }
-        
-        return ans;
+        return s;
     }
 };
