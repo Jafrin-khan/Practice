dc310533279ce2f2b51be93a8d9cc6773f33da39
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-10 14:34:53 +0530

    Time: 7 ms (87.91%), Space: 14 MB (49.86%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-    /*
-    BFS
-    TC = O(N*M)
-    SC = O(N*M)
-    */
+   
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
         if(image[sr][sc] == color) return image;
         
         int n = image.size();
         int m = image[0].size();
+        int c = image[sr][sc];
         
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        int oldColor = image[sr][sc];
+         int dx[4] = {-1 , 1 , 0 , 0};
+         int dy[4] = {0 , 0 , -1 , 1};
+         
+         queue<pair<int,int>> q;
+         
+         q.push({sr,sc});
         image[sr][sc] = color;
         
-        int dx[4] = {-1 , 1 , 0 , 0};
-        int dy[4] = {0 , 0 ,-1 , 1};
-        
-        while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                int x = q.front().first;
-                int y = q.front().second;q.pop();
-                
-                for(int i = 0 ; i < 4 ; i++){
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-                    
-                    if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                    if(image[nx][ny] == oldColor){
-                        q.push({nx,ny});
-                        image[nx][ny] = color;
-                    }
-                }
-            }
-        }
+         while(!q.empty()){
+             
+             int size = q.size();
+             
+             while(size--){
+                 int row = q.front().first;
+                 int col = q.front().second;q.pop();
+               
+                 for(int i = 0 ; i < 4 ; i++){
+                     int nx = row + dx[i];
+                     int ny = col + dy[i];
+                     
+                     if(nx < 0 || ny < 0 || nx >= n || ny >= m || image[nx][ny] != c) continue;
+                     image[nx][ny] = color;
+                     q.push({nx , ny});
+                 }
+             
+             }
+             
+         }
         
         return image;
     }
