f7f39325793fd78481ae8f55e03c9686f0aa7d1a
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-15 23:57:48 +0530

    Time: 9 ms (66.17%), Space: 14 MB (66.48%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
-   
+    
+    //dfs
+    
+    void dfs(int i , int j , vector<vector<int>>& image , int n , int m , int color , int prevCol){
+        
+        if(i < 0 || j < 0 || i >= n || j >= m || image[i][j] != prevCol) return;
+       
+        image[i][j] = color;
+        dfs(i+1 , j , image , n , m ,color , prevCol);
+        dfs(i,j+1 , image , n , m ,color , prevCol);
+        dfs(i-1 , j , image , n , m , color ,prevCol);
+        dfs(i,j-1 , image , n , m ,color , prevCol);
+    }
+    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
-        if(image[sr][sc] == color) return image;/////////////////////////////////////
+        if(image[sr][sc] == color) return image;
+        int prevCol = image[sr][sc];
         
-        int n = image.size();
-        int m = image[0].size();
-        int c = image[sr][sc];
-        
-         int dx[4] = {-1 , 1 , 0 , 0};
-         int dy[4] = {0 , 0 , -1 , 1};
-         
-         queue<pair<int,int>> q;
-         
-         q.push({sr,sc});
-        image[sr][sc] = color;
-        
-         while(!q.empty()){
-             
-             int size = q.size();
-             
-             while(size--){
-                 int row = q.front().first;
-                 int col = q.front().second;q.pop();
-               
-                 for(int i = 0 ; i < 4 ; i++){
-                     int nx = row + dx[i];
-                     int ny = col + dy[i];
-                     
-                     if(nx < 0 || ny < 0 || nx >= n || ny >= m || image[nx][ny] != c) continue;
-                     image[nx][ny] = color;
-                     q.push({nx , ny});
-                 }
-             
-             }
-             
-         }
+        dfs(sr , sc , image , image.size() , image[0].size() , color , prevCol);
         
         return image;
     }
