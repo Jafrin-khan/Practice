6e01c8f3f311eb432f25a2c4263b93a3ae6842e4
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-16 00:41:04 +0530

    Time: 14 ms (67.37%), Space: 15.8 MB (94.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,55 +1,43 @@
 class Solution {
 public:
-    
-    /*
-    MOORE'S VOTING ALGO
-    TC = O(N)
-    SC = O(1)
-    */
-    
-    //ye if else conditions line 19 aur 20 ki hamesha pehle aani h remember it VVVVVIMP
     vector<int> majorityElement(vector<int>& nums) {
         
-        int ele1 = -1e9;
-        int ele2 = -1e9;
+        int n = nums.size();
         int cnt1 = 0 , cnt2 = 0;
+        int ele1 , ele2;
         
-        for(int i = 0 ; i < nums.size() ; i++){
+        for(int i = 0 ; i < n ; i++){
             
             if(ele1 == nums[i]) cnt1++;
             else if(ele2 == nums[i]) cnt2++;
             
             else if(cnt1 == 0){
-                cnt1 = 1;
                 ele1 = nums[i];
+                cnt1 = 1;
             }
             
             else if(cnt2 == 0){
-                cnt2 = 1;
                 ele2 = nums[i];
+                cnt2 = 1;
             }
-            
-          
             
             else{
                 cnt1--;
                 cnt2--;
             }
-            
-        }
-        
-        cnt1 = 0;
-        cnt2 = 0;
-        
-        for(auto it : nums){
-            if(it == ele1) cnt1++;
-            if(it == ele2) cnt2++;
         }
         
         vector<int> ans;
+        cnt1 = 0;
+        cnt2 = 0;
         
-        if(cnt1 > (nums.size()/3)) ans.push_back(ele1);
-        if(cnt2 > (nums.size()/3)) ans.push_back(ele2);
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] == ele1) cnt1++;
+            if(nums[i] == ele2) cnt2++;
+        }
+        
+        if(cnt1 > (n/3)) ans.push_back(ele1);
+        if(cnt2 > (n/3)) ans.push_back(ele2);
         
         return ans;
     }
