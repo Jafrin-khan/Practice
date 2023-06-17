1d297353cc2d019adcaadcf45097f260c56fa4f5
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-17 20:03:01 +0530

    Time: 75 ms (87.69%), Space: 29.2 MB (58.56%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
-    void dfs(int i , int j , int n , int m , vector<vector<int>>& grid ,  vector<vector<int>>& vis){
+    
+    void dfs(int i , int j , vector<vector<int>>& grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        
         if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != 1) return;
         
-        vis[i][j] = 1;
-        grid[i][j] = '$';
+        grid[i][j] = -1;
         
-        dfs(i+1,j,n,m,grid,vis);
-        dfs(i,j+1,n,m,grid,vis);
-        dfs(i-1,j,n,m,grid,vis);
-        dfs(i,j-1,n,m,grid,vis);
+        dfs(i + 1 , j , grid);
+        dfs(i , j + 1 , grid);
+        dfs(i - 1 , j , grid);
+        dfs(i , j - 1 , grid);
     }
     int numEnclaves(vector<vector<int>>& grid) {
         
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n , vector<int>(m,0));
-        
-        for(int i = 0 ; i < n ; i++){
-            if(grid[i][0] == 1 && !vis[i][0]) dfs(i,0,n,m,grid,vis); 
-            if(grid[i][m-1] == 1 && !vis[i][m-1]) dfs(i,m-1,n,m,grid,vis);
-        }
-        
-        for(int j = 0 ; j < m ; j++){
-            if(grid[0][j] == 1 && !vis[0][j]) dfs(0,j,n,m,grid,vis);
-            if(grid[n-1][j] == 1 && !vis[n-1][j]) dfs(n-1,j,n,m,grid,vis);
-        }
         
         int cnt = 0;
+        
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
+            if(grid[i][0] == 1) dfs(i , 0 , grid);
+            if(grid[i][m-1] == 1) dfs(i , m-1 , grid);
+        }
+        
+        for(int i = 0 ; i < m ; i++){
+            if(grid[0][i] == 1) dfs(0 , i , grid);
+            if(grid[n-1][i] == 1) dfs( n-1 , i , grid);  
+        }
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m; j++){
                 if(grid[i][j] == 1) cnt++;
-             
             }
         }
         
         return cnt;
-        
     }
 };
