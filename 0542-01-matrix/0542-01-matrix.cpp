30615ffac4985fff452e1a5bb8e6c52002bcadbc
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-17 18:59:31 +0530

    Time: 99 ms (53.85%), Space: 34.4 MB (23.32%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
-    
-    /*
-    
-    Time Complexity: O(NxM + NxMx4) ~ O(N x M)
-
-For the worst case, the BFS function will be called for (N x M) nodes, and for every node, we are traversing for 4 neighbors, so it will take O(N x M x 4) time.
-
-Space Complexity: O(N x M) + O(N x M) + O(N x M) ~ O(N x M)
-
-O(N x M) for the visited array, distance matrix, and queue space takes up N x M locations at max. 
-    */
-    
-    /*
-    TC = O(M*N)
-    SC = O(M*N)
-    */
     vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        
+        vector<vector<int>> ans;
         
         int n = grid.size();
         int m = grid[0].size();
@@ -24,55 +10,40 @@ O(N x M) for the visited array, distance matrix, and queue space takes up N x M 
         vector<vector<int>> vis(n , vector<int>(m , 0));
         vector<vector<int>> dis(n , vector<int>(m , 0));
         
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<pair<int,int> , int>> q;//<<x,y>,steps>
         
-        
-        //pushing ind of all 1s in grid and their corresponding dis ,i.e, 0
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                
                 if(grid[i][j] == 0){
                     vis[i][j] = 1;
-                    q.push({{i,j},0});
+                    q.push({{i , j},0});
                 }
             }
         }
         
-        int dx[] = {-1 , 0 , 1 , 0};
-        int dy[] = {0 , 1 , 0 , -1};
+        int dx[4] = {-1 , 1 , 0 , 0};
+        int dy[4] = {0 , 0 , -1 , 1};
         
         while(!q.empty()){
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int steps = q.front().second;q.pop();
             
-            auto front = q.front();
-            q.pop();
-            
-            int x = front.first.first;
-            int y = front.first.second;
-            int d = front.second;
-            
-        
-            dis[x][y] = d;
-            
+            dis[x][y] = steps;
             
             for(int i = 0 ; i < 4 ; i++){
-                
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m || vis[nx][ny]){
-                    continue;
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m && !vis[nx][ny]){
+                    vis[nx][ny] = 1;
+                    q.push({{nx , ny} , steps + 1});
                 }
-                
-                vis[nx][ny] = 1;
-                q.push({{nx,ny},d+1});
-            
             }
-            
             
         }
         
         return dis;
-        
         
     }
 };
