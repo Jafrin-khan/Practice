11f63fe5624c1c2c12c665d2f9422a64509221ad
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-18 20:13:08 +0530

    Time: 30 ms (42.53%), Space: 13.4 MB (73.54%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -3,20 +3,34 @@ public:
     
     //purane submission dekho
     
-    bool bfs(int src ,vector<int>& color , vector<vector<int>>& adj){
+//     bool bfs(int src ,vector<int>& color , vector<vector<int>>& adj){
         
-        queue<int> q;
-        q.push(src);
-        color[src] = 0;/////////COLOR[SRC] TO DEFINE KRO WO HI NAHI KARA THA AUR SUBMIT KRNE CHALE
+//         queue<int> q;
+//         q.push(src);
+//         color[src] = 0;/////////COLOR[SRC] TO DEFINE KRO WO HI NAHI KARA THA AUR SUBMIT KRNE CHALE
         
-        while(!q.empty()){
+//         while(!q.empty()){
             
-            int node = q.front();q.pop();
+//             int node = q.front();q.pop();
             
-            for(auto it : adj[node]){
-                if(color[it] == -1) {color[it] = 1 - color[node];q.push(it);}////////////PUSH KRNA NAHI BHULNA
-                else if(color[it] == color[node]) return false;
+//             for(auto it : adj[node]){
+//                 if(color[it] == -1) {color[it] = 1 - color[node];q.push(it);}////////////PUSH KRNA NAHI BHULNA
+//                 else if(color[it] == color[node]) return false;
+//             }
+            
+//         }
+        
+//         return true;
+//     }
+    
+    bool dfs(int src , int col , vector<int>& color , vector<vector<int>>& adj){
+         
+        for(auto it : adj[src]){
+            if(color[it] == -1){
+                color[it] = !col;
+                if(!dfs(it , !col , color , adj)) return false;
             }
+            else if(color[it] == col) return false;
             
         }
         
@@ -29,7 +43,10 @@ public:
         
         vector<int> color(n , -1);
         for(int i = 0 ; i < n ; i++){
-            if(color[i] == -1) if(!bfs(i , color , adj)) return false;
+            if(color[i] == -1){
+                color[i] = 0;
+                if(!dfs(i , 0 , color , adj)) return false;
+            }
         }
         
         return true;
