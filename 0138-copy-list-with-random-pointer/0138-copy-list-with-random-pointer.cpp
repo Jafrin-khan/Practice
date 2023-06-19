eb6baa2a6be06321ebec832b1c2d13e19c8597ae
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-19 20:21:42 +0530

    Time: 8 ms (74.02%), Space: 11.2 MB (64.06%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -14,74 +14,47 @@ public:
 };
 */
 
-/*
-One brute force method is also there using hashmaps see TUF notes
-
-Time Complexity: O(N)+O(N)+O(N)
-
-Reason: Each step takes O(N) of time complexity.
-
-Space Complexity: O(1)
-
-Reason: No extra data structure was used for computation.
-
-*/
+//              01/07/2023 submission
 
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
         
-        if(head == NULL){
-            return head;
-        }
+        if(head == NULL) return head;
         
         Node* temp = head;
         
-        
-        /******************step 1********************/
-        //make copy of nodes and linking nodes by next pointers ONLY
-        /*
-        eg = 1->2->3->4
-             1->1->2->2->3->3->4->4
-        */
-        while(temp!=NULL){
-            
-            Node* nextNode = temp->next;
+        //linking dup nodes
+        while(temp != NULL){
             Node* newNode = new Node(temp->val);
+            Node* nextNode = temp->next;
             temp->next = newNode;
             newNode->next = nextNode;
             
             temp = temp->next->next;
         }
         
-        /******************step 2********************/
-        //linking random pointers
+        
+        //linking random ptrs
         temp = head;
         while(temp != NULL){
-            
-            if(temp->random){
-                temp->next->random = temp->random->next;
-            }
-            
+            if(temp->random) temp->next->random = temp->random->next;/////////////
             temp = temp->next->next;
         }
         
-        /******************step 3********************/
-        //segragating the lists
+        //segrating lists
+        Node* ans = new Node(0);
+        Node* toReturn = ans;
         temp = head;
-        Node* newHead = new Node(0);
-        Node* tail = newHead;
         
         while(temp != NULL){
+            ans->next = temp->next;
+            ans = ans->next;
             
-            tail->next = temp->next;
-            tail = tail->next;
-                
             temp->next = temp->next->next;
-            temp = temp->next;
+            temp = temp->next;///////////////////////
         }
         
-        return newHead->next;
-        
+        return toReturn->next;
     }
 };
