85cc0e19097d9843d03b38f965fb60f83475fbab
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-20 20:13:52 +0530

    Time: 109 ms (50.54%), Space: 13.3 MB (53.71%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,53 +1,41 @@
 class Solution {
     #define ll long long
 public:
-   /*
-   TC = O(n*(n*(logn)))
-   SC = O(1)
-   */
-    
-    /*
-    we can use set for preventing duplicates but due to SC its not optimal hence we use this technique:
-    
-    Remember->
-    1-if(i > 0 && nums[i] == nums[i-1]) continue;
-    2- if(j > i+1 && nums[j] == nums[j-1]) continue;
-    3- if(nums[low]+nums[high] == toFind){
-       while(left < right && nums[left] == nums[left-1]){left++;}
-       while(left < right && nums[right] == nums[right+1]){right--;}
-       }
-    */
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
         vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
+        
+        sort(nums.begin() , nums.end());
         ll n = nums.size();
         
         for(ll i = 0 ; i < n ; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;/**************************************/
-            ll nt = target - nums[i];
-            for(int j = i+1 ; j < n ; j++){
-                if(j > i+1 && nums[j] == nums[j-1]) continue;/*********************************/
-                ll left = j+1;
-                ll right = n-1;
-                ll toFind = nt-nums[j];
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            ll nT = target - nums[i];
+            for(ll j = i + 1 ; j < n ; j++){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                ll toFind = nT - nums[j];///////////////////////////////////////// DONT USE NT -= NUMS[J] AS NT VALUE HUM UPER WAALE LOOP SE LERE ISLIE CHANGE NI KR SKTE ISLIE HUME NAYA VARIABLE BANANE HAI 
+                
+                ll left = j + 1;
+                ll right = n - 1;
                 while(left < right){
                     if(nums[left] + nums[right] == toFind){
-                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        vector<int> v;
+                        v = {nums[i] , nums[j] , nums[left] , nums[right]};
+                        left++;
+                        right--;
                         
-                        left++;
-                        right--;
-                        while(left < right && nums[left] == nums[left-1]){left++;}/**************************************/
-                        while(left < right && nums[right] == nums[right+1]){right--;}/***********************************/
+                        ans.push_back(v);
+                   
+                    
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
                     }
                     
-                    else if(nums[left] + nums[right] < toFind){
-                        left++;
-                    }
-                    
-                    else{
-                        right--;
-                    }}}}
+                    else if((nums[left] + nums[right]) < toFind) left++;
+                    else right--;
+                }
+            }
+        }
         
         return ans;
     }
