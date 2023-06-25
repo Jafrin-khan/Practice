5f3dbb785767a80b3032d2e96060d5f2877824a4
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-25 18:57:42 +0530

    Time: 91 ms (44.27%), Space: 22.2 MB (39.75%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
     
-    /*
-Time Complexity: O( 8*N*M ) { N*M are the total cells, for each of which we also check 4 adjacent nodes for the shortest path length}, Where N = No. of rows of the binary maze and M = No. of columns of the binary maze.
-
-Space Complexity: O( N*M ), Where N = No. of rows of the binary maze and M = No. of columns of the binary maze.
+    //03/18/2023
     
-    */
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
+    
         int n = grid.size();
+        int m = grid[0].size();
         
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
         if(n == 1){
             return grid[0][0] == 0;
         } 
-       
-        vector<vector<int>> distTo(n , vector<int>(n,1e9));
+        
+        vector<vector<int>> disTo(n , vector<int>(m , 1e9));
         queue<pair<int,pair<int,int>>> q;
         
-        q.push({1,{n-1,n-1}});
-        distTo[n-1][n-1] = 1;
+        q.push({1,{n-1,m-1}});
+        disTo[n-1][m-1] = 1;///////////////////////////////////
         int ans = 1e9;
         
         while(!q.empty()){
-            int dis = q.front().first;
+            int d = q.front().first;
             int x = q.front().second.first;
             int y = q.front().second.second;q.pop();
             
-             for(int i = -1 ; i <= 1 ; i++){
-                 for(int j = -1 ; j <= 1 ; j++){
-                     int nx = x + i;
-                     int ny = y + j;
-                     
-                     if(nx < 0 || ny < 0 || nx >= n || ny >= n || (nx == x && ny == y) || grid[nx][ny] == 1) continue;
-                     if(nx == 0 && ny == 0 && grid[nx][ny] == 0) ans = min(ans , dis+1);
-                     if(distTo[nx][ny] > dis + 1){
-                         distTo[nx][ny] = dis+1;
-                         q.push({dis+1,{nx,ny}});
-                     }
-                     
-                 }
-             }
+            for(int i = -1 ; i <= 1 ; i++){
+                for(int j = -1 ; j <= 1 ; j++){
+                    int nx = i + x;
+                    int ny = j + y;
+                    
+                    if(nx < 0 || ny < 0 || nx >= n || ny >= m || (nx == x && ny == y) || grid[nx][ny] == 1) continue;
+                    
+                    if(nx == 0 && ny == 0) ans = min(ans , d + 1);
+                    if(disTo[nx][ny] > d + 1){
+                        disTo[nx][ny] = d + 1;
+                        q.push({disTo[nx][ny],{nx,ny}});
+                    }
+                }
+            }
         }
+        
         if(ans == 1e9) return -1;
         
         return ans;
