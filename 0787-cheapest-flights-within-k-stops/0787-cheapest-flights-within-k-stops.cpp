2c2894aaf9b5e990e911bd6d8e755297c41979ee
Jafrin <khan.jafrin.9346@gmail.com>
2023-06-28 00:08:47 +0530

    Time: 39 ms (23.46%), Space: 16.9 MB (10.45%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,39 +2,39 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        vector<pair<int,int>> adj[n];
+        vector<vector<int>> adj[n];
         
         for(auto it : flights){
-            adj[it[0]].push_back({it[1],it[2]});
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            
+            adj[u].push_back({v,wt});
         }
+        
         queue<pair<int,pair<int,int>>> q;
-        q.push({src , {0 , 0}}); // <node,steps,cost>
+        q.push({0,{src,0}});//<stops,<city,dis>>
         
         vector<int> disArr(n , 1e9);
         disArr[src] = 0;
-        
+       
         while(!q.empty()){
-            auto front = q.front();q.pop();
+            int stops = q.front().first;
+            int city = q.front().second.first;
+            int wt = q.front().second.second;q.pop();
             
-            int node = front.first;
-            int steps = front.second.first;
-            int costTillNode = front.second.second;
+            if(stops > k) continue;
             
-            if(steps > k) continue;
-            
-            for(auto it : adj[node]){
-                int adjNode = it.first;
-                int adjW = it.second;
-                
-                if(steps <= k && adjW + costTillNode < disArr[adjNode]){
-                    disArr[adjNode] = adjW + costTillNode;
-                    q.push({adjNode,{steps+1,adjW + costTillNode}});
+            for(auto it : adj[city]){
+                if(it[1] + wt < disArr[it[0]] && stops <= k){
+                    disArr[it[0]] = it[1] + wt;
+                    q.push({stops+1,{it[0] , it[1] + wt}});
                 }
-                
             }
         }
-         if(disArr[dst] == 1e9) return -1;
-        return disArr[dst];
         
+       if(disArr[dst] == 1e9) return -1;
+        return disArr[dst];
+            
     }
 };
