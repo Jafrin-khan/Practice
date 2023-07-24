f94a15e5c2c85425040475e22cbdd887fd353f46
Jafrin <khan.jafrin.9346@gmail.com>
2023-07-24 23:46:18 +0530

    Time: 20 ms (20.27%), Space: 9.9 MB (91.92%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    /*
-    TC = O(M*N).....every element of matrix is processed at most three times.
     
-    SC = O(M*N)...Stack space in recursive call
-    */
-    void dfs(int i , int j , int n , int m , vector<vector<char>>& grid ,  vector<vector<int>>& vis){
-        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != 'O') return;
+    void dfs(int i , int j , vector<vector<char>>& grid){
         
-        vis[i][j] = 1;
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 'X' || grid[i][j] == '$') return;
+        
         grid[i][j] = '$';
         
-        dfs(i+1,j,n,m,grid,vis);
-        dfs(i,j+1,n,m,grid,vis);
-        dfs(i-1,j,n,m,grid,vis);
-        dfs(i,j-1,n,m,grid,vis);
+        dfs(i+1 , j , grid);
+        dfs(i , j+1 , grid);
+        dfs(i-1 , j , grid);
+        dfs(i , j-1 , grid);
+        
     }
     void solve(vector<vector<char>>& grid) {
         
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n , vector<int>(m,0));
         
         for(int i = 0 ; i < n ; i++){
-            if(grid[i][0] == 'O' && !vis[i][0]) dfs(i,0,n,m,grid,vis); 
-            if(grid[i][m-1] == 'O' && !vis[i][m-1]) dfs(i,m-1,n,m,grid,vis);
+            if(grid[i][0] == 'O') dfs(i , 0 , grid);
+            if(grid[i][m-1] == 'O') dfs(i , m-1 , grid);
         }
         
         for(int j = 0 ; j < m ; j++){
-            if(grid[0][j] == 'O' && !vis[0][j]) dfs(0,j,n,m,grid,vis);
-            if(grid[n-1][j] == 'O' && !vis[n-1][j]) dfs(n-1,j,n,m,grid,vis);
+            if(grid[0][j] == 'O') dfs(0 , j , grid);
+            if(grid[n-1][j] == 'O') dfs(n-1 , j , grid);
         }
         
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 'O') grid[i][j] = 'X';
                 if(grid[i][j] == '$') grid[i][j] = 'O';
-                else grid[i][j] = 'X';
             }
         }
         
