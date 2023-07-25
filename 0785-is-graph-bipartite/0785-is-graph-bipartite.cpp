1b442cc6dd7e12b7a07721318c8944596c0e53c9
Jafrin <khan.jafrin.9346@gmail.com>
2023-07-25 21:12:27 +0530

    Time: 30 ms (44.33%), Space: 13.4 MB (69.80%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
     
-    //purane submission dekho
-    
-//     bool bfs(int src ,vector<int>& color , vector<vector<int>>& adj){
+    bool f(int node , int c , vector<vector<int>>& adj , vector<int>& color){
         
-//         queue<int> q;
-//         q.push(src);
-//         color[src] = 0;/////////COLOR[SRC] TO DEFINE KRO WO HI NAHI KARA THA AUR SUBMIT KRNE CHALE
-        
-//         while(!q.empty()){
-            
-//             int node = q.front();q.pop();
-            
-//             for(auto it : adj[node]){
-//                 if(color[it] == -1) {color[it] = 1 - color[node];q.push(it);}////////////PUSH KRNA NAHI BHULNA
-//                 else if(color[it] == color[node]) return false;
-//             }
-            
-//         }
-        
-//         return true;
-//     }
-    
-    bool dfs(int src , int col , vector<int>& color , vector<vector<int>>& adj){
-         
-        for(auto it : adj[src]){
+        color[node] = c;
+        for(auto it : adj[node]){
             if(color[it] == -1){
-                color[it] = !col;
-                if(!dfs(it , !col , color , adj)) return false;
+                if(!f(it , 1 - c , adj , color)) return false;
             }
-            else if(color[it] == col) return false;
-            
+            else if(color[it] == color[node]) return false;
         }
         
         return true;
@@ -40,13 +17,10 @@ public:
     bool isBipartite(vector<vector<int>>& adj) {
         
         int n = adj.size();
-        
         vector<int> color(n , -1);
+        
         for(int i = 0 ; i < n ; i++){
-            if(color[i] == -1){
-                color[i] = 0;
-                if(!dfs(i , 0 , color , adj)) return false;
-            }
+            if(color[i] == -1) if(!f(i , 0 , adj , color)) return false;
         }
         
         return true;
