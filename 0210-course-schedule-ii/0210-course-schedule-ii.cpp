f49626c5072228e5c8b7d5a22a3b16949e6f7de3
Jafrin <khan.jafrin.9346@gmail.com>
2023-07-27 22:46:42 +0530

    Time: 19 ms (92.13%), Space: 13.4 MB (80.40%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
-    /*
-Time Complexity: O(V+E), where V = no. of nodes and E = no. of edges. This is a simple BFS algorithm.
-
-Space Complexity: O(N) + O(N) ~ O(2N), O(N) for the indegree array, and O(N) for the queue data structure used in BFS(where N = no.of nodes). Extra O(N) for storing the topological sorting. Total ~ O(3N).
-    
-    */
-    
-    //solving by using toposort...KAHN'S ALGO
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<int> adj[n];
+  
+    vector<int> findOrder(int n, vector<vector<int>>& nums) {
         
-        for(int i = 0 ; i < prerequisites.size() ; i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
+        vector<vector<int>> adj(n);
+        vector<int> indegree(adj.size() , 0);
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+            int u = nums[i][0];
+            int v = nums[i][1];
+            
             adj[v].push_back(u);
+            indegree[u]++;
         }
         
-        vector<int> indegree(n,0);
-        
-        for(int i = 0 ; i < n ; i++){
-            for(auto it : adj[i]) indegree[it]++;
-        }
-        
-        int cnt = 0;
-        queue<int> q;
+       
         vector<int> topo;
+        queue<int> q;
         
-        for(int i = 0 ; i < n ; i++){
-            if(indegree[i] == 0){
-                q.push(i);
-            } 
+        for(int i = 0 ; i < indegree.size() ; i++){
+                if(indegree[i] == 0) q.push(i);
         }
         
         while(!q.empty()){
             int node = q.front();q.pop();
             topo.push_back(node);
-            cnt++;
             
             for(auto it : adj[node]){
                 indegree[it]--;
-                if(indegree[it] == 0) {
-                    q.push(it);
-                }
+                if(indegree[it] == 0) q.push(it);
             }
         }
+    
+        if(topo.size() == n) return topo;
         
-        if(cnt == n) return topo;
         return {};
+        
     }
 };
