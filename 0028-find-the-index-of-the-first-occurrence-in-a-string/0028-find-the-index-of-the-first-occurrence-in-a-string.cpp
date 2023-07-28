6340667f7b793a81cf1af2888e3aea42e3a0e752
Jafrin <khan.jafrin.9346@gmail.com>
2023-07-28 12:27:12 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (54.68%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,17 +1,83 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
+    
+//KMP -> http://www.btechsmartclass.com/data_structures/knuth-morris-pratt-algorithm.html
+    
+    
+    
+//     int strStr(string haystack, string needle) {
+    
+//         int i = 0 ; int n = needle.size();
+//         if(haystack.size() < n) return -1; 
         
-        
-        int i = 0 ; int n = needle.size();
-        if(haystack.size() < n) return -1; 
-        
-        while(i <= haystack.size() - n){
-            if(haystack[i] == needle[0]){
-                if(haystack.substr(i,n) == needle) return i;
+//         while(i <= haystack.size() - n){
+//             if(haystack[i] == needle[0]){
+//                 if(haystack.substr(i,n) == needle) return i;
+//             }
+//             i++;
+//         }
+//         return -1;
+//     }
+    
+/**************************KMP****************************************************/
+   
+//KMP -> http://www.btechsmartclass.com/data_structures/knuth-morris-pratt-algorithm.html
+    
+  
+        int strStr(string s, string pattern) {
+            
+            int n = pattern.size();
+            
+            vector<int> lps(n , 0);
+            
+            //makig lps array
+           int j = 0;
+
+	lps[0] = 0; 
+	int i = 1;
+	while (i < n) {
+		if (pattern[i] == pattern[j]) {
+			j++;
+			lps[i] = j;
+			i++;
+		}
+		else 
+		{
+			
+			if (j != 0) {
+				j = lps[j - 1];
+
+			}
+			else
+			{
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
+            
+            int ind = -1;
+            
+            i = 0;
+            j = 0;
+            while(i < s.size() && j < n){
+    
+                if(s[i] == pattern[j]){
+                    i++;
+                    j++;
+                    if(j == n){
+                      return i - n;
+                     
+                    } 
+                }
+               
+                else{
+                    if(j == 0) i++;
+                    else j = lps[j-1];
+                } 
             }
-            i++;
+    
+            return ind;
         }
-        return -1;
-    }
+
 };
