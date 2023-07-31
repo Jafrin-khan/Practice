dbb146afc74fdb9bc261d4a85aa55133169f0348
Jafrin <khan.jafrin.9346@gmail.com>
2023-07-31 11:25:38 +0530

    Time: 6 ms (34.61%), Space: 9.5 MB (19.97%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    
-    /*
-    It's possible in O(log(mn)) which is much faster than O(m+n).
-For proof, consider that log(mn) = log m + log n.
-And in the domain being considered m, n >= 1 (and also the >=0 domain) the following holds truelog m < m
-Therefore O(log(mn)) < O(m+n)
-    */
-    
-    // https://leetcode.com/problems/search-a-2d-matrix/discuss/1895837/C%2B%2B-BINARY-SEARCH-TREE-(**)-Explained-with-IMG
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
         int n = matrix.size();
         int m = matrix[0].size();
         
-        int row = 0;
-        int col = m-1;
+        int row = 0 , col = m-1;
         
-        while(row < n && col >= 0){
+        while(row < n && col >= 0){//*************
             if(matrix[row][col] == target) return true;
-            
-            else if(matrix[row][col] < target) row++;
-            
-            else col--;
+            if(matrix[row][col] > target) col--;
+            else row++;
         }
-       
+        
         return false;
     }
 };
+
