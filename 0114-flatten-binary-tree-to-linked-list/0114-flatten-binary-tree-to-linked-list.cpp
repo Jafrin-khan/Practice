ed891b4808a7d2a8eb4cd7b7080b7e1f1de0e5ed
Jafrin <khan.jafrin.9346@gmail.com>
2023-08-09 08:23:13 +0530

    Time: 0 ms (100.00%), Space: 12.8 MB (13.68%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,51 +11,21 @@
  */
 class Solution {
 public:
-    
-    /*
-Time Complexity: O(N)
-
-Reason: We are doing a simple preorder traversal.
-
-Space Complexity: O(N)
-
-Reason: Worst case time complexity will be O(N) (in case of skewed tree).
-    */
-    
-    /*
-    TC = O(N)
-    SC = O(N)....worst TC in case of skewed tree.....we can do SC as O(1) using Morris Traversal
-    */
-    
+    TreeNode* prev = NULL;
     TreeNode* f(TreeNode* root){
+
+       if (root == NULL) return NULL;
+
+      flatten(root -> right);
+      flatten(root -> left);
+
+      root -> right = prev;
+      root -> left = NULL;
+      prev = root;
         
-         if(root == NULL){
-            return root;
-        }
-        
-        TreeNode* leftPart = f(root->left);
-        TreeNode* rightPart = f(root->right);
-        
-        root->right = leftPart;
-        root->left = NULL;
-        TreeNode* traverse = root;
-        
-        while(traverse->right){
-            traverse = traverse->right;
-        }
-        
-        traverse->right = rightPart;
-        
-        return root;
-        
+     return root;
     }
-    
     void flatten(TreeNode* root) {
-        
-        if(root == NULL){
-            return;
-        }
-        
         f(root);
     }
 };
