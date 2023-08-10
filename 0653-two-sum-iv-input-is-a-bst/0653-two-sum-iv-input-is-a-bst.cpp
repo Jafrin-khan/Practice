c32e62553736752a6aff49317df1f5308d2fbd2b
Jafrin <khan.jafrin.9346@gmail.com>
2023-08-10 14:26:47 +0530

    Time: 35 ms (81.95%), Space: 39 MB (17.64%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -15,19 +15,16 @@ public:
      bool f(TreeNode* root , int k , unordered_map<int,int>& mp){
         
         if(root == NULL) return false;
-        
-       
-        int toFind = k - root->val;
-        if(mp.find(toFind) != mp.end()) return true;
-        
-         mp[root->val]++;
+        if(mp.find(k - root->val) != mp.end()) return true;
+        mp[root->val]++;
         
         return f(root->left , k , mp) || f(root->right , k , mp);
     }
-    
     bool findTarget(TreeNode* root, int k) {
         
         unordered_map<int,int> mp;
+        
         return f(root , k , mp);
     }
+   
 };
