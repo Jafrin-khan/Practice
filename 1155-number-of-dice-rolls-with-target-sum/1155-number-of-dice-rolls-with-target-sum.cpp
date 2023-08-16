654f576573f7e5d5d85d2965c3dbccf4939c35c6
Jafrin <khan.jafrin.9346@gmail.com>
2023-08-16 11:31:50 +0530

    Time: 23 ms (84.42%), Space: 8.5 MB (49.69%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int mod = 1e9+7;
-    int solve(int n,int &k,int x,vector<vector<int>> &dp){
+    int solve(int n,int k,int x,vector<vector<int>> &dp){
         if(n==0&&x==0)return 1;
         if(n<=0)return 0;
         if(dp[n][x] != -1)return dp[n][x];
