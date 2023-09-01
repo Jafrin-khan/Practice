a469c13180cc3d4e006e6c4b8d7c34a4751ffc3c
Jafrin <khan.jafrin.9346@gmail.com>
2023-09-01 11:24:18 +0530

    Time: 3 ms (33.40%), Space: 6.6 MB (24.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,47 +1,22 @@
 class Solution {
 public:
     
-//     1
-    
-    
-//                ~
-//              ~ 1
-        
-//            3,3
-        
-//         3,2    2,3
-        
-//       2,2  3,1   1,2 2,2 -->Overlapping subproblems hence memoization
-        
-        
-//     f(i , j){
-        
-//         if(i < 0 || j < 0) return 0;
-//         if(i == 0 && j == 0) return 1;
-        
-//         int left = f(i , j - 1);
-//         int up = f(i -1 , j);
-        
-//         return left + up;
-//     }
-    
-     //see 03/07/2023  submission
     int f(int i , int j , vector<vector<int>>& dp){
         
-        if(i < 0 || j < 0) return 0;
-        
         if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0; 
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        int left = f(i , j-1 , dp);
         int up = f(i-1 , j , dp);
+        int left = f(i , j-1 , dp);
         
         return dp[i][j] = left + up;
     }
+    
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(m , vector<int>(n,-1));
+        vector<vector<int>> dp(m , vector<int>(n , -1));
         return f(m-1 , n-1 , dp);
     }
 };
