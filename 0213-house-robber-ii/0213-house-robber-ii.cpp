c6204ba74577677db5e20dce5df4fd3bd6a521ed
Jafrin <khan.jafrin.9346@gmail.com>
2023-09-01 10:39:10 +0530

    Time: 0 ms (100.00%), Space: 8 MB (13.21%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    //03-06-2023
-    int f(int ind , vector<int>& nums , vector<int>& dp){
-    if(ind < 0) return 0;
-    if(ind == 0) return nums[0];
-
-    if(dp[ind] != -1) return dp[ind];
     
-    int notTake = f(ind-1 , nums , dp);
-    int take = f(ind-2 , nums , dp) + nums[ind];
-
-    return dp[ind] = max(take , notTake);
-}
-
+    int f(int ind , vector<int>& nums ,  vector<int>& dp){
+        
+        if(ind < 0) return 0;
+        
+        if(dp[ind] != -1) return dp[ind];
+        
+        int notTake = f(ind-1 , nums , dp);
+        int take = nums[ind] + f(ind-2 , nums , dp);
+        
+        return dp[ind] = max(take , notTake);
+    }
+    
     int rob(vector<int>& nums) {
+                
         int n = nums.size();
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0] , nums[1]);
         
-        if(n == 1) return nums[0];///////////////
-        if(n == 2) return max(nums[0] , nums[1]);////////////////
-
-        vector<int> dp(n , -1);
+        vector<int> dp1(n , -1);
+        vector<int> dp2(n , -1);
         
-        vector<int> v1;/////////
-        vector<int> v2;/////////////
-        
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(i != 0) v1.push_back(nums[i]);////////
-            if(i != n-1) v2.push_back(nums[i]);////////////
+        vector<int> n1 , n2;
+        for(int i = 0 ; i < n ; i++){
+            if(i != 0) n1.push_back(nums[i]);
+            if(i != n-1) n2.push_back(nums[i]);
         }
-        n--;
-        vector<int> dp1(n , -1);///////////////////
-        vector<int> dp2(n , -1);//////////////////////
-        return max(f(n-1 , v1 , dp1) , f(n-1 , v2 , dp2));
+        
+        return max(f(n1.size()-1 , n1 , dp1) , f(n2.size()-1 , n2 , dp2));
     }
 };
