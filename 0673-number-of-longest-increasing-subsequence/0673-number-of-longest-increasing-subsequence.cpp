3d9646ef8ca9300eac8a05b56f6f10473b228803
Jafrin <khan.jafrin.9346@gmail.com>
2023-09-02 22:09:54 +0530

    Time: 195 ms (46.91%), Space: 13.1 MB (65.49%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -3,33 +3,37 @@ public:
     int findNumberOfLIS(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n ,1);
+        
         vector<int> cnt(n , 1);
+        vector<int> dp(n , 1);
         
         int lis = 1;
         
         for(int ind = 0 ; ind < n ; ind++){
             for(int prev = 0 ; prev < ind ; prev++){
-                
-                if(nums[prev] < nums[ind] && dp[prev] + 1 > dp[ind]){
-                    dp[ind] = dp[prev] +1;
-                    cnt[ind] = cnt[prev]; 
+                if(nums[prev] < nums[ind] && dp[ind] < dp[prev]+1){
+                    dp[ind] = dp[prev]+1;
+                    cnt[ind] = cnt[prev];
                 }
                 
-                else if(nums[prev] < nums[ind] && dp[prev] + 1 == dp[ind]){
+                else if(nums[prev] < nums[ind] && dp[ind] == dp[prev]+1){
+                    dp[ind] = dp[prev]+1;
                     cnt[ind] += cnt[prev];
                 }
+                
+                lis = max(lis , dp[ind]); 
             }
-            
-            lis = max(lis , dp[ind]);
         }
         
         int ans = 0;
+        
         for(int i = 0 ; i < n ; i++){
-            if(dp[i] == lis) ans += cnt[i];
+            if(dp[i] == lis){
+                ans += cnt[i];;
+            }
         }
         
         return ans;
+        
     }
-    
 };
