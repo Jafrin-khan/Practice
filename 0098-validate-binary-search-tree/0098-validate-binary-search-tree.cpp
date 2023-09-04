f045ecabd494ecbb4df80ce6b803a7e3bb52773e
Jafrin <khan.jafrin.9346@gmail.com>
2023-09-04 21:27:31 +0530

    Time: 7 ms (84.74%), Space: 21.9 MB (17.00%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,35 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    /*
-    TC = O(N)
-    SC = O(N)
-    */
-    bool f(TreeNode* root , TreeNode* max , TreeNode* min){
-        
-        if(root == NULL){
-            return true;
-        }
-        
-        if((max != NULL && root->val >= max->val) || (min != NULL && root->val <= min->val)){
-            return false;
-        }
-        
-        return f(root->left , root , min) && f(root->right , max , root);
-    }
     
-    bool isValidBST(TreeNode* root) {
+    bool f(TreeNode* root , TreeNode* maxi , TreeNode* mini){
         
-        return f(root , NULL , NULL); 
+        if(root == NULL) return true;
+        
+        if((mini != NULL && root->val <= mini->val) || (maxi != NULL && root->val >= maxi->val)) return false;
+        
+        return f(root->left , root , mini) && f(root->right , maxi , root);
+    }
+    bool isValidBST(TreeNode* root) {
+        return f(root , NULL , NULL);
     }
 };
