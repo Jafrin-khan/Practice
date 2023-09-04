d0f4603a172cc8e8aa8b4c2b02c28fb26e32edea
Jafrin <khan.jafrin.9346@gmail.com>
2023-09-04 23:23:06 +0530

    Time: 24 ms (37.01%), Space: 26.1 MB (70.16%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,27 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* f(int& ind , int si , int ei , vector<int>& inorder , vector<int>& postorder){
+
+     TreeNode* f(int& ind , int si , int ei , vector<int>& postorder, vector<int>& inorder){
+        
         if(si > ei) return NULL;
-        int value = postorder[ind];
+        
+        TreeNode* root = new TreeNode(postorder[ind]);
         ind--;
-        TreeNode* root = new TreeNode(value);
         
         int idx;
         for(int i = si ; i <= ei ; i++){
-            if(inorder[i] == value){
+            if(inorder[i] == root->val){
                 idx = i;
                 break;
             }
         }
         
-        root->right = f(ind , idx+1 , ei , inorder , postorder);
-        root->left = f(ind , si , idx-1 , inorder , postorder);
+        root->right = f(ind , idx+1 , ei , postorder , inorder);
+        root->left = f(ind , si , idx-1 , postorder , inorder);
         
-        return root;  
+        return root;
+        
     }
-    
+   
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int ind = postorder.size()-1;
-        return f(ind , 0 , inorder.size()-1 , inorder , postorder);
+       int ind = postorder.size()-1;
+       return f(ind , 0 , inorder.size()-1 , postorder , inorder);
     }
-}; 
+};
