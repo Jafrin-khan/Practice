452e5eca04638282caefdef5c2d917ec6578df70
Jafrin <khan.jafrin.9346@gmail.com>
2023-09-09 21:16:31 +0530

    Time: 12 ms (77.64%), Space: 11.5 MB (55.86%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,8 +10,7 @@
  */
 class Solution {
 public:
-    //faltu code h kaam chalau dont refer it...jaise taise debug kara
-    ListNode* reverseList(ListNode* head) {
+     ListNode* reverse(ListNode* head){
         
         if(head == NULL) return head;
         
@@ -27,34 +26,36 @@ public:
         return prev;
     }
     
-    ListNode* reverseKGroup(ListNode* head, int k) {
+    int len(ListNode* head){
+        int l = 0;
+        while(head != NULL){
+            l++;
+            head = head->next;
+        }
+        return l;
+    }
+    
+    ListNode* f(ListNode* head , int k){
         
-        int len = 0;
-        ListNode* temp = head;
+        if(head == NULL || head->next == NULL || len(head) < k) return head;
         
-        while(len < k-1){
-            if(temp == NULL) return head;
-            len++;
-            temp = temp->next;
+        int kk = k-1;
+        ListNode* t = head;
+        
+        while(kk--){
+            t = t->next;
         }
         
-        if(temp == NULL) {
-            if(len > k-1){ return reverseList(head);}
-            else {return head; }  
-        }
+        ListNode* nextList = t->next;
+        nextList = f(nextList , k);
+        t->next = NULL;
         
-        ListNode* restList = temp->next;
-        temp->next = NULL;
-        
-        ListNode* newHead = reverseList(head);
-        
-        ListNode* traverse = newHead;
-        while(traverse->next != NULL) traverse = traverse->next;
-        
-        restList = reverseKGroup(restList, k);
-        traverse->next = restList;
+        ListNode* newHead = reverse(head);
+        head->next = nextList;
         
         return newHead;
-           
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        return f(head , k);
     }
 };
