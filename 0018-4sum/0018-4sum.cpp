f8c02fd02fd4e8936d06f22e90a21874839c2ba2
Jafrin <khan.jafrin.9346@gmail.com>
2023-09-10 13:58:30 +0530

    Time: 54 ms (86.11%), Space: 13.3 MB (53.52%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,37 +1,34 @@
 class Solution {
-    #define ll long long
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
+        long long n = nums.size();
+        sort(nums.begin() , nums.end());
+        
         vector<vector<int>> ans;
         
-        sort(nums.begin() , nums.end());
-        ll n = nums.size();
-        
-        for(ll i = 0 ; i < n ; i++){
+        for(long long i = 0 ; i < n ; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            ll nT = target - nums[i];
-            for(ll j = i + 1 ; j < n ; j++){
+            long long nT = target - nums[i];
+            for(long long j = i+1 ; j < n ; j++){
                 if(j > i+1 && nums[j] == nums[j-1]) continue;
-                ll toFind = nT - nums[j];///////////////////////////////////////// DONT USE NT -= NUMS[J] AS NT VALUE HUM UPER WAALE LOOP SE LERE ISLIE CHANGE NI KR SKTE ISLIE HUME NAYA VARIABLE BANANE HAI 
+                long long k = nT - nums[j];
                 
-                ll left = j + 1;
-                ll right = n - 1;
+                long long left = j+1;
+                long long right = n-1;
                 while(left < right){
-                    if(nums[left] + nums[right] == toFind){
-                        vector<int> v;
-                        v = {nums[i] , nums[j] , nums[left] , nums[right]};
-                        left++;
-                        right--;
+                    if(nums[left] + nums[right] == k){
+                        ans.push_back({nums[i] , nums[j] , nums[left] , nums[right]});
+                        left++;right--;
                         
-                        ans.push_back(v);
-                   
-                    
-                    while(left < right && nums[left] == nums[left-1]) left++;
-                    while(left < right && nums[right] == nums[right+1]) right--;
+                        while(left < right && nums[left] == nums[left-1]) left++;
+                        while(left < right && nums[right] == nums[right+1]) right--;
                     }
                     
-                    else if((nums[left] + nums[right]) < toFind) left++;
+                    else if(nums[left] + nums[right] < k){
+                        left++;
+                    }
+                    
                     else right--;
                 }
             }
