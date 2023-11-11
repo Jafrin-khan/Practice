91e7e95b11013c2430276265e3e245a96b756076
Jafrin <khan.jafrin.9346@gmail.com>
2023-11-11 22:26:49 +0530

    Time: 8 ms (43.87%), Space: 10.5 MB (30.36%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -5,23 +5,22 @@ public:
         
         if(i < 0 || j < 0 || i >= n || j >= m) return 1e9;
         
-        if(i == 0 && j == 0) return grid[0][0];
-        
         if(dp[i][j] != -1) return dp[i][j];
         
-        int up = grid[i][j] + f(i-1 , j , n , m , grid , dp);
-        int left = grid[i][j] + f(i , j-1 , n , m , grid , dp);
+        if(i == 0 && j == 0) return grid[0][0];
         
-        return dp[i][j] = min(up , left);
+        int take = grid[i][j] + f(i-1 , j , n , m , grid , dp);
+        int notTake = grid[i][j] + f(i , j-1 , n , m , grid , dp);
+        
+        return dp[i][j] = min(take , notTake);
     }
+    
     int minPathSum(vector<vector<int>>& grid) {
-        
         int n = grid.size();
         int m = grid[0].size();
         
         vector<vector<int>> dp(n , vector<int>(m , -1));
         
         return f(n-1 , m-1 , n , m , grid , dp);
-        
     }
 };
