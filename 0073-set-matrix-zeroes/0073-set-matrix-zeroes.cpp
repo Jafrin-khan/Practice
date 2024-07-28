b804b7e18211f1706af67e99191c6293eb31525a
Jafrin <khan.jafrin.9346@gmail.com>
2024-07-28 13:38:14 +0530

    Time: 6 ms (88.33%), Space: 17 MB (13.33%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,78 @@
 class Solution {
 public:
-    //03/09/2023 ka submission consider karo revise k lie
+  
+    /*
+    1 1 1
+    1 0 1
+    1 1 1
     
-    void setZeroes(vector<vector<int>>& matrix) {
+    r = 1
+    c = 1
         
-        int n = matrix.size();
-        int m = matrix[0].size();
+    {0,1} {2,1} m changing and n constant
+    {1,0} {1,2} n  changing and m constant
+    */
+    
+    
+    
+//     1 1 1
+//     1 0 1
+//     1 1 1
         
-        vector<int> row , col;
+//     (1,1)
+        
+//     1,0 1,1 1,2
+//     0,1 1,1 2,1
+    
+
+    void setZeroes(vector<vector<int>>& grid) {
+        
+        /* BRUTE */
+//         int r = grid.size();
+//         int c = grid[0].size();
+        
+//         for(int i = 0 ; i < r ; i++){
+//             for(int j = 0 ; j < c ; j++){
+                
+//                 if(grid[i][j] == 0){
+//                     //r constant
+//                     for(int p = 0 ; p < c ; p++) grid[i][p] = -1;
+//                     //c constant
+//                     for(int q = 0 ; q < r ; q++) grid[q][j] = -1;
+//                 }
+//             }
+//         }
+        
+//         for(int i = 0 ; i < r ; i++){
+//             for(int j = 0 ; j < c ; j++){
+//                 if(grid[i][j] == -1)  grid[i][j] = 0;
+//             }
+//         }
+        
+        /*Better*/
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<int> row;
+        vector<int> col;
         
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(matrix[i][j] == 0){
+                if(grid[i][j] == 0){
                     row.push_back(i);
                     col.push_back(j);
                 }
             }
         }
         
-        for(auto i : row){
-            for(int j = 0 ; j < m ; j++) matrix[i][j] = 0;
+        for(auto it : row){
+            for(int i = 0 ; i < m ; i++) grid[it][i] = 0;
         }
         
-        for(auto j : col){
-            for(int i = 0 ; i < n ; i++) matrix[i][j] = 0;
+        for(auto it : col){
+            for(int i = 0 ; i < n ; i++) grid[i][it] = 0;
         }
+        
     }
-};
+}; 
