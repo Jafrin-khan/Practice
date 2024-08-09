4b2b757479f3b6259854279f0794573274aad2b4
Jafrin <khan.jafrin.9346@gmail.com>
2024-08-09 22:00:44 +0530

    Time: 4 ms (32.09%), Space: 10.1 MB (15.71%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,89 +1,31 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    /*
-    //Recursion
-    //TC = O(N)
-    //SC = O(N)
-    void inOrder(TreeNode* root , vector<int>& ans){
+    
+    void inOrder(TreeNode* root , vector<int>& v){
         if(root == NULL) return;
         
-        
-        inOrder(root->left , ans);
-        ans.push_back(root->val);
-        inOrder(root->right , ans);
+        inOrder(root->left , v);
+        v.push_back(root->val);
+        inOrder(root->right , v);
     }
     
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        inOrder(root , ans);
         
-        return ans;
+        vector<int> v;
+        inOrder(root , v);
+        
+        return v;
+        
     }
-    */
-    
-    
-    /*
-    //Iterative
-    //TC = O(N)
-    //SC = O(N)
-   
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        stack<TreeNode*> st;
-        
-        while(true){
-            if(root != NULL){
-                st.push(root);
-                root = root->left;
-            }
-            else{
-                if(st.empty()) break;
-                root = st.top();st.pop();
-                ans.push_back(root->val);
-                root = root->right;
-            }
-        }
-        
-        return ans;
-    }
-    */
-    
-    //MORRIS
-    //O(N)
-    //O(1)
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        TreeNode* curr = root;
-        
-        while(curr != NULL){
-            
-            if(curr->left == NULL){
-                ans.push_back(curr->val);
-                curr = curr->right;
-            }
-            
-            else{
-                TreeNode* prev = curr->left;
-                while(prev->right && prev->right != curr){
-                    prev = prev->right;
-                }
-                
-                if(prev->right == curr){
-                    prev->right = NULL;
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                }
-                
-                else{
-                    prev->right = curr;
-                    curr = curr->left;
-                }
-            }
-        }
-        
-        return ans;
-    }
-    
-    
 };
