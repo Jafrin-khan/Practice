703f0e9526debce9715fe0fe6fb432c80f650417
Jafrin <khan.jafrin.9346@gmail.com>
2024-09-25 23:32:01 +0530

    Time: 0 ms (100.00%), Space: 17.5 MB (10.82%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    /*
-        
-Time Complexity:O(2^n*k)
-
-Reason: Assume if all the elements in the array are unique then the no. of subsequence you will get will be O(2^n). we also add the ds to our ans when we reach the base case that will take “k”//average space for the ds.
-
-Space Complexity:O(k*x)
-
-Reason: if we have x combinations then space will be x*k where k is the average length of the combination.
-    */
-    void f(int ind , int k , vector<vector<int>>& ans , vector<int> helper , vector<int>& nums){
-       
-        if(k == 0) {
+    
+    void f(int ind , int k , vector<int> helper , vector<vector<int>>& ans , vector<int> nums){
+        if(k == 0){
             ans.push_back(helper);
             return;
         }
         
         for(int i = ind ; i < nums.size() ; i++){
-            
             if(i > ind && nums[i] == nums[i-1]) continue;
             
-            if(k < nums[i]) break;//***********************************************
-            
+            if(k >= nums[i]){
             helper.push_back(nums[i]);
-            f(i+1 , k - nums[i] , ans , helper , nums);
+            f(i+1 , k - nums[i] , helper , ans , nums);
             helper.pop_back();
+            }
         }
     }
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+    
+    vector<vector<int>> combinationSum2(vector<int>& nums, int k) {
+        
+        sort(nums.begin() , nums.end());
         
         int n = nums.size();
-        sort(nums.begin() , nums.end());//***************************************
-        
-        vector<vector<int>> ans;
         vector<int> helper;
-        f(0 , target , ans , helper , nums);
+        vector<vector<int>> ans;
+        
+        f(0 , k , helper , ans , nums);
         
         return ans;
     }
