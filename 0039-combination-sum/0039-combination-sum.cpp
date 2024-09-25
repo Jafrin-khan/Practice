52985f9f620ac982180fe7f7e931127b6a711e05
Jafrin <khan.jafrin.9346@gmail.com>
2024-09-25 23:15:17 +0530

    Time: 35 ms (21.70%), Space: 34.9 MB (20.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     
-    void f(int ind , int k , vector<int>& helper , vector<vector<int>>& ans , vector<int>& nums){
+    void f(int ind , int k , vector<int>& nums, vector<int> helper , vector<vector<int>>& ans){
         
-        if(ind < 0){
-            if(k == 0) {
+        if(ind < 0) return;
+        
+        if(k == 0){
             ans.push_back(helper);
-        }
             return;
         }
+        
+        //notTake
+        f(ind-1 , k , nums , helper , ans);
         
         //take
         if(k >= nums[ind]){
         helper.push_back(nums[ind]);
-        f(ind , k - nums[ind] , helper , ans , nums);
+        f(ind , k - nums[ind] , nums, helper , ans);
         helper.pop_back();
         }
-        
-        //notTake
-        f(ind-1 , k , helper , ans , nums);
     }
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+    
+    vector<vector<int>> combinationSum(vector<int>& nums, int k) {
         
-        int n = nums.size();
-        
-         vector<vector<int>> ans;
         vector<int> helper;
-        f(n-1 , target ,helper, ans , nums);
+        vector<vector<int>> ans;
+        set<vector<int>> st;
         
-       
+        f(nums.size()-1 , k , nums , helper , ans);
         
         return ans;
     }
