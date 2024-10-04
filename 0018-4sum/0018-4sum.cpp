c4a4cdfa351d07cbf71e01f341a87d9f035516b9
Jafrin <khan.jafrin.9346@gmail.com>
2024-10-05 00:23:17 +0530

    Time: 18 ms (91.06%), Space: 16.1 MB (72.71%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,33 +2,30 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
-        long long n = nums.size();
         sort(nums.begin() , nums.end());
         
+        int n = nums.size();
         vector<vector<int>> ans;
         
-        for(long long i = 0 ; i < n ; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            long long nT = target - nums[i];
-            for(long long j = i+1 ; j < n ; j++){
-                if(j > i+1 && nums[j] == nums[j-1]) continue;
-                long long k = nT - nums[j];
+        for(int i = 0 ; i < n ; i++){
+            if(i != 0 && nums[i] == nums[i-1]) continue;
+            
+            long long k = target - nums[i];
+            for(int j = i+1 ; j < n ; j++){
+               if(j != i+1 && nums[j] == nums[j-1]) continue;
+                long long kk = k - nums[j];
                 
-                long long left = j+1;
-                long long right = n-1;
+                int left = j+1;
+                int right = n-1;
+                
                 while(left < right){
-                    if(nums[left] + nums[right] == k){
-                        ans.push_back({nums[i] , nums[j] , nums[left] , nums[right]});
-                        left++;right--;
-                        
-                        while(left < right && nums[left] == nums[left-1]) left++;
-                        while(left < right && nums[right] == nums[right+1]) right--;
+                    if(nums[left] + nums[right] == kk) {
+                    ans.push_back({nums[i] , nums[j] , nums[left++] , nums[right--]});
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
                     }
                     
-                    else if(nums[left] + nums[right] < k){
-                        left++;
-                    }
-                    
+                    else if(nums[left] + nums[right] < kk) left++;
                     else right--;
                 }
             }
