e8cb0c4e3102ae66f8498c37c2b2556d3af0844b
Jafrin <khan.jafrin.9346@gmail.com>
2024-10-06 23:08:12 +0530

    Time: 187 ms (13.63%), Space: 148.6 MB (15.86%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     
-    /*
-Time Complexity: O( (2^n) *k*(n/2) )
-
-Reason: O(2^n) to generate every substring and O(n/2)  to check if the substring generated is a palindrome. O(k) is for inserting the palindromes in another data structure, where k  is the average length of the palindrome list.
-
-Space Complexity: O(k * x)
-
-Reason: The space complexity can vary depending upon the length of the answer. k is the average length of the list of palindromes and if we have x such list of palindromes in our final answer. The depth of the recursion tree is n, so the auxiliary space required is equal to the O(n).
-    */
-    bool isValid(string s){
-        int start = 0;
-        int end = s.size()-1;
-        
-        while(start < end){
-            if(s[start++] != s[end--]) return false;
-        }
+    bool isValid(int start , int end , string s){
+       while(start < end){
+                if(s[start] != s[end]) return false;
+                start++;
+                end--;
+            }
         return true;
     }
     
-    void f(int ind , vector<vector<string>>& ans , vector<string> helper , string s){
+    void f(int ind , vector<string> v , vector<vector<string>>& ans , string s){
         
         if(ind == s.size()){
-            ans.push_back(helper);//****************************************************************
+            ans.push_back(v);
             return;
         }
         
         for(int i = ind ; i < s.size() ; i++){
-            string temp = s.substr(ind , i - ind + 1);
-            if(isValid(temp)){
-                helper.push_back(temp);
-                f(i+1 , ans , helper , s);
-                helper.pop_back();
+            if(isValid(ind , i , s)){
+                v.push_back(s.substr(ind , i - ind + 1));
+                f(i+1 , v , ans , s);
+                v.pop_back();
             }
         }
     }
@@ -42,7 +31,9 @@ Reason: The space complexity can vary depending upon the length of the answer. k
         vector<vector<string>> ans;
         vector<string> helper;
         
-        f(0 , ans , helper , s);
+        f(0 , helper , ans , s);
+        
         return ans;
+        
     }
 };
